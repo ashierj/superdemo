b80a051f3e716b9c29b31f0a6fbfd492049e937c
Malcolm Locke <mlocke@gitlab.com>
2023-09-27 21:52:25 +1300

    Add LATERAL query to Security::FindingsFinder
    This change adds a new query strategy using LATERAL to the
    `Security::FindingsFinder` .
    
    Currently the performance on this finder is very bad on pipelines with
    large numbers of findings as it must filter all of the findings before
    ordering the result.
    
    This change allows a reduction on the number of rows filtered when
    paginating the lower pages of the result.
    
    The new query has test parity with the old one and is behind a new
    feature flag - security_findings_finder_lateral_join

--- ee/app/finders/security/findings_finder.rb
+++ ee/app/finders/security/findings_finder.rb
@@ -20,7 +20,7 @@ module Security
 
     ResultSet = Struct.new(:relation, :findings) do
       delegate :current_page, :limit_value, :total_pages, :total_count,
-        :next_page, :prev_page, :page, :empty?, to: :relation
+        :next_page, :prev_page, :page, :empty?, :without_count, to: :relation
     end
 
     DEFAULT_PAGE = 1
@@ -81,14 +81,14 @@ module Security
     end
 
     def security_findings
-      @security_findings ||= include_dismissed? ? all_security_findings : undismissed_security_findings
+      @security_findings ||= if Feature.enabled?(:security_findings_finder_lateral_join, project)
+                               load_security_findings_lateral
+                             else
+                               load_security_findings
+                             end
     end
 
-    def undismissed_security_findings
-      all_security_findings.undismissed_by_vulnerability
-    end
-
-    def all_security_findings
+    def load_security_findings
       pipeline.security_findings
               .with_pipeline_entities
               .with_scan
@@ -107,6 +107,63 @@ module Security
               .then { |relation| by_severity_levels(relation) }
               .then { |relation| by_scanner_external_ids(relation) }
               .then { |relation| by_state(relation) }
+              .then { |relation| by_include_dismissed(relation) }
+    end
+
+    def load_security_findings_lateral
+      # This method generates a query of the general form
+      #
+      #   SELECT security_findings.*
+      #   FROM security_scans
+      #   LATERAL (
+      #     SELECT * FROM security_findings
+      #     LIMIT n
+      #   )
+      #   WHERE security_scans.x = 'y'
+      #   ...
+      #
+      # This is done for performance reasons to reduce the amount of data loaded
+      # in the query compared to a more conventional
+      #
+      #   SELECT security_findings.*
+      #   FROM security_findings
+      #   JOIN security_scans ...
+      #
+      # The latter form can end up reading a very large number of rows on projects
+      # with high numbers of findings.
+
+      lateral_relation = Security::Finding
+        .where('"security_findings"."scan_id" = "security_scans"."id"') # rubocop:disable CodeReuse/ActiveRecord
+        .where('"security_findings"."severity" = "severities"."severity"') # rubocop:disable CodeReuse/ActiveRecord
+        .deduplicated
+        .ordered
+        .then { |relation| by_uuid(relation) }
+        .then { |relation| by_confidence_levels(relation) }
+        .then { |relation| by_scanner_external_ids(relation) }
+        .then { |relation| by_state(relation) }
+        .then { |relation| by_include_dismissed(relation) }
+        .limit(per_page * page)
+
+      from_sql = <<~SQL.squish
+          "security_scans",
+          unnest('{#{severities.join(',')}}'::smallint[]) AS "severities" ("severity"),
+          LATERAL (#{lateral_relation.to_sql}) AS "security_findings"
+      SQL
+
+      Security::Finding
+        .from(from_sql) # rubocop:disable CodeReuse/ActiveRecord
+        .with_pipeline_entities
+        .with_scan
+        .with_scanner
+        .with_state_transitions
+        .with_issue_links
+        .with_merge_request_links
+        .merge(::Security::Scan.by_pipeline_ids(pipeline.id))
+        .merge(::Security::Scan.latest_successful)
+        .ordered
+        .then { |relation| by_report_types(relation) }
+        .page(page)
+        .per(per_page)
     end
 
     def per_page
@@ -119,6 +176,12 @@ module Security
 
     def include_dismissed?
       params[:scope] == 'all' || params[:state]
+    end
+
+    def by_include_dismissed(relation)
+      return relation if include_dismissed?
+
+      relation.undismissed_by_vulnerability
     end
 
     def by_scanner_external_ids(relation)
@@ -142,13 +205,25 @@ module Security
     def by_report_types(relation)
       return relation unless params[:report_type]
 
-      relation.by_report_types(params[:report_type])
+      if Feature.enabled?(:security_findings_finder_lateral_join, project)
+        relation.merge(::Security::Scan.by_scan_types(params[:report_type]))
+      else
+        relation.by_report_types(params[:report_type])
+      end
     end
 
     def by_severity_levels(relation)
       return relation unless params[:severity]
 
       relation.by_severity_levels(params[:severity])
+    end
+
+    def severities
+      if params[:severity]
+        Security::Finding.severities.fetch_values(*params[:severity])
+      else
+        Security::Finding.severities.values
+      end
     end
 
     def by_uuid(relation)
--- ee/spec/finders/security/pure_findings_finder_spec.rb
+++ ee/spec/finders/security/pure_findings_finder_spec.rb
@@ -3,42 +3,52 @@
 require 'spec_helper'
 
 RSpec.describe Security::PureFindingsFinder, feature_category: :vulnerability_management do
-  it_behaves_like 'security findings finder' do
-    let(:findings) { finder_result.to_a }
-    let(:query_limit) { 8 }
-
-    describe 'parsing artifacts' do
+  [true, false].each do |security_findings_finder_lateral_join|
+    context "when security_findings_finder_lateral_join enabled is #{security_findings_finder_lateral_join}" do
       before do
-        allow(::Gitlab::Ci::Parsers).to receive(:fabricate!)
+        stub_feature_flags(
+          security_findings_finder_lateral_join: security_findings_finder_lateral_join
+        )
       end
 
-      it 'does not parse artifacts' do
-        service_object.execute
+      it_behaves_like 'security findings finder' do
+        let(:findings) { finder_result.to_a }
+        let(:query_limit) { 8 }
 
-        expect(::Gitlab::Ci::Parsers).not_to have_received(:fabricate!)
-      end
-    end
+        describe 'parsing artifacts' do
+          before do
+            allow(::Gitlab::Ci::Parsers).to receive(:fabricate!)
+          end
 
-    describe '#available?' do
-      let_it_be(:pipeline) { create(:ci_pipeline) }
-      let_it_be(:scan) { create(:security_scan, :latest_successful, pipeline: pipeline) }
+          it 'does not parse artifacts' do
+            service_object.execute
 
-      subject { service_object }
+            expect(::Gitlab::Ci::Parsers).not_to have_received(:fabricate!)
+          end
+        end
 
-      context 'when there are zero security findings' do
-        it { is_expected.not_to be_available }
-      end
+        describe '#available?' do
+          let_it_be(:pipeline) { create(:ci_pipeline) }
+          let_it_be(:scan) { create(:security_scan, :latest_successful, pipeline: pipeline) }
 
-      context 'when there is a security finding without finding data' do
-        let_it_be(:security_finding) { create(:security_finding, scan: scan, finding_data: {}) }
+          subject { service_object }
 
-        it { is_expected.not_to be_available }
-      end
+          context 'when there are zero security findings' do
+            it { is_expected.not_to be_available }
+          end
 
-      context 'when there is a security finding with finding data' do
-        let_it_be(:security_finding) { create(:security_finding, :with_finding_data, scan: scan) }
+          context 'when there is a security finding without finding data' do
+            let_it_be(:security_finding) { create(:security_finding, scan: scan, finding_data: {}) }
 
-        it { is_expected.to be_available }
+            it { is_expected.not_to be_available }
+          end
+
+          context 'when there is a security finding with finding data' do
+            let_it_be(:security_finding) { create(:security_finding, :with_finding_data, scan: scan) }
+
+            it { is_expected.to be_available }
+          end
+        end
       end
     end
   end
