df5e82018f62fdd50a0e991787ffc65c56f25060
huzaifaiftikhar1 <hiftikhar@gitlab.com>
2023-10-10 16:21:48 +0530

    GraphQL API for updating audit event streaming amazon_s3_configurations
    Changelog: added
    EE: true

---
+++ ee/app/graphql/mutations/audit_events/amazon_s3_configurations/update.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+module Mutations
+  module AuditEvents
+    module AmazonS3Configurations
+      class Update < Base
+        graphql_name 'AmazonS3ConfigurationUpdate'
+
+        include ::Audit::Changes
+
+        UPDATE_EVENT_NAME = 'amazon_s3_configuration_updated'
+        AUDIT_EVENT_COLUMNS = [:access_key_xid, :secret_access_key, :bucket_name, :aws_region, :name].freeze
+
+        authorize :admin_external_audit_events
+
+        argument :id, ::Types::GlobalIDType[::AuditEvents::AmazonS3Configuration],
+          required: true,
+          description: 'ID of the Amazon S3 configuration to update.'
+
+        argument :name, GraphQL::Types::String,
+          required: false,
+          description: 'Destination name.'
+
+        argument :access_key_xid, GraphQL::Types::String,
+          required: false,
+          description: 'Access key ID of the Amazon S3 account.'
+
+        argument :secret_access_key, GraphQL::Types::String,
+          required: false,
+          description: 'Secret access key of the Amazon S3 account.'
+
+        argument :bucket_name, GraphQL::Types::String,
+          required: false,
+          description: 'Name of the bucket where the audit events would be logged.'
+
+        argument :aws_region, GraphQL::Types::String,
+          required: false,
+          description: 'AWS region where the bucket is created.'
+
+        field :amazon_s3_configuration, ::Types::AuditEvents::AmazonS3ConfigurationType,
+          null: true,
+          description: 'Updated Amazon S3 configuration.'
+
+        def resolve(id:, access_key_xid: nil, secret_access_key: nil, bucket_name: nil, aws_region: nil, name: nil)
+          config = authorized_find!(id)
+          config_attributes = {
+            access_key_xid: access_key_xid,
+            secret_access_key: secret_access_key,
+            bucket_name: bucket_name,
+            aws_region: aws_region,
+            name: name
+          }.compact
+
+          if config.update(config_attributes)
+            audit_update(config)
+            { amazon_s3_configuration: config, errors: [] }
+          else
+            { amazon_s3_configuration: nil, errors: Array(config.errors) }
+          end
+        end
+
+        private
+
+        def audit_update(config)
+          AUDIT_EVENT_COLUMNS.each do |column|
+            audit_changes(
+              column,
+              as: column.to_s,
+              entity: config.group,
+              model: config,
+              event_type: UPDATE_EVENT_NAME
+            )
+          end
+        end
+
+        def find_object(config_gid)
+          GitlabSchema.object_from_id(config_gid, expected_type: ::AuditEvents::AmazonS3Configuration).sync
+        end
+      end
+    end
+  end
+end
