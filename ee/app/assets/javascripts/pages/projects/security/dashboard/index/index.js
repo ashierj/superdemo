d1c213ebe0bbbfeca9b0729768be20891889b488
Simon Knox <simon@gitlab.com>
2023-10-10 15:41:41 +1100

    Remove waitForCssLoaded function
    Part of removing startupCSS, which has already been disabled
    so this function was just resolving immediately

--- ee/app/assets/javascripts/pages/projects/security/dashboard/index/index.js
+++ ee/app/assets/javascripts/pages/projects/security/dashboard/index/index.js
@@ -1,10 +1,7 @@
 import initSecurityDashboard from 'ee/security_dashboard/security_dashboard_init';
 import { DASHBOARD_TYPES } from 'ee/security_dashboard/store/constants';
-import { waitForCSSLoaded } from '~/helpers/startup_css_helper';
 
-waitForCSSLoaded(() => {
-  initSecurityDashboard(
-    document.getElementById('js-project-security-dashboard'),
-    DASHBOARD_TYPES.PROJECT,
-  );
-});
+initSecurityDashboard(
+  document.getElementById('js-project-security-dashboard'),
+  DASHBOARD_TYPES.PROJECT,
+);
