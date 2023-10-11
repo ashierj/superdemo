1d2d4f15e1bf6e1f97dd4f8a4c7e502a1761cb75
Miguel Rincon <mrincon@gitlab.com>
2023-10-11 12:26:11 +0200

    Remove redundant version in runner details
    This change updates the runner details to remove the duplicated version
    field that is already present in the table.
    
    It also relocated the runner upgrade badge to the header of the runner
    in the EE version.
    
    Changelog: changed

--- ee/spec/frontend/ci/runner/components/runner_details_spec.js
+++ ee/spec/frontend/ci/runner/components/runner_details_spec.js
@@ -1,12 +1,8 @@
-import { GlAlert } from '@gitlab/ui';
-import { s__ } from '~/locale';
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+
 import RunnerDetails from '~/ci/runner/components/runner_details.vue';
 import RunnerDetail from '~/ci/runner/components/runner_detail.vue';
-import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
-import { UPGRADE_STATUS_AVAILABLE } from 'ee/ci/runner/constants';
-
 import RunnerMaintenanceNoteDetail from 'ee_component/ci/runner/components/runner_maintenance_note_detail.vue';
-import RunnerUpgradeStatusBadge from 'ee_component/ci/runner/components/runner_upgrade_status_badge.vue';
 
 import { runnerData } from 'jest/ci/runner/mock_data';
 
@@ -16,7 +12,6 @@ describe('RunnerDetails', () => {
   let wrapper;
 
   const findRunnerMaintenanceNoteDetail = () => wrapper.findComponent(RunnerMaintenanceNoteDetail);
-  const findRunnerUpgradeStatusBadge = () => wrapper.findComponent(RunnerUpgradeStatusBadge);
 
   const createComponent = ({ props = {}, mountFn = shallowMountExtended, ...options } = {}) => {
     wrapper = mountFn(RunnerDetails, {
@@ -26,34 +21,6 @@ describe('RunnerDetails', () => {
       ...options,
     });
   };
-
-  describe('Upgrade status', () => {
-    describe.each`
-      feature                                      | provide
-      ${'runner_upgrade_management'}               | ${{ glFeatures: { runnerUpgradeManagement: true } }}
-      ${'runner_upgrade_management_for_namespace'} | ${{ glFeatures: { runnerUpgradeManagementForNamespace: true } }}
-    `('When $feature is available', ({ provide }) => {
-      beforeEach(() => {
-        createComponent({
-          props: {
-            runner: {
-              ...mockRunner,
-              upgradeStatus: UPGRADE_STATUS_AVAILABLE,
-            },
-          },
-          stubs: {
-            GlAlert,
-            RunnerDetail,
-          },
-          provide,
-        });
-      });
-
-      it('displays upgrade available badge', () => {
-        expect(findRunnerUpgradeStatusBadge().text()).toBe(s__('Runners|Upgrade available'));
-      });
-    });
-  });
 
   describe('Maintenance Note', () => {
     const mockNoteHtml = 'Note.';
