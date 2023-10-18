5afd2caeb7f5f4658fc12fbb6cae901d552a5de3
Daniel Tian <dtian@gitlab.com>
2023-10-18 21:28:08 +0000

    Clean up AutoFixHelpText component

--- ee/app/assets/javascripts/security_dashboard/components/shared/auto_fix_help_text.vue
+++ ee/app/assets/javascripts/security_dashboard/components/shared/auto_fix_help_text.vue
@@ -1,8 +1,8 @@
 <script>
-import { GlBadge, GlPopover, GlIcon } from '@gitlab/ui';
+import { GlBadge, GlPopover, GlIcon, GlLink, GlSprintf } from '@gitlab/ui';
 import { s__ } from '~/locale';
 
-const ICONCOLOR = {
+const ICON_COLOR = {
   opened: 'gl-text-green-500',
   closed: 'gl-text-red-500',
   merged: 'gl-text-blue-500',
@@ -15,13 +15,12 @@ const ICON = {
 };
 
 export default {
-  i18n: {
-    AUTO_FIX: s__('AutoRemediation|Auto-fix'),
-  },
   components: {
     GlBadge,
     GlIcon,
     GlPopover,
+    GlLink,
+    GlSprintf,
   },
   props: {
     mergeRequest: {
@@ -29,9 +28,16 @@ export default {
       required: true,
     },
   },
+  computed: {
+    mergeRequestIdString() {
+      return this.mergeRequest.securityAutoFix
+        ? s__('AutoRemediation|!%{mergeRequestIid}: Auto-fix')
+        : s__('AutoRemediation|!%{mergeRequestIid}');
+    },
+  },
   methods: {
     getIconColor(state) {
-      return ICONCOLOR[state] || 'gl-text-gray-500';
+      return ICON_COLOR[state] || 'gl-text-gray-500';
     },
     getIcon(state) {
       return ICON[state] || 'issue-open-m';
@@ -41,39 +47,24 @@ export default {
 </script>
 
 <template>
-  <!-- eslint-disable @gitlab/vue-require-i18n-strings -->
   <div ref="popover" data-testid="vulnerability-solutions-bulb">
     <gl-badge ref="badge" variant="neutral" icon="merge-request" />
-    <gl-popover
-      data-testid="vulnerability-solutions-popover"
-      :target="() => $refs.popover"
-      placement="top"
-    >
+    <gl-popover :target="() => $refs.popover" placement="top">
       <template #title>
         <span>{{ s__('AutoRemediation| 1 Merge Request') }}</span>
       </template>
       <ul class="gl-list-style-none gl-pl-0 gl-mb-0">
         <li class="gl-align-items-center gl-display-flex gl-mb-2">
           <gl-icon
-            data-testid="vulnerability-solutions-popover-icon"
             :name="getIcon(mergeRequest.state)"
             :size="16"
             :class="getIconColor(mergeRequest.state)"
           />
-          <a
-            data-testid="vulnerability-solutions-popover-link"
-            :href="mergeRequest.webUrl"
-            class="gl-ml-3"
-          >
-            <span data-testid="vulnerability-solutions-popover-link-id"
-              >!{{ mergeRequest.iid
-              }}<span
-                v-if="mergeRequest.securityAutoFix"
-                data-testid="vulnerability-solutions-popover-link-autofix"
-                >{{ `: ${$options.i18n.AUTO_FIX}` }}</span
-              >
-            </span>
-          </a>
+          <gl-link :href="mergeRequest.webUrl" class="gl-ml-3">
+            <gl-sprintf :message="mergeRequestIdString">
+              <template #mergeRequestIid>{{ mergeRequest.iid }}</template>
+            </gl-sprintf>
+          </gl-link>
         </li>
       </ul>
     </gl-popover>
