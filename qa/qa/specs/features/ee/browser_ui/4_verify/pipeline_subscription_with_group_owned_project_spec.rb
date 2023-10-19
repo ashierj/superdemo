2072c31014201d320da57c66affba91bd797ef5c
Harsha Muralidhar <hmuralidhar@gitlab.com>
2023-10-19 02:05:11 +0000

    Promote verify specs to reliable - Iteration 1

--- qa/qa/specs/features/ee/browser_ui/4_verify/pipeline_subscription_with_group_owned_project_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/4_verify/pipeline_subscription_with_group_owned_project_spec.rb
@@ -49,7 +49,8 @@ module QA
       end
 
       context 'when upstream project new tag pipeline finishes' do
-        it 'triggers pipeline in downstream project', testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/347998' do
+        it 'triggers pipeline in downstream project', :reliable,
+          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/347998' do
           # Downstream project should have one pipeline at this time
           Support::Waiter.wait_until { downstream_project.pipelines.size == 1 }
 
