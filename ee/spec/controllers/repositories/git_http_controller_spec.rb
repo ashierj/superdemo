4b402dd6a316920f2e7acbcdb3571c3ee9bdd190
Javiera Tapia <jtapia@gitlab.com>
2023-10-20 00:19:52 -0300

    Set deploy key as actor for Git proxied requests (Geo)
    This commit sets deploy keys as access_actor in the EE context.
    Why? When a repository is not synchronized in a Geo secondary site
    due to selective synchronization, and it is forced to proxy
    Git commands (i.e., clone) to the primary site, the proxied request
    can bypass the scope of deploy keys and perform the action
    whereas deploy keys are not enabled for the project.
    
    Changelog: fixed
    EE: true

--- ee/spec/controllers/repositories/git_http_controller_spec.rb
+++ ee/spec/controllers/repositories/git_http_controller_spec.rb
@@ -3,6 +3,9 @@
 require 'spec_helper'
 
 RSpec.describe Repositories::GitHttpController, feature_category: :source_code_management do
+  include EE::GeoHelpers
+  include GitHttpHelpers
+
   context 'when repository container is a group wiki' do
     include WikiHelpers
 
@@ -25,8 +28,6 @@ RSpec.describe Repositories::GitHttpController, feature_category: :source_code_m
   end
 
   context 'git audit streaming event' do
-    include GitHttpHelpers
-
     it_behaves_like 'sends git audit streaming event' do
       subject do
         post :git_upload_pack, params: { repository_path: "#{project.full_path}.git" }
@@ -85,5 +86,124 @@ RSpec.describe Repositories::GitHttpController, feature_category: :source_code_m
         end
       end
     end
+  end
+
+  shared_context 'with public deploy keys and Geo proxied request' do
+    let_it_be(:admin) { create(:user, :admin) }
+    let_it_be(:group) { create(:group) }
+    let_it_be(:project) { create(:project, :private, :repository, namespace: group) }
+    let_it_be(:geo_node) do
+      create(:geo_node_with_selective_sync_for, :primary, model: project, namespaces: :model_parent)
+    end
+
+    let_it_be(:repository_path) { "#{project.full_path}.git" }
+
+    let_it_be_with_reload(:deploy_key) { create(:deploy_key, public: true, user: admin) }
+
+    let(:decoded_headers) do
+      { scope: project.full_path, gl_id: "key-#{deploy_key.id}" }
+    end
+
+    before do
+      stub_licensed_features(geo: true)
+      stub_current_geo_node(geo_node)
+
+      allow(::Gitlab::Geo::JwtRequestDecoder).to receive(:geo_auth_attempt?).and_return(true)
+      allow(controller).to receive(:verify_workhorse_api!).and_return(true)
+
+      allow_next_instance_of(::Gitlab::Geo::JwtRequestDecoder) do |instance|
+        allow(instance).to receive(:decode).and_return(decoded_headers)
+      end
+    end
+  end
+
+  shared_examples 'a request with write access needed' do
+    include_context 'with public deploy keys and Geo proxied request'
+
+    context 'and write access is granted' do
+      it 'returns a successful response' do
+        deploy_key.projects << project
+        deploy_key.save!
+
+        deploy_keys_project = deploy_key.deploy_keys_project_for(project)
+        deploy_keys_project.update!(can_push: true)
+
+        subject
+
+        expect(response).to have_gitlab_http_status(:ok)
+      end
+    end
+
+    context 'and write access is not granted' do
+      it 'returns a failed response' do
+        deploy_key.projects << project
+        deploy_key.save!
+
+        subject
+
+        expect(response).to have_gitlab_http_status(:forbidden)
+        expect(response.body).to eq("This deploy key does not have write access to this project.")
+      end
+    end
+  end
+
+  shared_examples 'a request without write access needed' do
+    include_context 'with public deploy keys and Geo proxied request'
+
+    context 'and enabled for project' do
+      it 'returns a successful response' do
+        deploy_key.projects << project
+        deploy_key.save!
+
+        subject
+
+        expect(response).to have_gitlab_http_status(:ok)
+      end
+    end
+
+    context 'and disabled for project' do
+      let(:expected_response) do
+        "The project you were looking for could not be found or you don't have permission to view it."
+      end
+
+      it 'returns a failed response' do
+        subject
+
+        expect(response).to have_gitlab_http_status(:not_found)
+        expect(response.body).to eq(expected_response)
+      end
+    end
+  end
+
+  context 'GET #git_upload_pack' do
+    subject do
+      get :info_refs, params: { repository_path: repository_path, service: 'git-upload-pack' }
+    end
+
+    it_behaves_like 'a request without write access needed'
+  end
+
+  context 'POST #git_upload_pack' do
+    subject do
+      post :git_upload_pack, params: { repository_path: repository_path }
+    end
+
+    it_behaves_like 'a request without write access needed'
+  end
+
+  context 'GET #git_receive_pack' do
+    subject do
+      get :git_receive_pack, params: { repository_path: repository_path }
+    end
+
+    it_behaves_like 'a request with write access needed'
+  end
+
+  context 'POST #git_receive_pack' do
+    subject do
+      post :git_receive_pack, params: { repository_path: repository_path }
+    end
+
+    it_behaves_like 'a request with write access needed'
   end
 end
