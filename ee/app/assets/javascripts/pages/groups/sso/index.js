85da23c562cc08ea6db09f7ec1d743779e00e034
Dannyel Cardoso da Fonseca <dannyelcf@gmail.com>
2023-10-22 15:05:39 +0000

    Resolve "Mermaid diagrams and charts are not visible in the login page"

--- ee/app/assets/javascripts/pages/groups/sso/index.js
+++ ee/app/assets/javascripts/pages/groups/sso/index.js
@@ -2,9 +2,11 @@ import { initSamlAuthorize, redirectUserWithSSOIdentity } from 'ee/saml_sso';
 import UsernameValidator from '~/pages/sessions/new/username_validator';
 import initConfirmDanger from '~/init_confirm_danger';
 import { initLanguageSwitcher } from '~/language_switcher';
+import { renderGFM } from '~/behaviors/markdown/render_gfm';
 
 new UsernameValidator(); // eslint-disable-line no-new
 initSamlAuthorize();
 redirectUserWithSSOIdentity();
 initConfirmDanger();
 initLanguageSwitcher();
+renderGFM(document.body);
