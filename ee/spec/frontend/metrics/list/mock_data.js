601d1547a4e5a574a26c452fa006422b8e5d71d6
Daniele Rossetti <drossetti@gitlab.com>
2023-10-24 20:09:45 +0000

    Developer Metrics List UI

---
+++ ee/spec/frontend/metrics/list/mock_data.js
@@ -0,0 +1,22 @@
+export const mockMetrics = [
+  {
+    name: 'metric A',
+    description: 'a counter metric called A',
+    type: 'COUNTER',
+  },
+  {
+    name: 'metric B',
+    description: 'a gauge metric called B',
+    type: 'GAUGE',
+  },
+  {
+    name: 'metric C',
+    description: 'a histogram metric called C',
+    type: 'HISTOGRAM',
+  },
+  {
+    name: 'metric D',
+    description: 'a exp histogram metric called D',
+    type: 'EXPONENTIAL HISTOGRAM',
+  },
+];
