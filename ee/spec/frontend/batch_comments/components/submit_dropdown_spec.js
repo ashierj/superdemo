63986323a21d850d15d9c661c72ddd300b49c753
Phil Hughes <me@iamphill.com>
2023-10-24 16:40:13 +0100

    Use GraphQL to fetch canApprove permissions in review dropdown
    This removes a the canApprove query from page load
    and moves it to the place when the user will use it.
    It also fixes a bug where the user could approve after page
    load and then leave a review with the
    approve checkbox being visible.

--- ee/spec/frontend/batch_comments/components/submit_dropdown_spec.js
+++ ee/spec/frontend/batch_comments/components/submit_dropdown_spec.js
@@ -1,17 +1,42 @@
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
 // eslint-disable-next-line no-restricted-imports
 import Vuex from 'vuex';
+import { GlDisclosureDropdown } from '@gitlab/ui';
+import createMockApollo from 'helpers/mock_apollo_helper';
 import { mountExtended } from 'helpers/vue_test_utils_helper';
 import waitForPromises from 'helpers/wait_for_promises';
 import SubmitDropdown from '~/batch_comments/components/submit_dropdown.vue';
+import userCanApproveQuery from '~/batch_comments/queries/can_approve.query.graphql';
 
 Vue.use(Vuex);
+Vue.use(VueApollo);
 
 let wrapper;
 let publishReview;
 
 function factory({ canApprove = true, requirePasswordToApprove = true } = {}) {
   publishReview = jest.fn();
+  const requestHandlers = [
+    [
+      userCanApproveQuery,
+      () =>
+        Promise.resolve({
+          data: {
+            project: {
+              id: 1,
+              mergeRequest: {
+                id: 1,
+                userPermissions: {
+                  canApprove,
+                },
+              },
+            },
+          },
+        }),
+    ],
+  ];
+  const apolloProvider = createMockApollo(requestHandlers);
 
   const store = new Vuex.Store({
     getters: {
@@ -22,14 +47,17 @@ function factory({ canApprove = true, requirePasswordToApprove = true } = {}) {
       getNoteableData: () => ({
         id: 1,
         preview_note_path: '/preview',
-        current_user: {
-          can_approve: canApprove,
-        },
         require_password_to_approve: requirePasswordToApprove,
       }),
       noteableType: () => 'merge_request',
     },
     modules: {
+      diffs: {
+        namespaced: true,
+        state: {
+          projectPath: 'gitlab-org/gitlab',
+        },
+      },
       batchComments: {
         namespaced: true,
         actions: {
@@ -40,6 +68,7 @@ function factory({ canApprove = true, requirePasswordToApprove = true } = {}) {
   });
   wrapper = mountExtended(SubmitDropdown, {
     store,
+    apolloProvider,
   });
 }
 
@@ -52,6 +81,8 @@ describe('Batch comments submit dropdown', () => {
     '$existsText approve password if require_password_to_approve is $requirePasswordToApprove',
     async ({ requirePasswordToApprove, exists }) => {
       factory({ requirePasswordToApprove });
+
+      wrapper.findComponent(GlDisclosureDropdown).vm.$emit('shown');
 
       await waitForPromises();
 
