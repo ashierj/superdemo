a60b0939b51f93e20019603151d1dd81b34d4a67
Ezekiel Kigbo <3397881-ekigbo@users.noreply.gitlab.com>
2023-10-25 13:35:47 +0000

    Add DurationChart and DurationOverviewChart stories
    Adds the cycle analytics duration chart and duration
    overview charts to storybook.
    
    Adds some mock data for use rendering

---
+++ ee/app/assets/javascripts/analytics/cycle_analytics/components/duration_chart.stories.js
@@ -0,0 +1,33 @@
+import { withVuexStore } from 'storybook_addons/vuex_store';
+import DurationChart from './duration_chart.vue';
+import { selectedStageData, selectedStage } from './stories_constants';
+
+export default {
+  component: DurationChart,
+  title: 'ee/analytics/cycle_analytics/components/duration_chart',
+  decorators: [withVuexStore],
+};
+
+const Template = (args, { argTypes, createVuexStore }) => ({
+  components: { DurationChart },
+  props: Object.keys(argTypes),
+  template: '<duration-chart v-bind="$props" />',
+  store: createVuexStore({
+    state: {
+      selectedStage,
+    },
+    modules: {
+      durationChart: {
+        namespaced: true,
+        getters: {
+          durationChartPlottableData: () => selectedStageData,
+        },
+        state: {
+          isLoading: false,
+        },
+      },
+    },
+  }),
+});
+
+export const Default = Template.bind({});
