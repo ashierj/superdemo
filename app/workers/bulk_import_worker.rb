c7d2707346659ce899fca153c2ceeeedaf68eda7
Max Fan <mfan@gitlab.com>
2023-10-25 16:47:35 -0700

    Marking bulk import pipeline workers idempotent

--- app/workers/bulk_import_worker.rb
+++ app/workers/bulk_import_worker.rb
@@ -6,6 +6,7 @@ class BulkImportWorker # rubocop:disable Scalability/IdempotentWorker
   data_consistency :always
   feature_category :importers
   sidekiq_options retry: false, dead: false
+  idempotent!
 
   def perform(bulk_import_id)
     bulk_import = BulkImport.find_by_id(bulk_import_id)
