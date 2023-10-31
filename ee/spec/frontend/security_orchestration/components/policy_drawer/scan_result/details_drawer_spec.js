40e49eebdd8fdf585e0bb042c1fd540785dc666b
Alexander Turinske <aturinske@gitlab.com>
2023-10-31 18:35:03 +0000

    Add policy settings into policy drawer
    - add policy settings at the top of summary
    - hide unknown settings as they would not
      do anything
    - add tests
    
    Changelog: added
    
    EE: true

--- ee/spec/frontend/security_orchestration/components/policy_drawer/scan_result/details_drawer_spec.js
+++ ee/spec/frontend/security_orchestration/components/policy_drawer/scan_result/details_drawer_spec.js
@@ -4,19 +4,22 @@ import BranchExceptionsToggleList from 'ee/security_orchestration/components/pol
 import PolicyDrawerLayout from 'ee/security_orchestration/components/policy_drawer/drawer_layout.vue';
 import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
 import { NAMESPACE_TYPES } from 'ee/security_orchestration/constants';
-import PolicyApprovals from 'ee/security_orchestration/components/policy_drawer/scan_result/policy_approvals.vue';
+import Approvals from 'ee/security_orchestration/components/policy_drawer/scan_result/policy_approvals.vue';
+import Settings from 'ee/security_orchestration/components/policy_drawer/scan_result/policy_settings.vue';
 import {
+  mockProjectScanResultPolicy,
   mockProjectWithAllApproverTypesScanResultPolicy,
-  mockDefaultBranchesScanResultObject,
+  mockApprovalSettingsScanResultPolicy,
 } from '../../../mocks/mock_scan_result_policy_data';
 
 describe('DetailsDrawer component', () => {
   let wrapper;
 
   const findSummary = () => wrapper.findByTestId('policy-summary');
-  const findPolicyApprovals = () => wrapper.findComponent(PolicyApprovals);
+  const findPolicyApprovals = () => wrapper.findComponent(Approvals);
   const findPolicyDrawerLayout = () => wrapper.findComponent(PolicyDrawerLayout);
   const findBranchExceptionsToggleList = () => wrapper.findComponent(BranchExceptionsToggleList);
+  const findSettings = () => wrapper.findComponent(Settings);
 
   const factory = ({ propsData } = {}) => {
     wrapper = shallowMountExtended(DetailsDrawer, {
@@ -28,36 +31,57 @@ describe('DetailsDrawer component', () => {
     });
   };
 
-  describe('default policy', () => {
-    beforeEach(() => {
-      factory({ propsData: { policy: mockProjectWithAllApproverTypesScanResultPolicy } });
+  describe('policy drawer layout props', () => {
+    it('passes the policy to the PolicyDrawerLayout component', () => {
+      factory({ propsData: { policy: mockProjectScanResultPolicy } });
+      expect(findPolicyDrawerLayout().props('policy')).toBe(mockProjectScanResultPolicy);
     });
 
-    it.each`
-      prop             | expected
-      ${'policy'}      | ${mockProjectWithAllApproverTypesScanResultPolicy}
-      ${'description'} | ${mockDefaultBranchesScanResultObject.description}
-    `('passes the correct $prop prop to the PolicyDrawerLayout component', ({ prop, expected }) => {
-      expect(findPolicyDrawerLayout().props(prop)).toBe(expected);
+    it('passes the description to the PolicyDrawerLayout component', () => {
+      factory({ propsData: { policy: mockProjectScanResultPolicy } });
+      expect(findPolicyDrawerLayout().props('description')).toBe(
+        'This policy enforces critical vulnerability CS approvals',
+      );
     });
+  });
 
+  describe('summary', () => {
     it('renders the policy summary', () => {
+      factory({ propsData: { policy: mockProjectScanResultPolicy } });
       expect(findSummary().exists()).toBe(true);
     });
 
-    it('renders the "RequireApproval" component correctly', () => {
-      expect(findPolicyApprovals().exists()).toBe(true);
-      expect(findPolicyApprovals().props('approvers')).toStrictEqual([
-        ...mockProjectWithAllApproverTypesScanResultPolicy.groupApprovers,
-        ...mockProjectWithAllApproverTypesScanResultPolicy.roleApprovers.map((r) =>
-          convertToTitleCase(r.toLowerCase()),
-        ),
-        ...mockProjectWithAllApproverTypesScanResultPolicy.userApprovers,
-      ]);
+    describe('settings', () => {
+      it('passes the settings to the "Settings" component if settings are present', () => {
+        factory({ propsData: { policy: mockApprovalSettingsScanResultPolicy } });
+        expect(findSettings().props('settings')).toEqual(
+          mockApprovalSettingsScanResultPolicy.approval_settings,
+        );
+      });
+
+      it('passes the empty object to the "Settings" component if no settings are present', () => {
+        factory({ propsData: { policy: mockProjectScanResultPolicy } });
+        expect(findSettings().props('settings')).toEqual({});
+      });
     });
 
-    it('should not render branch exceptions list without exceptions', () => {
-      expect(findBranchExceptionsToggleList().exists()).toBe(false);
+    describe('approvals', () => {
+      it('renders the "Approvals" component correctly', () => {
+        factory({ propsData: { policy: mockProjectWithAllApproverTypesScanResultPolicy } });
+        expect(findPolicyApprovals().exists()).toBe(true);
+        expect(findPolicyApprovals().props('approvers')).toStrictEqual([
+          ...mockProjectWithAllApproverTypesScanResultPolicy.allGroupApprovers,
+          ...mockProjectWithAllApproverTypesScanResultPolicy.roleApprovers.map((r) =>
+            convertToTitleCase(r.toLowerCase()),
+          ),
+          ...mockProjectWithAllApproverTypesScanResultPolicy.userApprovers,
+        ]);
+      });
+
+      it('should not render branch exceptions list without exceptions', () => {
+        factory({ propsData: { policy: mockProjectWithAllApproverTypesScanResultPolicy } });
+        expect(findBranchExceptionsToggleList().exists()).toBe(false);
+      });
     });
   });
 });
