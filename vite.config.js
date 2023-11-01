ce2ee7c0dae4e9ca1c10d6e9d08d4e3cd42644c7
Stanislav Lashmanov <slashmanov@gitlab.com>
2023-11-01 10:08:44 +0400

    Fix Katex styles import for Vite

--- vite.config.js
+++ vite.config.js
@@ -69,6 +69,10 @@ export default defineConfig({
         find: '~/',
         replacement: javascriptsPath,
       },
+      {
+        find: '~katex',
+        replacement: 'katex',
+      },
     ],
   },
   plugins: [
