0b490bdbda06febdda47b2e0e0cea1d9e52dd381
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2023-11-01 15:57:45 -0300

    Create group wiki repo if absent when verifying on primary
    Changelog: fixed
    EE: true

--- ee/spec/replicators/geo/group_wiki_repository_replicator_spec.rb
+++ ee/spec/replicators/geo/group_wiki_repository_replicator_spec.rb
@@ -6,9 +6,34 @@ RSpec.describe Geo::GroupWikiRepositoryReplicator, feature_category: :geo_replic
   let(:model_record) { build(:group_wiki_repository, group: create(:group)) }
 
   include_examples 'a repository replicator' do
-    def handle_model_record_before_verification_integration_examples
-      model_record.save!
-      model_record.repository.create_if_not_exists
+    describe '#verify' do
+      context 'when wiki git repository does not exist' do
+        it 'creates an empty git repository' do
+          expect { replicator.verify }
+            .to change { model_record.repository.exists? }
+            .from(false)
+            .to(true)
+
+          expect(replicator.primary_checksum).to be_present
+        end
+
+        it 'logs an error message' do
+          message =
+            'Git repository of group wiki was not found. To avoid verification error, creating empty Git repository'
+
+          expect(Gitlab::Geo::Logger)
+            .to receive(:error)
+            .with(
+              hash_including(
+                message: message,
+                group_wiki_repository_id: model_record.id,
+                group_id: model_record.group_id
+              )
+            )
+
+          replicator.verify
+        end
+      end
     end
   end
 
