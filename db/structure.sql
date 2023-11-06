2418106fa6c7c9bc58f5e5210d555c6dfbf100c3
Dennis Field <dennisf@radiosound.com>
2023-11-06 10:41:33 -0500

    Add setting for external redirect page for job artifacts
    Create a new migration in application_settings and add setting to API
    and helper
    
    Modify controller to skip linking to external redirect page if setting
    is disabled (and add corresponding test)
    
    Add administration documentation for the new setting
    
    Changelog: changed

--- db/structure.sql
+++ db/structure.sql
@@ -11868,6 +11868,7 @@ CREATE TABLE application_settings (
     project_jobs_api_rate_limit integer DEFAULT 600 NOT NULL,
     math_rendering_limits_enabled boolean DEFAULT true NOT NULL,
     service_access_tokens_expiration_enforced boolean DEFAULT true NOT NULL,
+    enable_artifact_external_redirect_warning_page boolean DEFAULT true NOT NULL,
     CONSTRAINT app_settings_container_reg_cleanup_tags_max_list_size_positive CHECK ((container_registry_cleanup_tags_service_max_list_size >= 0)),
     CONSTRAINT app_settings_container_registry_pre_import_tags_rate_positive CHECK ((container_registry_pre_import_tags_rate >= (0)::numeric)),
     CONSTRAINT app_settings_dep_proxy_ttl_policies_worker_capacity_positive CHECK ((dependency_proxy_ttl_group_policy_worker_capacity >= 0)),
