977fb95ae9eb2ca3aa2535f5c59c17b58c32f209
Ahmed Hemdan <ahemdan@gitlab.com>
2023-11-08 13:31:33 +0000

    Make secrets check an ultimate feature

--- ee/app/models/gitlab_subscriptions/features.rb
+++ ee/app/models/gitlab_subscriptions/features.rb
@@ -222,6 +222,7 @@ module GitlabSubscriptions
       license_scanning
       okrs
       personal_access_token_expiration_policy
+      pre_receive_secret_detection
       product_analytics
       project_quality_summary
       project_level_analytics_dashboard
--- ee/lib/ee/gitlab/checks/push_rules/secrets_check.rb
+++ ee/lib/ee/gitlab/checks/push_rules/secrets_check.rb
@@ -8,8 +8,11 @@ module EE
           def validate!
             # Return early and not perform the check if:
             #   1. no push rule exist
-            #   2. feature flag is disabled
-            return unless push_rule && ::Feature.enabled?(:pre_receive_secret_detection_push_check, push_rule.project)
+            #   2. and license is not ultimate
+            #   3. and feature flag is disabled
+            return unless push_rule &&
+              push_rule.project.licensed_feature_available?(:pre_receive_secret_detection) &&
+              ::Feature.enabled?(:pre_receive_secret_detection_push_check, push_rule.project)
           end
         end
       end
