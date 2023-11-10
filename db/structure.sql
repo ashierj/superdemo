12b3e949d38c7551ec8862376fd26f7e3b59b431
Aboobacker MK <akarakath@gitlab.com>
2023-11-10 07:06:49 +0000

    Save the timestamp of the last project authorization recalculation
    Save the timestamp of the last project authorization recalculation so
    that it can be used for debugging purposes.
    
    Changelog: added

--- db/structure.sql
+++ db/structure.sql
@@ -24347,6 +24347,7 @@ CREATE TABLE user_details (
     enterprise_group_associated_at timestamp with time zone,
     email_reset_offered_at timestamp with time zone,
     mastodon text DEFAULT ''::text NOT NULL,
+    project_authorizations_recalculated_at timestamp with time zone DEFAULT '2010-01-01 00:00:00+00'::timestamp with time zone NOT NULL,
     CONSTRAINT check_245664af82 CHECK ((char_length(webauthn_xid) <= 100)),
     CONSTRAINT check_444573ee52 CHECK ((char_length(skype) <= 500)),
     CONSTRAINT check_466a25be35 CHECK ((char_length(twitter) <= 500)),
