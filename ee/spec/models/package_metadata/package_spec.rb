08c28f020f5638e92633dd24b1f1e98c7ffe536f
Adam Cohen <acohen@gitlab.com>
2023-11-11 05:51:51 +0000

    Remove duplicated purl_types in specs

--- ee/spec/models/package_metadata/package_spec.rb
+++ ee/spec/models/package_metadata/package_spec.rb
@@ -5,26 +5,8 @@ require 'spec_helper'
 RSpec.describe PackageMetadata::Package, type: :model, feature_category: :software_composition_analysis do
   using RSpec::Parameterized::TableSyntax
 
-  let(:purl_types) do
-    {
-      composer: 1,
-      conan: 2,
-      gem: 3,
-      golang: 4,
-      maven: 5,
-      npm: 6,
-      nuget: 7,
-      pypi: 8,
-      apk: 9,
-      rpm: 10,
-      deb: 11,
-      'cbl-mariner': 12,
-      wolfi: 13
-    }
-  end
-
   describe 'enums' do
-    it { is_expected.to define_enum_for(:purl_type).with_values(purl_types) }
+    it_behaves_like 'purl_types enum'
   end
 
   describe '#license_ids_for' do
