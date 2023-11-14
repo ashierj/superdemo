4853bc49fe6e30bb4088ff95f3e486ae9c3fd9ef
Alper Akgun <aakgun@gitlab.com>
2023-11-14 16:50:24 +0300

    Default_resources_per_workspace_container, max_resources_per_workspace
    Add default_resources_per_workspace_container,
    max_resources_per_workspace to remote_development_agent_configs table
    
    Changelog: added

--- db/structure.sql
+++ db/structure.sql
@@ -22593,6 +22593,8 @@ CREATE TABLE remote_development_agent_configs (
     network_policy_enabled boolean DEFAULT true NOT NULL,
     gitlab_workspaces_proxy_namespace text DEFAULT 'gitlab-workspaces'::text NOT NULL,
     network_policy_egress jsonb DEFAULT '[{"allow": "0.0.0.0/0", "except": ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]}]'::jsonb NOT NULL,
+    default_resources_per_workspace_container jsonb DEFAULT '{}'::jsonb NOT NULL,
+    max_resources_per_workspace jsonb DEFAULT '{}'::jsonb NOT NULL,
     CONSTRAINT check_72947a4495 CHECK ((char_length(gitlab_workspaces_proxy_namespace) <= 63)),
     CONSTRAINT check_9f5cd54d1c CHECK ((char_length(dns_zone) <= 256))
 );
