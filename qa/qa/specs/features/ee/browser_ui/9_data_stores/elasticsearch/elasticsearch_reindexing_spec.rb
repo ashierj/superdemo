12b272940c5a8bab1d5156d39f16f9bbf06b9025
Mark Lapierre <mlapierre@gitlab.com>
2023-11-14 09:04:22 +0000

    Fix elasticsearch test
    - removes newline from expected file path
    - fixes some styling that inconsistent with the rest of the test code

--- qa/qa/specs/features/ee/browser_ui/9_data_stores/elasticsearch/elasticsearch_reindexing_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/9_data_stores/elasticsearch/elasticsearch_reindexing_spec.rb
@@ -40,18 +40,20 @@ module QA
         testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/348040'
       ) do
         expect { Runtime::Search.find_code(project_file_name, project_file_content) }.not_to raise_error
+
         Page::Main::Menu.perform(&:go_to_groups)
 
         QA::Page::Main::Menu.perform do |menu|
           menu.search_for(project_file_content)
         end
 
-        file_path = "#{project.group.sandbox.path} /
-        #{project.group.path} / #{project.name}: #{project_file_name}"
+        file_path = "#{project.group.sandbox.path} / " \
+                    "#{project.group.path} / #{project.name}: #{project_file_name}"
 
         Page::Search::Results.perform do |search|
           search.switch_to_code
-          expect(search).to have_file_in_project_with_content project_file_content, file_path
+
+          expect(search).to have_file_in_project_with_content(project_file_content, file_path)
         end
       end
 
@@ -68,33 +70,34 @@ module QA
         Page::Project::Show.perform(&:open_web_ide!)
         Page::Project::WebIDE::VSCode.perform do |ide|
           ide.wait_for_ide_to_load
-          ide.create_new_file_from_template template[:file_name], template[:name]
+          ide.create_new_file_from_template(template[:file_name], template[:name])
           ide.wait_for_ide_to_load
-          ide.commit_toggle template[:file_name]
+          ide.commit_toggle(template[:file_name])
           ide.push_to_existing_branch
           ide.wait_for_ide_to_load
           ide.switch_to_original_window
         end
+
         expect { Runtime::Search.find_code(template[:file_name], template[:name]) }.not_to raise_error
+
         Page::Main::Menu.perform(&:go_to_groups)
 
-        QA::Support::Retrier
-          .retry_on_exception(
-            max_attempts: Runtime::Search::RETRY_MAX_ITERATION, sleep_interval: Runtime::Search::RETRY_SLEEP_INTERVAL
-          ) do
+        QA::Support::Retrier.retry_on_exception(
+          max_attempts: Runtime::Search::RETRY_MAX_ITERATION, sleep_interval: Runtime::Search::RETRY_SLEEP_INTERVAL
+        ) do
           QA::Page::Main::Menu.perform do |menu|
-            menu.search_for template[:name]
+            menu.search_for(template[:name])
           end
           Page::Search::Results.perform do |search|
             search.switch_to_code
 
-            file_path = "#{project.group.sandbox.path} /
-            #{project.group.path} / #{project.name}: #{template[:file_name]}"
+            file_path = "#{project.group.sandbox.path} / " \
+                        "#{project.group.path} / #{project.name}: #{template[:file_name]}"
 
             aggregate_failures "testing expectations" do
-              expect(search).to have_project_in_search_result project.name
-              expect(search).to have_file_in_project template[:file_name], project.name
-              expect(search).to have_file_in_project_with_content template[:name], file_path
+              expect(search).to have_project_in_search_result(project.name)
+              expect(search).to have_file_in_project(template[:file_name], project.name)
+              expect(search).to have_file_in_project_with_content(template[:name], file_path)
             end
           end
         end
