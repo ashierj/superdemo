bbb51d90c3d3ff4cf2c8f7c21f6990ffaeb6592b
Samantha Ming <sming@gitlab.com>
2023-11-15 18:10:12 +0100

    Refactor spec file for split button
    Refactor structure so all the render tests are together.
    Fix a vue warning error.
    Simplify the createComponent step by passing a default button prop.

--- ee/spec/frontend/vue_shared/security_reports/components/split_button_spec.js
+++ ee/spec/frontend/vue_shared/security_reports/components/split_button_spec.js
@@ -1,23 +1,25 @@
 import { nextTick } from 'vue';
 import { GlButton, GlButtonGroup, GlCollapsibleListbox, GlListboxItem } from '@gitlab/ui';
-import { shallowMount, mount } from '@vue/test-utils';
+import { shallowMountExtended, mountExtended } from 'helpers/vue_test_utils_helper';
 import SplitButton from 'ee/vue_shared/security_reports/components/split_button.vue';
 import * as urlUtility from '~/lib/utils/url_utility';
 
-const buttons = [
-  {
-    name: 'button one',
-    tagline: "button one's tagline",
-    isLoading: false,
-    action: 'button1Action',
-  },
-  {
-    name: 'button two',
-    tagline: "button two's tagline",
-    isLoading: false,
-    action: 'button2Action',
-  },
-];
+const defaultProps = {
+  buttons: [
+    {
+      name: 'button one',
+      tagline: "button one's tagline",
+      isLoading: false,
+      action: 'button1Action',
+    },
+    {
+      name: 'button two',
+      tagline: "button two's tagline",
+      isLoading: false,
+      action: 'button2Action',
+    },
+  ],
+};
 
 describe('Split Button', () => {
   let wrapper;
@@ -27,42 +29,53 @@ describe('Split Button', () => {
   const findListbox = () => wrapper.findComponent(GlCollapsibleListbox);
   const findListboxItem = () => wrapper.findComponent(GlListboxItem);
 
-  const createComponent = (props, mountFn = shallowMount) => {
+  const createComponent = (props, mountFn = shallowMountExtended) => {
     wrapper = mountFn(SplitButton, {
       propsData: {
+        ...defaultProps,
         ...props,
       },
     });
   };
 
   it('does not render button group if buttons array is empty', () => {
-    createComponent({
-      buttons: [],
-    });
+    createComponent({ buttons: [] });
 
     expect(findButtonGroup().exists()).toBe(false);
   });
 
   it('renders disabled listbox and disabled button if disabled prop is true', () => {
-    createComponent({
-      buttons: buttons.slice(0),
-      disabled: true,
-    });
+    createComponent({ disabled: true });
 
     expect(findButton().attributes('disabled')).toBe('true');
     expect(findListbox().attributes('disabled')).toBe('true');
   });
 
   it('renders a loading icon in button and disables listbox when loading prop is true', () => {
-    createComponent({ buttons: buttons.slice(0).map((b) => ({ ...b, loading: true })) });
+    const { buttons } = defaultProps;
+
+    createComponent({ buttons: [...buttons].map((b) => ({ ...b, loading: true })) });
+
     expect(findButton().props('loading')).toBe(true);
     expect(findListbox().props('disabled')).toBe(true);
   });
 
+  it('renders a correct amount of listbox items', () => {
+    createComponent();
+
+    expect(findListbox().props('items')).toHaveLength(2);
+  });
+
+  it('renders both button text and tagline', () => {
+    createComponent({}, mountExtended);
+
+    const item = findListboxItem();
+    expect(item.text()).toContain('button one');
+    expect(item.text()).toContain("button one's tagline");
+  });
+
   it('emits correct action on button click', () => {
-    createComponent({
-      buttons: buttons.slice(0),
-    });
+    createComponent({}, mountExtended);
 
     findButton().vm.$emit('click');
 
@@ -73,10 +86,9 @@ describe('Split Button', () => {
   it('visits url if href property is specified', () => {
     const spy = jest.spyOn(urlUtility, 'visitUrl').mockReturnValue({});
     const href = 'https://gitlab.com';
+    const { buttons } = defaultProps;
 
-    createComponent({
-      buttons: [{ ...buttons.slice(0), action: undefined, href }],
-    });
+    createComponent({ buttons: [{ ...buttons[0], href }] });
 
     findButton().vm.$emit('click');
 
@@ -84,29 +96,8 @@ describe('Split Button', () => {
     expect(spy).toHaveBeenCalledWith(href, true);
   });
 
-  it('renders a correct amount of listbox items', () => {
-    createComponent({
-      buttons,
-    });
-
-    expect(findListbox().props('items')).toHaveLength(2);
-  });
-
-  it('renders both button text and tagline', () => {
-    createComponent(
-      {
-        buttons: buttons.slice(0),
-      },
-      mount,
-    );
-    const item = findListboxItem();
-
-    expect(item.text()).toContain('button one');
-    expect(item.text()).toContain("button one's tagline");
-  });
-
   it('updates selected item', async () => {
-    createComponent({ buttons });
+    createComponent();
 
     findListbox().vm.$emit('select', 'button2Action');
     await nextTick();
