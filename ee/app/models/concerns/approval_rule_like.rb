5b124c05ab41720bd03abc2e7dfff5c431c3e3d9
Igor Drozdov <idrozdov@gitlab.com>
2023-11-17 08:18:01 +0100

    Disable joins for approval rules group users
    EE: true
    Changelog: performance

--- ee/app/models/concerns/approval_rule_like.rb
+++ ee/app/models/concerns/approval_rule_like.rb
@@ -24,9 +24,7 @@ module ApprovalRuleLike
       after_add: :audit_add, after_remove: :audit_remove
 
     has_many :group_members, through: :groups
-    has_many :group_users, -> {
-      distinct.allow_cross_joins_across_databases(url: "https://gitlab.com/gitlab-org/gitlab/-/issues/417457")
-    }, through: :groups, source: :users, disable_joins: -> { Feature.enabled?(:approval_rules_disable_joins) }
+    has_many :group_users, -> { distinct }, through: :groups, source: :users, disable_joins: true
 
     belongs_to :security_orchestration_policy_configuration, class_name: 'Security::OrchestrationPolicyConfiguration', optional: true
     belongs_to :scan_result_policy_read,
