1356d54f60fd0b61471c181080b6dc4b1c0c4425
Alper Akgun <aakgun@gitlab.com>
2023-11-22 08:50:07 +0000

    Breadcrumbs navigation in the Workspaces UI
    Changelog: added

---
+++ ee/app/assets/javascripts/remote_development/components/common/workspaces_breadcrumbs.vue
@@ -0,0 +1,39 @@
+<script>
+import { GlBreadcrumb } from '@gitlab/ui';
+import { s__ } from '~/locale';
+import { ROUTES } from '../../constants';
+
+export const BREADCRUMB_LABELS = {
+  index: s__('Workspaces|Workspaces'),
+  new: s__('Workspaces|Create a new workspace'),
+};
+
+export default {
+  components: {
+    GlBreadcrumb,
+  },
+  computed: {
+    crumbs() {
+      const crumbs = [
+        {
+          text: BREADCRUMB_LABELS[ROUTES.index],
+          to: ROUTES.index,
+        },
+      ];
+
+      if (this.$route.name === ROUTES.new) {
+        crumbs.push({
+          text: BREADCRUMB_LABELS[ROUTES.new],
+          to: ROUTES.new,
+        });
+      }
+
+      return crumbs;
+    },
+  },
+};
+</script>
+
+<template>
+  <gl-breadcrumb :items="crumbs" />
+</template>
