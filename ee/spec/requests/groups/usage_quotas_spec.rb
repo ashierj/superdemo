34701295ca11dbd42ab57eeb898ddce1e62cc5a0
Angelo Gulina <agulina@gitlab.com>
2023-11-24 08:02:01 +0000

    Create enable_add_on_users_filtering

--- ee/spec/requests/groups/usage_quotas_spec.rb
+++ ee/spec/requests/groups/usage_quotas_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe 'view usage quotas', feature_category: :consumables_cost_management do
   describe 'GET /groups/:group/-/usage_quotas' do
-    subject { get group_usage_quotas_path(namespace) }
+    subject(:request) { get group_usage_quotas_path(namespace) }
 
     let_it_be(:namespace) { create(:group) }
     let_it_be(:user) { create(:user) }
@@ -19,6 +19,26 @@ RSpec.describe 'view usage quotas', feature_category: :consumables_cost_manageme
 
     context 'when storage size is over limit' do
       it_behaves_like 'namespace storage limit alert'
+    end
+
+    context 'with enable_add_on_users_filtering enabled' do
+      it 'exposes the feature flags' do
+        request
+
+        expect(response.body).to have_pushed_frontend_feature_flags(enableAddOnUsersFiltering: true)
+      end
+    end
+
+    context 'with enable_add_on_users_filtering disabled' do
+      before do
+        stub_feature_flags(enable_add_on_users_filtering: false)
+      end
+
+      it 'does not expose feature flags' do
+        request
+
+        expect(response.body).not_to have_pushed_frontend_feature_flags(enableAddOnUsersFiltering: true)
+      end
     end
   end
 end
