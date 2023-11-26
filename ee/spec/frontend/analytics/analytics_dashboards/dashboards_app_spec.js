6baeaff4d50a0a12b3043c307833533b50727e99
Artur Fedorov <afedorov@gitlab.com>
2023-11-27 00:36:18 +0100

    This MR removed double component registration
    As part of VUE3 migration extra component
    registration has to be removed

--- ee/spec/frontend/analytics/analytics_dashboards/dashboards_app_spec.js
+++ ee/spec/frontend/analytics/analytics_dashboards/dashboards_app_spec.js
@@ -1,35 +1,21 @@
 import { shallowMount } from '@vue/test-utils';
-import VueRouter from 'vue-router';
-import Vue from 'vue';
 import AnalyticsDashboardsApp from 'ee/analytics/analytics_dashboards/dashboards_app.vue';
 import createRouter from 'ee/analytics/analytics_dashboards/router';
 
 describe('AnalyticsDashboardsApp', () => {
-  /** @type {import('helpers/vue_test_utils_helper').ExtendedWrapper} */
   let wrapper;
-  let router;
-
-  Vue.use(VueRouter);
 
   const findRouterView = () => wrapper.findComponent({ ref: 'router-view' });
 
   const createWrapper = () => {
-    router = createRouter();
-
     wrapper = shallowMount(AnalyticsDashboardsApp, {
-      router,
-      stubs: {
-        RouterView: true,
-      },
+      router: createRouter(),
     });
   };
 
   describe('when mounted', () => {
-    beforeEach(() => {
-      createWrapper();
-    });
-
     it('should render', () => {
+      createWrapper();
       expect(findRouterView().exists()).toBe(true);
     });
   });
