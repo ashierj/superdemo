6662f491ccc88875d3ebae68358440f5dbe41b8b
Niklas <mc.taucher2003@gmail.com>
2023-11-27 20:21:28 +0000

    Add migration to fix broken user achievement records
    Changelog: fixed

---
+++ db/post_migrate/20231127185327_fix_broken_user_achievements.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class FixBrokenUserAchievements < Gitlab::Database::Migration[2.2]
+  disable_ddl_transaction!
+  restrict_gitlab_migration gitlab_schema: :gitlab_main
+
+  milestone '16.7'
+
+  def up
+    update_column_in_batches(:user_achievements, :awarded_by_user_id, Users::Internal.ghost.id) do |table, query|
+      query.where(table[:awarded_by_user_id].eq(nil))
+    end
+
+    update_column_in_batches(:user_achievements, :revoked_by_user_id, Users::Internal.ghost.id) do |table, query|
+      query.where(table[:revoked_at].not_eq(nil)).where(table[:revoked_by_user_id].eq(nil))
+    end
+  end
+
+  def down
+    # noop -- this is a data migration and can't be reversed
+  end
+end
