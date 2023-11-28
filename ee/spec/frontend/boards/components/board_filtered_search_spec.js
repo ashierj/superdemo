d304c8e13c23b0c1f54fb28019fab0d56ecea2d6
Florie Guibert <fguibert@gitlab.com>
2023-11-28 10:49:24 +1000

    Clean up :apollo_boards feature flag
    Part 1

--- ee/spec/frontend/boards/components/board_filtered_search_spec.js
+++ ee/spec/frontend/boards/components/board_filtered_search_spec.js
@@ -1,14 +1,10 @@
-import Vue, { nextTick } from 'vue';
-// eslint-disable-next-line no-restricted-imports
-import Vuex from 'vuex';
+import { nextTick } from 'vue';
+
 import { mountExtended } from 'helpers/vue_test_utils_helper';
 import { stubComponent } from 'helpers/stub_component';
 import BoardFilteredSearch from 'ee/boards/components/board_filtered_search.vue';
 import BoardFilteredSearchCe from '~/boards/components/board_filtered_search.vue';
-import { createStore } from '~/boards/stores';
 import * as urlUtility from '~/lib/utils/url_utility';
-
-Vue.use(Vuex);
 
 describe('ee/BoardFilteredSearch', () => {
   let wrapper;
@@ -38,58 +34,25 @@ describe('ee/BoardFilteredSearch', () => {
 
   const findFilteredSearch = () => wrapper.findComponent(BoardFilteredSearchCe);
 
-  describe('when boardScopeConfig watcher is triggered', () => {
-    beforeEach(async () => {
-      store = createStore();
+  beforeEach(async () => {
+    createComponent();
 
-      createComponent();
+    jest.spyOn(urlUtility, 'updateHistory');
 
-      jest.spyOn(store, 'dispatch').mockImplementation();
-      jest.spyOn(urlUtility, 'updateHistory');
-
-      store.state.boardConfig = { labels: [{ title: 'test', color: 'black', id: '1' }] };
-
-      await nextTick();
+    wrapper.setProps({
+      board: { labels: [{ title: 'test', color: 'black', id: '1' }] },
     });
-
-    it('calls performSearch', () => {
-      expect(store.dispatch).toHaveBeenCalledWith('performSearch');
-    });
-
-    it('calls historyPushState', () => {
-      expect(urlUtility.updateHistory).toHaveBeenCalledWith({
-        url: '?label_name[]=test',
-      });
-    });
-
-    it('passes the correct props to BoardFilteredSearchCe', () => {
-      expect(findFilteredSearch().props()).toEqual(
-        expect.objectContaining({ eeFilters: { labelName: ['test'] } }),
-      );
-    });
+    await nextTick();
   });
 
-  describe('when Apollo boards FF is on', () => {
-    beforeEach(async () => {
-      createComponent({ provide: { isApolloBoard: true } });
-
-      jest.spyOn(urlUtility, 'updateHistory');
-
-      wrapper.setProps({
-        board: { labels: [{ title: 'test', color: 'black', id: '1' }] },
-      });
-      await nextTick();
+  it('updates url and tokens when board watcher is triggered', () => {
+    expect(urlUtility.updateHistory).toHaveBeenCalledWith({
+      url: '?label_name[]=test',
     });
 
-    it('updates url and tokens when board watcher is triggered', () => {
-      expect(urlUtility.updateHistory).toHaveBeenCalledWith({
-        url: '?label_name[]=test',
-      });
-
-      expect(findFilteredSearch().props()).toEqual(
-        expect.objectContaining({ eeFilters: { labelName: ['test'] } }),
-      );
-      expect(updateTokensSpy).toHaveBeenCalled();
-    });
+    expect(findFilteredSearch().props()).toEqual(
+      expect.objectContaining({ eeFilters: { labelName: ['test'] } }),
+    );
+    expect(updateTokensSpy).toHaveBeenCalled();
   });
 });
