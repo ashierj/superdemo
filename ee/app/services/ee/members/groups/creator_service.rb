6249974e52671ec603fe7bbdb4f7730a61c9c2c6
Jarka Košanová <jarka@gitlab.com>
2023-11-28 17:06:59 +0000

    Validate adding members with higher role
    Merge branch 'security-fix-admin-member-perm' into 'master'
    
    See merge request gitlab-org/security/gitlab!3694
    
    Changelog: security

--- ee/app/services/ee/members/groups/creator_service.rb
+++ ee/app/services/ee/members/groups/creator_service.rb
@@ -37,6 +37,22 @@ module EE
         def member_role_id
           args[:member_role_id]
         end
+
+        override :member_role_too_high?
+        def member_role_too_high?
+          return false if skip_authorization?
+
+          user_role = max_role
+
+          return false if current_user.can_admin_all_resources?
+          return false unless member_attributes[:access_level]
+
+          member_attributes[:access_level] > user_role
+        end
+
+        def max_role
+          member.group.highest_group_member(current_user)&.access_level
+        end
       end
     end
   end
