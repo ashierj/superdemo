b0cf61131f21381978509ab2698b9da57522e726
Martin Čavoj <mcavoj@gitlab.com>
2023-11-29 16:12:59 +0000

    Treat security policy bots as external
    Merge branch 'security-fix-external-user-access-to-internal-projects-16-4' into '16-4-stable-ee'
    
    See merge request gitlab-org/security/gitlab!3677
    
    Changelog: security

--- ee/app/models/ee/user.rb
+++ ee/app/models/ee/user.rb
@@ -231,6 +231,12 @@ module EE
       end
     end
 
+    def external?
+      return true if security_policy_bot?
+
+      read_attribute(:external)
+    end
+
     def cannot_be_admin_and_auditor
       if admin? && auditor?
         errors.add(:admin, 'user cannot also be an Auditor.')
