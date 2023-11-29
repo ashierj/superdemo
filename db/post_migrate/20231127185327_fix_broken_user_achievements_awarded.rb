b8f6dc1de04147da80a14d9f170fde24b1f39464
Niklas <mc.taucher2003@gmail.com>
2023-11-29 09:22:43 +0000

    Split migration into two migrations

---
+++ db/post_migrate/20231127185327_fix_broken_user_achievements_awarded.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class FixBrokenUserAchievementsAwarded < Gitlab::Database::Migration[2.2]
+  disable_ddl_transaction!
+  restrict_gitlab_migration gitlab_schema: :gitlab_main
+
+  milestone '16.7'
+
+  def up
+    update_column_in_batches(:user_achievements, :awarded_by_user_id, Users::Internal.ghost.id) do |table, query|
+      query.where(table[:awarded_by_user_id].eq(nil))
+    end
+  end
+
+  def down
+    # noop -- this is a data migration and can't be reversed
+  end
+end
