7aa60a44e91eb6f85051c39f809f2f2f6f50dc98
Krasimir Angelov <kangelov@gitlab.com>
2023-11-30 16:02:58 +1300

    Remove column ignores after bigint conversion for notes
    Remove the column ignore rules for the following columns:
    
    * notes.id
    * todos.note_id
    * system_note_metadata.note_id
    * epic_user_mentions.note_id
    * suggestions.note_id
    * issue_user_mentions.note_id
    * note_diff_files.diff_note_id
    * snippet_user_mentions.note_id
    * design_user_mentions.note_id
    * vulnerability_user_mentions.note_id
    * commit_user_mentions.note_id
    * merge_request_user_mentions.note_id
    * timelogs.note_id
    * award_emoji.awardable_id
    * events.target_id
    
    https://gitlab.com/gitlab-org/gitlab/-/issues/433208

--- ee/app/models/epic_user_mention.rb
+++ ee/app/models/epic_user_mention.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class EpicUserMention < UserMention
-  include IgnorableColumns
-
-  ignore_column :note_id_convert_to_bigint, remove_with: '16.7', remove_after: '2023-11-16'
-
   belongs_to :epic
   belongs_to :note
 end
