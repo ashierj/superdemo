abb3896d03975cf4e04e8c1da52bd554f37a9993
Fabien Catteau <fcatteau@gitlab.com>
2023-12-06 06:36:56 +0000

    Remove flag that enables CVS GA
    Remove the global_dependency_scanning_on_advisory_ingestion flag.
    As a result, Continuous Vulnerability Scanning (CVS)
    for Dependency Scanning always runs globally,
    and the opt-in behavior introduced when the feature
    was experimental is no longer available.
    
    Changelog: changed
    EE: true

--- ee/spec/services/package_metadata/advisory_scan_service_spec.rb
+++ ee/spec/services/package_metadata/advisory_scan_service_spec.rb
@@ -5,13 +5,12 @@ require 'spec_helper'
 RSpec.describe PackageMetadata::AdvisoryScanService, feature_category: :software_composition_analysis do
   describe '.execute' do
     let(:advisory) { build(:pm_advisory) }
-    let(:global) { false }
 
     it 'calls the advisory scanner execute method' do
       expect(::Gitlab::VulnerabilityScanning::AdvisoryScanner).to receive(:scan_projects_for)
-      .with(advisory, global: global)
+      .with(advisory)
 
-      described_class.execute(advisory, global: global)
+      described_class.execute(advisory)
     end
   end
 end
