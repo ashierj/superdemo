12967a0906ad266b98aed7b2cf59379146bca337
Bala Kumar <sbalakumar@gitlab.com>
2023-12-09 18:58:28 +0530

    Update ingestion logic for vulnerability reads owasp top 10
    Changelog: added
    EE: true

--- ee/spec/services/security/ingestion/ingest_report_slice_service_spec.rb
+++ ee/spec/services/security/ingestion/ingest_report_slice_service_spec.rb
@@ -33,6 +33,7 @@ RSpec.describe Security::Ingestion::IngestReportSliceService, feature_category: 
       expect(Security::Ingestion::Tasks::IngestFindingSignatures).to have_received(:execute).ordered.with(pipeline, finding_maps)
       expect(Security::Ingestion::Tasks::IngestFindingEvidence).to have_received(:execute).ordered.with(pipeline, finding_maps)
       expect(Security::Ingestion::Tasks::IngestVulnerabilityFlags).to have_received(:execute).ordered.with(pipeline, finding_maps)
+      expect(Security::Ingestion::Tasks::IngestVulnerabilityReads).to have_received(:execute).ordered.with(pipeline, finding_maps)
       expect(Security::Ingestion::Tasks::IngestVulnerabilityStatistics).to have_received(:execute).ordered.with(pipeline, finding_maps)
       expect(Security::Ingestion::Tasks::IngestRemediations).to have_received(:execute).ordered.with(pipeline, finding_maps)
       expect(Security::Ingestion::Tasks::HooksExecution).to have_received(:execute).ordered.with(pipeline, finding_maps)
