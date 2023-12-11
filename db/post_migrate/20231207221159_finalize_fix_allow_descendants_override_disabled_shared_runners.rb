81890e7c05bcc81f0de1c3535b07323197bcb2bc
Dylan Griffith <dyl.griffith@gmail.com>
2023-12-11 07:51:54 +0000

    Fix gitlab_schema: :gitlab_main
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/139353

--- db/post_migrate/20231207221159_finalize_fix_allow_descendants_override_disabled_shared_runners.rb
+++ db/post_migrate/20231207221159_finalize_fix_allow_descendants_override_disabled_shared_runners.rb
@@ -5,7 +5,7 @@ class FinalizeFixAllowDescendantsOverrideDisabledSharedRunners < Gitlab::Databas
 
   disable_ddl_transaction!
 
-  restrict_gitlab_migration gitlab_schema: :gitlab_main_cell
+  restrict_gitlab_migration gitlab_schema: :gitlab_main
 
   def up
     ensure_batched_background_migration_is_finished(
