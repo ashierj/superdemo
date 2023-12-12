67b452e8dacb3031e88e5fc67985311f7005b74e
Michael Kozono <mkozono@gitlab.com>
2023-12-12 01:30:05 +0000

    Fix Rubocop offenses in blob_downloader_spec.rb

--- ee/spec/lib/gitlab/geo/replication/blob_downloader_spec.rb
+++ ee/spec/lib/gitlab/geo/replication/blob_downloader_spec.rb
@@ -11,29 +11,30 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
   let(:model_record) { create(:package_file, :npm) }
   let(:replicator) { model_record.replicator }
 
-  subject { described_class.new(replicator: replicator) }
+  subject(:downloader) { described_class.new(replicator: replicator) }
 
   describe '#execute' do
     before do
       stub_current_geo_node(secondary)
     end
 
-    context 'precondition failures' do
-      context 'not a Geo secondary' do
+    describe 'preconditions' do
+      context 'when not a Geo secondary' do
         it 'returns failure' do
           stub_current_geo_node(primary)
+          stub_primary_node
 
-          result = subject.execute
+          result = downloader.execute
 
           expect(result.success).to be_falsey
         end
       end
 
-      context 'no Geo primary exists' do
+      context 'when no Geo primary exists' do
         it 'returns failure' do
           primary.update!(primary: false)
 
-          result = subject.execute
+          result = downloader.execute
 
           expect(result.success).to be_falsey
         end
@@ -45,13 +46,14 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
             path = replicator.carrierwave_uploader.path
             expect(File).to receive(:directory?).with(path).and_return(true)
 
-            result = subject.execute
+            result = downloader.execute
 
             expect(result.success).to be_falsey
           end
         end
 
-        xit 'ensures the file destination directory exists' # Not worth testing here as-is. Extract the functionality first.
+        # Not worth testing here as-is. Extract the functionality first.
+        xit 'ensures the file destination directory exists'
       end
 
       context 'when the file is on Object Storage' do
@@ -62,9 +64,9 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
           stub_current_geo_node(secondary_object_storage)
         end
 
-        let!(:model_record) { create(:package_file, :npm, :object_storage) }
+        let!(:model_record) { create(:package_file, :npm, :object_storage) } # rubocop:disable RSpec/ScatteredLet -- due to creation on primary then test on secondary
 
-        subject { described_class.new(replicator: model_record.replicator) }
+        subject(:downloader) { described_class.new(replicator: model_record.replicator) }
 
         context 'with object storage sync enabled' do
           let(:sync_object_storage) { true }
@@ -75,10 +77,10 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
             it 'returns success' do
               content = replicator.carrierwave_uploader.file.read
               size = content.bytesize
-              stub_request(:get, subject.resource_url)
+              stub_request(:get, downloader.resource_url)
                 .to_return(status: 200, body: content)
 
-              result = subject.execute
+              result = downloader.execute
 
               expect_blob_downloader_result(result, success: true, bytes_downloaded: size, primary_missing_file: false)
             end
@@ -98,7 +100,7 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
                 allow(request).to receive(:headers).and_return(geo_internal_headers)
               end
 
-              stub_request(:get, subject.resource_url)
+              stub_request(:get, downloader.resource_url)
                 .to_return(status: 302, headers: { 'Location' => remote_url })
 
               # This stub is intended to cause this test to fail when all
@@ -116,7 +118,7 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
             end
 
             it 'returns success', :aggregate_failures do
-              result = subject.execute
+              result = downloader.execute
 
               expect_blob_downloader_result(result, success: true, bytes_downloaded: size, primary_missing_file: false)
 
@@ -131,7 +133,7 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
             end
 
             it 'returns failure' do
-              result = subject.execute
+              result = downloader.execute
 
               expect(result.success).to be_falsey
             end
@@ -142,7 +144,7 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
           let(:sync_object_storage) { false }
 
           it 'returns failure' do
-            result = subject.execute
+            result = downloader.execute
 
             expect(result.success).to be_falsey
           end
@@ -152,11 +154,11 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
 
     context 'when an error occurs while getting a Tempfile' do
       it 'returns failure' do
-        subject
+        downloader
 
         expect(Tempfile).to receive(:new).and_raise('boom')
 
-        result = subject.execute
+        result = downloader.execute
 
         expect(result.success).to be_falsey
         expect(result.extra_details).to have_key(:error)
@@ -166,27 +168,33 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
     context 'when the HTTP response is unsuccessful' do
       context 'when the HTTP response indicates a missing file on the primary' do
         it 'returns a failed result indicating primary_missing_file' do
-          stub_request(:get, subject.resource_url)
-            .to_return(status: 404,
-                       headers: { content_type: 'application/json' },
-                       body: { geo_code: Gitlab::Geo::Replication::FILE_NOT_FOUND_GEO_CODE }.to_json)
+          stub_request(:get, downloader.resource_url)
+            .to_return(
+              status: 404,
+              headers: { content_type: 'application/json' },
+              body: { geo_code: Gitlab::Geo::Replication::FILE_NOT_FOUND_GEO_CODE }.to_json
+            )
 
-          result = subject.execute
+          result = downloader.execute
 
-          expect_blob_downloader_result(result, success: false, bytes_downloaded: 0, primary_missing_file: true, reason: 'The file is missing on the Geo primary site')
+          expect_blob_downloader_result(result, success: false, bytes_downloaded: 0, primary_missing_file: true,
+            reason: 'The file is missing on the Geo primary site')
         end
       end
 
       context 'when the HTTP response does not indicate a missing file on the primary' do
         it 'returns a failed result' do
-          stub_request(:get, subject.resource_url)
-            .to_return(status: 404,
-                       headers: { content_type: 'application/json' },
-                       body: 'Not found')
+          stub_request(:get, downloader.resource_url)
+            .to_return(
+              status: 404,
+              headers: { content_type: 'application/json' },
+              body: 'Not found'
+            )
 
-          result = subject.execute
+          result = downloader.execute
 
-          expect_blob_downloader_result(result, success: false, bytes_downloaded: 0, primary_missing_file: false, reason: 'Non-success HTTP response status code 404')
+          expect_blob_downloader_result(result, success: false, bytes_downloaded: 0, primary_missing_file: false,
+            reason: 'Non-success HTTP response status code 404')
         end
       end
     end
@@ -196,14 +204,14 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
         path = replicator.carrierwave_uploader.path
         content = replicator.carrierwave_uploader.file.read
         size = content.bytesize
-        stub_request(:get, subject.resource_url).to_return(status: 200, body: content)
+        stub_request(:get, downloader.resource_url).to_return(status: 200, body: content)
 
-        result = subject.execute
+        result = downloader.execute
         stat = File.stat(path)
 
         expect_blob_downloader_result(result, success: true, bytes_downloaded: size, primary_missing_file: false)
         expect(stat.size).to eq(size)
-        expect(stat.mode & 0777).to eq(0666 - File.umask)
+        expect(stat.mode & 0o0777).to eq(0o0666 - File.umask)
         expect(File.binread(path)).to eq(content)
       end
 
@@ -211,12 +219,13 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
         it 'returns a failed result' do
           allow(replicator).to receive(:primary_checksum).and_return('something')
           bad_content = 'corrupted!!!'
-          stub_request(:get, subject.resource_url)
+          stub_request(:get, downloader.resource_url)
             .to_return(status: 200, body: bad_content)
 
-          result = subject.execute
+          result = downloader.execute
 
-          expect_blob_downloader_result(result, success: false, bytes_downloaded: bad_content.bytesize, primary_missing_file: false)
+          expect_blob_downloader_result(result,
+            success: false, bytes_downloaded: bad_content.bytesize, primary_missing_file: false)
         end
       end
 
@@ -224,12 +233,13 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
         it 'returns a successful result' do
           expect(replicator).to receive(:primary_checksum).and_return(nil)
           content = 'foo'
-          stub_request(:get, subject.resource_url)
+          stub_request(:get, downloader.resource_url)
             .to_return(status: 200, body: content)
 
-          result = subject.execute
+          result = downloader.execute
 
-          expect_blob_downloader_result(result, success: true, bytes_downloaded: content.bytesize, primary_missing_file: false)
+          expect_blob_downloader_result(result,
+            success: true, bytes_downloaded: content.bytesize, primary_missing_file: false)
         end
       end
 
@@ -241,18 +251,19 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
           stub_current_geo_node(secondary_object_storage)
         end
 
-        let!(:model_record) { create(:package_file, :npm, :object_storage) }
+        let!(:model_record) { create(:package_file, :npm, :object_storage) } # rubocop:disable RSpec/ScatteredLet -- due to creation on primary then test on secondary
 
         it 'returns a successful result' do
           allow(replicator).to receive(:primary_checksum).and_return("03")
 
           content = 'foo' # 3 bytes
-          stub_request(:get, subject.resource_url)
+          stub_request(:get, downloader.resource_url)
             .to_return(status: 200, body: content)
 
-          result = subject.execute
+          result = downloader.execute
 
-          expect_blob_downloader_result(result, success: true, bytes_downloaded: content.bytesize, primary_missing_file: false)
+          expect_blob_downloader_result(result,
+            success: true, bytes_downloaded: content.bytesize, primary_missing_file: false)
         end
       end
     end
@@ -262,10 +273,7 @@ RSpec.describe Gitlab::Geo::Replication::BlobDownloader, feature_category: :geo_
     expect(result.success).to eq(success)
     expect(result.bytes_downloaded).to eq(bytes_downloaded)
     expect(result.primary_missing_file).to eq(primary_missing_file)
-
-    if reason
-      expect(result.reason).to eq(reason)
-    end
+    expect(result.reason).to eq(reason) if reason
 
     # Sanity check to help ensure a valid test
     expect(success).not_to be_nil
