0de0cb2bcf32ae7fe282fb6ce9db96e3d6839d7c
Ahmed Hemdan <ahemdan@gitlab.com>
2023-12-12 06:42:35 +0000

    List and filter blobs in secrets check
    This commit updates the secrets push check to list and filter blobs:
    
    * To be below a certain size threshold.
    * If they have already been committed to the repository.
    * If they are binary.

--- ee/spec/lib/ee/gitlab/checks/push_rules/secrets_check_spec.rb
+++ ee/spec/lib/ee/gitlab/checks/push_rules/secrets_check_spec.rb
@@ -3,9 +3,66 @@
 require 'spec_helper'
 
 RSpec.describe EE::Gitlab::Checks::PushRules::SecretsCheck, feature_category: :secret_detection do
-  include_context 'push rules checks context'
-
+  let_it_be(:user) { create(:user) }
   let_it_be(:push_rule) { create(:push_rule) }
+
+  # Project is created with a custom repository, so
+  # we create a README to have a blob committed already.
+  let_it_be(:project) do
+    create(
+      :project,
+      :custom_repo,
+      files: { 'README' => 'Documentation goes here.' },
+      push_rule: push_rule
+    )
+  end
+
+  let_it_be(:repository) { project.repository }
+
+  # Set revisions as follows:
+  #   1. oldrev to commit created with the project above.
+  #   2. newrev to commit created in before_all below.
+  let(:oldrev) { '5e08a9ef8e7e257cbc727d6786bbc33ad7662625' }
+  let(:newrev) { 'fe29d93da4843da433e62711ace82db601eb4f8f' }
+  let(:changes) do
+    [
+      {
+        oldrev: oldrev,
+        newrev: newrev,
+        ref: 'refs/heads/master'
+      }
+    ]
+  end
+
+  let(:protocol) { 'ssh' }
+  let(:timeout) { Gitlab::GitAccess::INTERNAL_TIMEOUT }
+  let(:logger) { Gitlab::Checks::TimedLogger.new(timeout: timeout) }
+  let(:user_access) { Gitlab::UserAccess.new(user, container: project) }
+  let(:changes_access) do
+    Gitlab::Checks::ChangesAccess.new(
+      changes,
+      project: project,
+      user_access: user_access,
+      protocol: protocol,
+      logger: logger
+    )
+  end
+
+  subject { described_class.new(changes_access) }
+
+  before_all do
+    project.add_developer(user)
+
+    # Create a new commit to be used as the new revision in changes passed to secrets check.
+    repository.commit_files(
+      user,
+      branch_name: 'add-dotenv-file',
+      message: 'Add .env file',
+      actions: [
+        { action: :create, file_path: '.env', content: "SECRET=glpat-JUST20LETTERSANDNUMB" } # gitleaks:allow
+      ]
+    )
+  end
 
   describe '#validate!' do
     it_behaves_like 'check ignored when push rule unlicensed'
@@ -43,10 +100,7 @@ RSpec.describe EE::Gitlab::Checks::PushRules::SecretsCheck, feature_category: :s
             stub_licensed_features(pre_receive_secret_detection: true)
           end
 
-          it 'returns without raising errors' do
-            # Since the check does nothing at the moment, it just execute without raising errors
-            expect { subject.validate! }.not_to raise_error
-          end
+          it_behaves_like 'list and filter blobs'
         end
       end
 
@@ -66,10 +120,7 @@ RSpec.describe EE::Gitlab::Checks::PushRules::SecretsCheck, feature_category: :s
             stub_licensed_features(pre_receive_secret_detection: true)
           end
 
-          it 'returns without raising errors' do
-            # Since the check does nothing at the moment, it just execute without raising errors
-            expect { subject.validate! }.not_to raise_error
-          end
+          it_behaves_like 'list and filter blobs'
         end
 
         context 'when feature flag is disabled' do
