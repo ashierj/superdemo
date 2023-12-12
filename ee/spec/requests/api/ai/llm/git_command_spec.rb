cc1ad968878ca03272671ce2934a4b6f2d62e47c
Nicolas Dular <ndular@gitlab.com>
2023-12-12 17:55:37 +0100

    Move git command assistant to vertex
    This moves the git command service to use vertex by default.
    
    Changelog: changed
    EE: true

--- ee/spec/requests/api/ai/llm/git_command_spec.rb
+++ ee/spec/requests/api/ai/llm/git_command_spec.rb
@@ -6,8 +6,7 @@ RSpec.describe API::Ai::Llm::GitCommand, :saas, feature_category: :source_code_m
   let_it_be(:current_user) { create :user }
 
   let(:url) { '/ai/llm/git_command' }
-  let(:model) { 'vertexai' }
-  let(:input_params) { { prompt: 'list 10 commit titles', model: model } }
+  let(:input_params) { { prompt: 'list 10 commit titles' } }
   let(:make_request) { post api(url, current_user), params: input_params }
 
   before do
@@ -80,44 +79,7 @@ RSpec.describe API::Ai::Llm::GitCommand, :saas, feature_category: :source_code_m
       end
     end
 
-    context 'when openai model is requested' do
-      let(:model) { 'openai' }
-      let(:header) { { 'Authorization' => ['Bearer test-key'], 'Content-Type' => ['application/json'] } }
-
-      before do
-        stub_ee_application_setting(openai_api_key: 'test-key')
-      end
-
-      it_behaves_like 'delegates AI request to Workhorse' do
-        let(:expected_params) do
-          expected_content = <<~PROMPT
-          Provide the appropriate git commands for: list 10 commit titles.
-          Respond with JSON format
-          ##
-          {
-            "commands": [The list of commands],
-            "explanation": The explanation with the commands wrapped in backticks
-          }
-          PROMPT
-
-          {
-            'URL' => ::Gitlab::Llm::OpenAi::Workhorse::CHAT_URL,
-            'Header' => header,
-            'Body' => {
-              model: 'gpt-3.5-turbo',
-              messages: [{
-                role: "user",
-                content: expected_content
-              }],
-              temperature: 0.4,
-              max_tokens: 300
-            }.to_json
-          }
-        end
-      end
-    end
-
-    context 'when openai experimentation is unavailable' do
+    context 'when ai_global_switch is turned off' do
       before do
         stub_feature_flags(ai_global_switch: false)
       end
