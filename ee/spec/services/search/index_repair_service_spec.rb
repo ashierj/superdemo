a4171ada62adcee6b5c721bf2b6a3ef8235c83c5
Ravi Kumar <rkumar@gitlab.com>
2023-12-14 16:49:22 +0000

    Fix the index_name in IndexrepairService
    It was checking in the main index for project documents. Since the
    project documents have been moved to the separate index, it should
    check in the separate index.
    
    Changelog: fixed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/139540
    EE: true

--- ee/spec/services/search/index_repair_service_spec.rb
+++ ee/spec/services/search/index_repair_service_spec.rb
@@ -236,5 +236,176 @@ RSpec.describe ::Search::IndexRepairService, feature_category: :global_search do
         service.execute
       end
     end
+
+    context 'when migration migrate_projects_to_separate_index is finished' do
+      include ElasticsearchHelpers
+      before do
+        set_elasticsearch_migration_to(:migrate_projects_to_separate_index, including: true)
+      end
+
+      context 'when project and blobs exist in the index' do
+        before do
+          blob_body = {
+            query: {
+              bool: {
+                filter: [
+                  { term: { type: 'blob' } },
+                  { term: { project_id: project.id } }
+                ]
+              }
+            }
+          }
+
+          project_body = {
+            query: {
+              bool: {
+                filter: [
+                  { term: { type: 'project' } },
+                  { term: { id: project.id } }
+                ]
+              }
+            }
+          }
+
+          allow(client).to receive(:count).with(
+            index: Repository.index_name,
+            body: blob_body,
+            routing: project.es_id
+          ).and_return({ 'count' => 10 })
+
+          allow(client).to receive(:count).with(
+            index: Project.index_name,
+            body: project_body,
+            routing: project.es_id
+          ).and_return({ 'count' => 1 })
+        end
+
+        it 'does not log anything' do
+          expect(logger).not_to receive(:warn)
+
+          service.execute
+        end
+      end
+
+      context 'when blobs are missing from the index' do
+        let_it_be(:project_stats) { create(:project_statistics, project: project, repository_size: 100) }
+
+        before do
+          blob_body = { query: { bool: { filter: [{ term: { type: 'blob' } },
+            { term: { project_id: project.id } }] } } }
+          project_body = { query: { bool: { filter: [{ term: { type: 'project' } }, { term: { id: project.id } }] } } }
+
+          allow(client).to receive(:count).with(
+            index: Repository.index_name,
+            body: blob_body,
+            routing: project.es_id
+          ).and_return({ 'count' => 0 })
+
+          allow(client).to receive(:count).with(
+            index: Project.index_name,
+            body: project_body,
+            routing: project.es_id
+          ).and_return({ 'count' => 1 })
+        end
+
+        context 'when index_status does not exist' do
+          before do
+            allow(project).to receive(:index_status).and_return(nil)
+          end
+
+          it_behaves_like 'a service that repairs the blobs index' do
+            let(:expected_hash) do
+              {
+                message: 'blob documents missing from index for project',
+                class: described_class.to_s,
+                namespace_id: project.namespace_id,
+                root_namespace_id: project.root_namespace.id,
+                project_id: project.id,
+                project_last_repository_updated_at: be_within(0.05.seconds).of(project.last_repository_updated_at),
+                index_status_last_commit: nil,
+                index_status_indexed_at: nil,
+                repository_size: 100
+              }.stringify_keys
+            end
+          end
+        end
+
+        context 'when index_status exists' do
+          let_it_be_with_reload(:index_status) { create(:index_status, project: project) }
+
+          context 'when index_status last_commit does not match last project commit' do
+            before do
+              index_status.update!(last_commit: 'FAKE_SHA')
+            end
+
+            it_behaves_like 'a service that repairs the blobs index' do
+              let(:expected_hash) do
+                {
+                  message: 'blob documents missing from index for project',
+                  class: described_class.to_s,
+                  namespace_id: project.namespace_id,
+                  root_namespace_id: project.root_namespace.id,
+                  project_id: project.id,
+                  project_last_repository_updated_at: be_within(0.05.seconds).of(project.last_repository_updated_at),
+                  index_status_last_commit: 'FAKE_SHA',
+                  index_status_indexed_at: index_status.indexed_at,
+                  repository_size: 100
+                }.stringify_keys
+              end
+            end
+          end
+
+          context 'when index_status last_commit matches the repository head_commit' do
+            it_behaves_like 'a service that does no repair work for the blobs index'
+          end
+
+          context 'when project has a null commit' do
+            before do
+              allow(project).to receive(:commit).and_return(nil)
+            end
+
+            it_behaves_like 'a service that does no repair work for the blobs index'
+          end
+        end
+      end
+
+      context 'when project is missing from the index' do
+        before do
+          stub_ee_application_setting(elasticsearch_search: true, elasticsearch_indexing: true)
+
+          blob_body = { query: { bool: { filter: [{ term: { type: 'blob' } },
+            { term: { project_id: project.id } }] } } }
+          project_body = { query: { bool: { filter: [{ term: { type: 'project' } }, { term: { id: project.id } }] } } }
+
+          allow(client).to receive(:count).with(
+            index: Repository.index_name,
+            body: blob_body,
+            routing: project.es_id
+          ).and_return({ 'count' => 20 })
+
+          allow(client).to receive(:count).with(
+            index: Project.index_name,
+            body: project_body,
+            routing: project.es_id
+          ).and_return({ 'count' => 0 })
+        end
+
+        it 'enqueues the project for indexing and logs a warning' do
+          expect(::Elastic::ProcessBookkeepingService).to receive(:track!).with(project)
+
+          expected_hash = {
+            message: 'project document missing from index',
+            class: 'Search::IndexRepairService',
+            namespace_id: project.namespace_id,
+            root_namespace_id: project.root_namespace.id,
+            project_id: project.id
+          }.stringify_keys
+
+          expect(logger).to receive(:warn).with(a_hash_including(expected_hash))
+
+          service.execute
+        end
+      end
+    end
   end
 end
