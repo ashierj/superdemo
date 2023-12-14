afec9b9739e7cd31a1eb57c98d8916038ae199e6
Julia Miocene <jmiocene@gitlab.com>
2023-12-14 11:02:11 +0100

    Fix insights illustration

--- ee/app/assets/javascripts/insights/constants.js
+++ ee/app/assets/javascripts/insights/constants.js
@@ -12,7 +12,7 @@ export const EMPTY_STATE_TITLE = __('Invalid Insights config file detected');
 export const EMPTY_STATE_DESCRIPTION = __(
   'Please check the configuration file to ensure that it is available and the YAML is valid',
 );
-export const EMPTY_STATE_SVG_PATH = '/assets/illustrations/monitoring/getting_started.svg';
+export const EMPTY_STATE_SVG_PATH = '/assets/illustrations/empty-state/empty-dashboard-md.svg';
 
 export const INSIGHTS_CONFIGURATION_TEXT = s__(
   'Insights|Configure a custom report for insights into your group processes such as amount of issues, bugs, and merge requests per month. %{linkStart}How do I configure an insights report?%{linkEnd}',
