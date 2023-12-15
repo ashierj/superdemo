f3080536685a88f9193132514c02a8e9c3d881d3
Roy Liu <rliu@gitlab.com>
2023-12-15 21:21:13 +0000

    Add free access end date to code suggestions alert
    Changelog: changed
    EE: true

--- ee/spec/views/layouts/group.html.haml_spec.rb
+++ ee/spec/views/layouts/group.html.haml_spec.rb
@@ -18,4 +18,33 @@ RSpec.describe 'layouts/group', feature_category: :groups_and_projects do
       expect(rendered).to render_template('shared/_free_user_cap_alert')
     end
   end
+
+  context 'when code_suggestions_ga_non_owner_alert is present' do
+    before do
+      allow(view).to receive(:show_code_suggestions_ga_non_owner_alert?).and_return(true)
+    end
+
+    it 'renders supported IDE extensions doc link' do
+      render
+
+      expect(rendered).to have_link('your IDE',
+        href: help_page_path('user/project/repository/code_suggestions/index', anchor: 'supported-editor-extensions'))
+    end
+
+    context 'with code_suggestions_ga_non_owner_alert_end_date feature flag' do
+      it 'renders the end date sentence when the flag is enabled' do
+        render
+
+        expect(rendered).to have_content(_('Code Suggestions transitions to a paid feature on February 15, 2024'))
+      end
+
+      it 'renders the end date sentence when the flag is disabled' do
+        stub_feature_flags(code_suggestions_ga_non_owner_alert_end_date: false)
+
+        render
+
+        expect(rendered).not_to have_content(_('Code Suggestions transitions to a paid feature on February 15, 2024'))
+      end
+    end
+  end
 end
