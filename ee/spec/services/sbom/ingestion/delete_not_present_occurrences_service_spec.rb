f497600650840bc9a171b5d7b7ad34c463d4b1dc
Michael Becker <11881043-wandering_person@users.noreply.gitlab.com>
2023-12-18 13:43:09 +0000

    Skip ingesting the SBOM reports if any of the SBOM builds fail
    If a single SBOM job fails, during a pipeline for the default branch,
    then the corresponding components are marked as no longer detected.
    
    This is misleading and can lead to false positives and issues further
    down the reporting workflow. This [discussion thread][0] discusses the
    issues in more detail and comes to the conclusion that we should skip
    ingesting the SBOM reports if *any* of these SBOM jobs fails.
    
    related to: https://gitlab.com/gitlab-org/gitlab/-/issues/421666
    
    [0]:https://gitlab.com/gitlab-org/gitlab/-/issues/421666#note_1532251397
    
    Changelog: changed
    EE: true

--- ee/spec/services/sbom/ingestion/delete_not_present_occurrences_service_spec.rb
+++ ee/spec/services/sbom/ingestion/delete_not_present_occurrences_service_spec.rb
@@ -9,6 +9,22 @@ RSpec.describe Sbom::Ingestion::DeleteNotPresentOccurrencesService, feature_cate
 
   subject(:execute) { described_class.execute(pipeline, ingested_ids) }
 
+  shared_examples 'it no-ops with failed sbom jobs' do
+    context 'when there are failed sbom jobs' do
+      let(:metadata) { { artifacts: { reports: { cyclonedx: { foo: :bar } } } } }
+
+      before do
+        create(:ee_ci_build, :failed, pipeline: pipeline)
+          .metadata
+          .update!(config_options: metadata)
+      end
+
+      it 'does not effect occurence count' do
+        expect { execute }.not_to change { Sbom::Occurrence.count }
+      end
+    end
+  end
+
   describe '#execute' do
     context 'when project has occurrences' do
       let_it_be_with_reload(:occurrences) { create_list(:sbom_occurrence, 4, pipeline: pipeline, source: source) }
@@ -19,6 +35,8 @@ RSpec.describe Sbom::Ingestion::DeleteNotPresentOccurrencesService, feature_cate
         it 'deletes all occurrences' do
           expect { execute }.to change { project.sbom_occurrences.reload.count }.from(4).to(0)
         end
+
+        it_behaves_like 'it no-ops with failed sbom jobs'
       end
 
       context 'when a subset of occurrences have been removed' do
@@ -30,6 +48,8 @@ RSpec.describe Sbom::Ingestion::DeleteNotPresentOccurrencesService, feature_cate
 
           expect(project.sbom_occurrences.reload).to match_array(ingested_occurrences)
         end
+
+        it_behaves_like 'it no-ops with failed sbom jobs'
       end
     end
   end
