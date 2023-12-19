485c97d0533762926a8066504a780bb8c3e123f1
Niklas <mc.taucher2003@gmail.com>
2023-12-19 08:28:33 +0000

    Change permission for namespace type to read_namespace
    Changelog: fixed
    
    Namespaces have not been visible on the GraphQL API for
    users that are not part of the namespace. This is different
    than it is for groups when they are public. This changes
    the namespaces to be visible to everyone, if they are public.

--- ee/app/graphql/ee/types/namespace_type.rb
+++ ee/app/graphql/ee/types/namespace_type.rb
@@ -18,64 +18,74 @@ module EE
           ::Types::GitlabSubscriptions::AddOnPurchaseType,
           null: true,
           description: 'AddOnPurchase associated with the namespace',
-          resolver: ::Resolvers::GitlabSubscriptions::Namespaces::AddOnPurchaseResolver
+          resolver: ::Resolvers::GitlabSubscriptions::Namespaces::AddOnPurchaseResolver,
+          authorize: :read_namespace_via_membership
 
         field :additional_purchased_storage_size,
               GraphQL::Types::Float,
               null: true,
-              description: 'Additional storage purchased for the root namespace in bytes.'
+              description: 'Additional storage purchased for the root namespace in bytes.',
+              authorize: :read_namespace_via_membership
 
         field :total_repository_size_excess,
               GraphQL::Types::Float,
               null: true,
               description: 'Total excess repository size of all projects in the root namespace in bytes. ' \
-                           'This only applies to namespaces under Project limit enforcement.'
+                           'This only applies to namespaces under Project limit enforcement.',
+              authorize: :read_namespace_via_membership
 
         field :total_repository_size,
               GraphQL::Types::Float,
               null: true,
-              description: 'Total repository size of all projects in the root namespace in bytes.'
+              description: 'Total repository size of all projects in the root namespace in bytes.',
+              authorize: :read_namespace_via_membership
 
         field :contains_locked_projects,
               GraphQL::Types::Boolean,
-              null: false,
+              null: true,
               description: 'Includes at least one project where the repository size exceeds the limit. ' \
                            'This only applies to namespaces under Project limit enforcement.',
-              method: :contains_locked_projects?
+              method: :contains_locked_projects?,
+              authorize: :read_namespace_via_membership
 
         field :repository_size_excess_project_count,
               GraphQL::Types::Int,
-              null: false,
+              null: true,
               description: 'Number of projects in the root namespace where the repository size exceeds the limit. ' \
-                           'This only applies to namespaces under Project limit enforcement.'
+                           'This only applies to namespaces under Project limit enforcement.',
+              authorize: :read_namespace_via_membership
 
         field :actual_repository_size_limit,
               GraphQL::Types::Float,
               null: true,
               description: 'Size limit for repositories in the namespace in bytes. ' \
-                           'This limit only applies to namespaces under Project limit enforcement.'
+                           'This limit only applies to namespaces under Project limit enforcement.',
+              authorize: :read_namespace_via_membership
 
         field :actual_size_limit,
               GraphQL::Types::Float,
               null: true,
               description: 'The actual storage size limit (in bytes) based on the enforcement type ' \
-                           'of either repository or namespace. This limit is agnostic of enforcement type.'
+                           'of either repository or namespace. This limit is agnostic of enforcement type.',
+              authorize: :read_namespace_via_membership
 
         field :storage_size_limit,
               GraphQL::Types::Float,
               null: true,
               description: 'The storage limit (in bytes) included with the root namespace plan. ' \
-                           'This limit only applies to namespaces under namespace limit enforcement.'
+                           'This limit only applies to namespaces under namespace limit enforcement.',
+              authorize: :read_namespace_via_membership
 
         field :is_temporary_storage_increase_enabled,
               GraphQL::Types::Boolean,
-              null: false,
+              null: true,
               description: 'Status of the temporary storage increase.',
               deprecated: {
                 reason: 'Feature removal, will be completely removed in 17.0',
                 milestone: '16.7'
               },
-              method: :temporary_storage_increase_enabled?
+              method: :temporary_storage_increase_enabled?,
+              authorize: :read_namespace_via_membership
 
         field :temporary_storage_increase_ends_on,
               ::Types::TimeType,
@@ -84,13 +94,15 @@ module EE
               deprecated: {
                 reason: 'Feature removal, will be completely removed in 17.0',
                 milestone: '16.7'
-              }
+              },
+              authorize: :read_namespace_via_membership
 
         field :compliance_frameworks,
               ::Types::ComplianceManagement::ComplianceFrameworkType.connection_type,
               null: true,
               description: 'Compliance frameworks available to projects in this namespace.',
-              resolver: ::Resolvers::ComplianceManagement::FrameworkResolver
+              resolver: ::Resolvers::ComplianceManagement::FrameworkResolver,
+              authorize: :read_namespace_via_membership
 
         field :scan_execution_policies,
               ::Types::SecurityOrchestration::ScanExecutionPolicyType.connection_type,
