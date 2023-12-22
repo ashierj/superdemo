2d8dc4e81b32c0a7433830077b3bdd307da6265d
Eduardo Sanz García <esanz-garcia@gitlab.com>
2023-12-22 14:34:59 +0000

    Retire gitlab_enable_admin_mode_sign_in
    Replace `gitlab_enable_admin_mode_sign_in` helper for
    `enable_admin_mode!`.
    
    This simplifies the developer experience. It creates a single function
    to access the admin area, with a parameter to switch between a fast or a
    slow way.

--- ee/spec/features/clusters/cluster_detail_page_spec.rb
+++ ee/spec/features/clusters/cluster_detail_page_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe 'Clusterable > Show page', feature_category: :deployment_manageme
     let(:cluster) { create(:cluster, :provided_by_gcp, :instance) }
 
     before do
-      gitlab_enable_admin_mode_sign_in(current_user)
+      enable_admin_mode!(current_user)
     end
 
     it 'shows the environments tab' do
