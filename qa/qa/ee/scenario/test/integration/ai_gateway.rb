9d22b1fa4f41c020221aee936d41028bc7559bef
Valerie Burton <vburton@gitlab.com>
2023-12-22 07:58:02 +0000

    E2E: Add Orchestrated code_suggestions_spec

---
+++ qa/qa/ee/scenario/test/integration/ai_gateway.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module QA
+  module EE
+    module Scenario
+      module Test
+        module Integration
+          class AiGateway < QA::Scenario::Test::Instance::All
+            tags :ai_gateway
+          end
+        end
+      end
+    end
+  end
+end
