55ef8336031bf7287e307ac7121eae5eee049bad
Siddharth Dungarwal <sdungarwal@gitlab.com>
2024-01-08 13:07:12 +0000

    Add migration to reindex all epics

---
+++ ee/elastic/migrate/20231213172132_reindex_all_epics.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ReindexAllEpics < Elastic::Migration
+  include ::Search::Elastic::MigrationDatabaseBackfillHelper
+
+  batch_size 10_000
+  batched!
+  throttle_delay 1.minute
+  retry_on_failure
+
+  DOCUMENT_TYPE = Epic
+
+  def respect_limited_indexing?
+    true
+  end
+end
