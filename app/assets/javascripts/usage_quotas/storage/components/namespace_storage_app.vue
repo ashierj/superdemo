86df2e0106e29bf38dbc6c536703bdeed27e3d37
Sheldon Led <sheldonled@gitlab.com>
2024-01-10 09:41:27 +0000

    Introduce NamespaceStorageApp in core

---
+++ app/assets/javascripts/usage_quotas/storage/components/namespace_storage_app.vue
@@ -0,0 +1,39 @@
+<script>
+import { GlAlert } from '@gitlab/ui';
+
+export default {
+  name: 'NamespaceStorageApp',
+  components: {
+    GlAlert,
+  },
+  props: {
+    namespaceLoadingError: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    projectsLoadingError: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+};
+</script>
+<template>
+  <div>
+    <gl-alert
+      v-if="namespaceLoadingError || projectsLoadingError"
+      variant="danger"
+      :dismissible="false"
+      class="gl-mt-4"
+    >
+      {{
+        s__(
+          'UsageQuota|An error occured while loading the storage usage details. Please refresh the page to try again.',
+        )
+      }}
+    </gl-alert>
+    <slot name="ee-storage-app"></slot>
+  </div>
+</template>
