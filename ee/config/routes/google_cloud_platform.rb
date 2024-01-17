ab8251ca594a51a7d59a7fc6403aea4cb98283a7
Rahul Chanila <rchanila@gitlab.com>
2024-01-17 01:37:49 +0000

    Adds show route for google artifact registry
    EE: true

--- ee/config/routes/google_cloud_platform.rb
+++ ee/config/routes/google_cloud_platform.rb
@@ -2,4 +2,8 @@
 
 namespace :google_cloud_platform do
   resources :artifact_registry, only: :index
+
+  get '/artifact_registry/projects/:project/locations/:location/repositories/:repository/dockerImages/:image',
+    to: 'artifact_registry#show',
+    as: :artifact_registry_image
 end
