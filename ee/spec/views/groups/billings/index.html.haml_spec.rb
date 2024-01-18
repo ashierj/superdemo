8d57a39890cc9adab8d6f3f3081bbeebdf6af59a
Ross Byrne <robyrne@gitlab.com>
2024-01-18 09:03:44 +0000

    Updated copy for due pro hand-raise PQL on billings page
    Changelog: changed
    EE: true

--- ee/spec/views/groups/billings/index.html.haml_spec.rb
+++ ee/spec/views/groups/billings/index.html.haml_spec.rb
@@ -93,10 +93,10 @@ RSpec.describe 'groups/billings/index', :saas, :aggregate_failures, feature_cate
         it 'renders the code suggestions component' do
           render
 
-          expect(rendered).to have_content(s_('CodeSuggestions|Introducing Duo Pro'))
-          expect(rendered).to have_content('Enhance your coding experience with intelligent recommendations')
+          expect(rendered).to have_content(s_('CodeSuggestions|Introducing the GitLab Duo Pro add-on'))
+          expect(rendered).to have_content('Boost productivity by using Code Suggestions to write and understand code.')
           href = help_page_path('user/project/repository/code_suggestions/index')
-          expect(rendered).to have_link('Duo Pro', href: href)
+          expect(rendered).to have_link('Code Suggestions', href: href)
           hand_raise_selector = '.js-hand-raise-lead-button[data-track-label="code_suggestions_hand_raise_lead_form"]'
           expect(rendered).to have_selector(hand_raise_selector)
         end
@@ -110,7 +110,7 @@ RSpec.describe 'groups/billings/index', :saas, :aggregate_failures, feature_cate
         it 'does not render the code suggestions component' do
           render
 
-          expect(rendered).not_to have_content(s_('CodeSuggestions|Introducing the Code Suggestions add-on'))
+          expect(rendered).not_to have_content(s_('CodeSuggestions|Introducing the GitLab Duo Pro add-on'))
         end
       end
     end
