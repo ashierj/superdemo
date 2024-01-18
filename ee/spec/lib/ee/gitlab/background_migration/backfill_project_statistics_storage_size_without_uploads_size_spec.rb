febb89058f437a33ef43b99c37bf7475bd727176
Leonardo da Rosa <ldarosa@gitlab.com>
2024-01-18 19:30:56 +0000

    Removes database migrations up to 15-9-stable-ee
    Pre-16.3 migration files were removed and schema
    changes moved to db/init_structure.sql file
    
    Changelog: changed

--- ee/spec/lib/ee/gitlab/background_migration/backfill_project_statistics_storage_size_without_uploads_size_spec.rb
+++ ee/spec/lib/ee/gitlab/background_migration/backfill_project_statistics_storage_size_without_uploads_size_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe Gitlab::BackgroundMigration::BackfillProjectStatisticsStorageSizeWithoutUploadsSize, :migration, schema: 20221104115712, feature_category: :consumables_cost_management do # rubocop:disable Layout/LineLength
+RSpec.describe Gitlab::BackgroundMigration::BackfillProjectStatisticsStorageSizeWithoutUploadsSize, :migration, schema: 20230216060333, feature_category: :consumables_cost_management do # rubocop:disable Layout/LineLength
   include MigrationHelpers::ProjectStatisticsHelper
 
   let!(:namespace) { table(:namespaces) }
