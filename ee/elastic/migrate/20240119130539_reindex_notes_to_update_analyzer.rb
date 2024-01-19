c0fd247544196cad1cdbf0a7e9f983154db7398a
rkumar555 <rkumar@gitlab.com>
2024-01-19 13:20:35 +0100

    Add the code_analyzer for notes note
    code_analyzer allows to better search the code syntax in the notes
    note. Also, adding an elastic migration to use Zero downtime
    reindexing to reindex all notes.
    
    Changelog: added
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/142301
    EE: true

---
+++ ee/elastic/migrate/20240119130539_reindex_notes_to_update_analyzer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class ReindexNotesToUpdateAnalyzer < Elastic::Migration
+  def migrate
+    Elastic::ReindexingTask.create!(targets: %w[Note], options: { skip_pending_migrations_check: true })
+  end
+
+  def completed?
+    true
+  end
+end
