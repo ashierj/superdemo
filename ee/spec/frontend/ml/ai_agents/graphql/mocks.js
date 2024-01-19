e343e98ea2c994e44690b879e6819ed78c544278
Darby Frey <dfrey@gitlab.com>
2024-01-19 15:29:36 +0000

    AI Agent list view
    AI agents are now listed in the UI
    
    Changelog: added
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/141664
    EE: true

--- ee/spec/frontend/ml/ai_agents/graphql/mocks.js
+++ ee/spec/frontend/ml/ai_agents/graphql/mocks.js
@@ -21,3 +21,47 @@ export const createAiAgentsResponses = {
     },
   },
 };
+
+export const listAiAgentsResponses = {
+  data: {
+    project: {
+      id: 'gid://gitlab/Project/1',
+      aiAgents: {
+        nodes: [
+          {
+            id: 'gid://gitlab/Ai::Agent/1',
+            name: 'agent-1',
+            versions: [
+              {
+                id: 'gid://gitlab/Ai::AgentVersion/1',
+                prompt: 'example prompt',
+                model: 'default',
+              },
+            ],
+            _links: {
+              showPath: '/namespace/projects/-/ml/agents/1',
+            },
+          },
+        ],
+        pageInfo: {
+          hasNextPage: false,
+          hasPreviousPage: false,
+          startCursor: 'eyJpZCI6IjEwIn0',
+          endCursor: 'eyJpZCI6IjEifQ',
+          __typename: 'PageInfo',
+        },
+      },
+    },
+  },
+};
+
+export const listAiAgentsEmptyResponses = {
+  data: {
+    project: {
+      id: 'gid://gitlab/Project/1',
+      aiAgents: {
+        nodes: [],
+      },
+    },
+  },
+};
