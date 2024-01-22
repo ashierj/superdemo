1e48821a0a1c2d3bf5ced4e16c4c9eab593f2a28
Desiree Chevalier <dchevalier@gitlab.com>
2024-01-22 14:46:36 -0500

    Fix selector for pipeline subscription e2e spec

--- qa/qa/ee/page/project/settings/pipeline_subscriptions.rb
+++ qa/qa/ee/page/project/settings/pipeline_subscriptions.rb
@@ -7,13 +7,17 @@ module QA
         module Settings
           class PipelineSubscriptions < QA::Page::Base
             view 'ee/app/views/projects/settings/subscriptions/_table.html.haml' do
-              element 'add-new-subscription'
+              element 'add-new-subscription-btn'
               element 'upstream-project-path-field'
               element 'subscribe-button'
             end
 
+            view 'ee/app/assets/javascripts/ci/pipeline_subscriptions/components/pipeline_subscriptions_table.vue' do
+              element 'add-new-subscription-btn'
+            end
+
             def subscribe(project_path)
-              click_element('add-new-subscription')
+              click_element('add-new-subscription-btn')
               fill_element('upstream-project-path-field', project_path)
               click_element('subscribe-button')
             end
