680fc12cadc37b9214645ac827d30b0cca54ee51
rkumar555 <rkumar@gitlab.com>
2024-01-22 15:05:08 +0100

    Set state to ready

--- ee/app/workers/search/zoekt/scheduling_worker.rb
+++ ee/app/workers/search/zoekt/scheduling_worker.rb
@@ -53,8 +53,10 @@ module Search
           Node.descending_order_by_free_bytes.each do |node|
             space_required = BUFFER_FACTOR * storage_statistics.repository_size
             if (node.used_bytes + space_required) <= node.total_bytes * WATERMARK_LIMIT
+              # TODO: Once we have the task which moves pending to ready then remove the state attribute from here
+              # https://gitlab.com/gitlab-org/gitlab/-/issues/439042
               zoekt_index = Search::Zoekt::Index.new(namespace_id: zoekt_enabled_namespace.root_namespace_id,
-                zoekt_node_id: node.id, zoekt_enabled_namespace: zoekt_enabled_namespace)
+                zoekt_node_id: node.id, zoekt_enabled_namespace: zoekt_enabled_namespace, state: :ready)
 
               unless zoekt_index.save
                 logger.error(build_structured_payload(task: :node_assignment,
