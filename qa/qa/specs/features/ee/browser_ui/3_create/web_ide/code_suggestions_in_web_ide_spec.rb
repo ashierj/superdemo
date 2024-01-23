5dc3bd9ae80526874761aa6133ac71d4ba5f0e08
Jay McCure <jmccure@gitlab.com>
2024-01-23 16:22:29 +1000

    E2E test: generalize code suggestions webIDE test

--- qa/qa/specs/features/ee/browser_ui/3_create/web_ide/code_suggestions_in_web_ide_spec.rb
+++ qa/qa/specs/features/ee/browser_ui/3_create/web_ide/code_suggestions_in_web_ide_spec.rb
@@ -10,17 +10,6 @@ module QA
       let(:project) { create(:project, :with_readme, name: 'webide-code-suggestions-project') }
       let(:file_name) { 'new_file.rb' }
 
-      shared_examples 'code suggestions in the Web IDE' do |testcase|
-        it 'returns a suggestion which can be accepted', testcase: testcase do
-          Page::Project::WebIDE::VSCode.perform do |ide|
-            ide.add_prompt_into_a_file(file_name, prompt_data)
-            ide.verify_prompt_appears_and_accept(prompt_regex)
-
-            expect(ide.validate_prompt(prompt_regex)).to eq true
-          end
-        end
-      end
-
       before do
         Flow::Login.sign_in
 
@@ -35,19 +24,46 @@ module QA
 
       context 'when requesting code generation' do
         let(:prompt_data) { 'def reverse_string' }
-        let(:prompt_regex) { /\.reverse/ }
 
-        it_behaves_like 'code suggestions in the Web IDE', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/425756'
+        it 'returns a suggestion which can be accepted',
+          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/425756' do
+          Page::Project::WebIDE::VSCode.perform do |ide|
+            ide.add_prompt_into_a_file(file_name, prompt_data)
+            previous_content_length = ide.editor_content_length
+            previous_content_lines = ide.editor_content_lines
+
+            # code generation will put suggestion on the next line
+            ide.wait_for_code_suggestion
+            expect(ide.editor_content_length).to be > previous_content_length, "Expected a suggestion"
+            expect(ide.editor_content_lines).to be > previous_content_lines, "Expected additional lines in suggestion"
+
+            ide.accept_code_suggestion
+            expect(ide.editor_content_length).to be > previous_content_length, "Expected accepted suggestion in file"
+            expect(ide.editor_content_lines).to be > previous_content_lines, "Expected additional lines in file"
+          end
+        end
       end
 
       context 'when requesting code completion' do
         let(:prompt_data) { "def set_name(whitespace_name)\n    this.name = whitespace_name." }
 
-        # We check that any character is suggested after the prompt,
-        # except a new line, as code completions appear on the same line
-        let(:prompt_regex) { /whitespace_name\.[^\n]/ }
+        it 'returns a suggestion which can be accepted',
+          testcase: 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/437111' do
+          Page::Project::WebIDE::VSCode.perform do |ide|
+            ide.add_prompt_into_a_file(file_name, prompt_data)
+            previous_content_length = ide.editor_content_length
+            previous_content_lines = ide.editor_content_lines
 
-        it_behaves_like 'code suggestions in the Web IDE', 'https://gitlab.com/gitlab-org/gitlab/-/quality/test_cases/437111'
+            # code completion will put suggestion on the same line
+            ide.wait_for_code_suggestion
+            expect(ide.editor_content_length).to be > previous_content_length, 'Expected a suggestion'
+            expect(ide.editor_content_lines).to eq(previous_content_lines), 'Expected suggestion on same line'
+
+            ide.accept_code_suggestion
+            expect(ide.editor_content_length).to be > previous_content_length, 'Expected accepted suggestion in file'
+            expect(ide.editor_content_lines).to eq(previous_content_lines), 'Expected suggestion on same line'
+          end
+        end
       end
     end
   end
