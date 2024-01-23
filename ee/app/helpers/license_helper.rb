6f64497263b61435e0e7e05cd7c4dbf3b1200b43
Paul Slaughter <pslaughter@gitlab.com>
2024-01-23 19:30:29 +0000

    Fix current_license_title namespace translation
    - We should use s_ for namespaced i18n
    
    Changelog: fixed

--- ee/app/helpers/license_helper.rb
+++ ee/app/helpers/license_helper.rb
@@ -12,7 +12,7 @@ module LicenseHelper
   end
 
   def current_license_title
-    License.current&.plan&.titleize || _('BillingPlans|Free')
+    License.current&.plan&.titleize || s_('BillingPlans|Free')
   end
 
   def has_active_license?
