2dc624c38e61d25a16a917b7f7129be1b5dbe4d4
Jannik Lehmann <jlehmann@gitlab.com>
2024-01-26 10:47:57 +0000

    Refactor vuex usage out of inline findings
    This commit refactors the vuex usage out of
    the inline findings feature.
    
    EE: true

--- ee/app/assets/javascripts/diffs/components/inline_findings_utils.js
+++ ee/app/assets/javascripts/diffs/components/inline_findings_utils.js
@@ -1,18 +1,14 @@
-/* eslint-disable import/export */
-export * from '~/diffs/store/getters';
-
-// Returns the code quality degradations for a specific line of a given file
-export const fileLineCodequality = (state) => (file, line) => {
-  const fileDiff = state.codequalityDiff.files?.[file] || [];
+export const fileLineCodequality = (file, line, codequalityData) => {
+  const fileDiff = codequalityData?.files?.[file] || [];
   const lineDiff = fileDiff.filter((violation) => violation.line === line);
   return lineDiff;
 };
 
 // Returns the SAST degradations for a specific line of a given file
-export const fileLineSast = (state) => (file, line) => {
+export const fileLineSast = (file, line, sastData) => {
   const lineDiff = [];
 
-  state?.sastDiff?.added?.map((e) => {
+  sastData?.added?.map((e) => {
     const startLine = parseInt(e.location.startLine, 10);
     if (e.location.file === file && startLine === line) {
       lineDiff.push({
