e8d499516ed527ad416c26eb16a308daa9cc6275
Vasilii Iakliushin <viakliushin@gitlab.com>
2024-01-26 12:52:13 +0000

    Save repository object format on project creation
    Contributes to https://gitlab.com/gitlab-org/gitlab/-/issues/419887
    
    **Problem**
    
    In previous MR
    https://gitlab.com/gitlab-org/gitlab/-/merge_requests/136819 we extended
    `project_repositories` table with a new field `object_format` to keep
    the type of the repository (`sha1` or `sha256`). But this field isn't
    populated yet.
    
    **Solution**
    
    Populate the field with a `object_format` value from Gitaly on project's
    creation.

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1279,8 +1279,6 @@ class Repository
   end
 
   def object_format
-    return unless exists?
-
     cache_key = "object_format:#{full_path}"
 
     request_store_cache.fetch(cache_key) do
@@ -1291,6 +1289,8 @@ class Repository
         FORMAT_SHA256
       end
     end
+  rescue Gitlab::Git::Repository::NoRepository
+    nil
   end
 
   def blank_ref
