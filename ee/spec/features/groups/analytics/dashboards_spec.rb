5387b70202f99c7cf0a8f30f39c6ab9f65fa7729
Felipe Artur <felipefac@gmail.com>
2024-01-30 16:56:20 -0300

    Add feature specs for custom VSD
    Include feature specs for value stream
    dashboards customized with YAML.

--- ee/spec/features/groups/analytics/dashboards_spec.rb
+++ ee/spec/features/groups/analytics/dashboards_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe 'Analytics Dashboard', :js, feature_category: :value_stream_manag
   let_it_be(:user) { current_user }
   let_it_be(:group) { create(:group, name: "vsd test group") }
   let_it_be(:project) { create(:project, :repository, name: "vsd project", group: group) }
+  let_it_be(:custom_vsd_fixture_path) { 'ee/spec/fixtures/analytics/valid_value_stream_dashboard_configuration.yaml' }
 
   let(:metric_table) { find_by_testid('panel-dora-chart') }
 
@@ -24,6 +25,21 @@ RSpec.describe 'Analytics Dashboard', :js, feature_category: :value_stream_manag
   context 'with a valid user' do
     before_all do
       group.add_developer(user)
+    end
+
+    def create_custom_vsd(user, pointer_project, yaml_fixture_path)
+      repository_file_path = '.gitlab/analytics/dashboards/value_streams/value_streams.yaml'
+
+      yaml_configuration =
+        File.read(yaml_fixture_path)
+
+      pointer_project.repository.create_file(
+        user,
+        repository_file_path,
+        yaml_configuration,
+        message: "commit #{repository_file_path}",
+        branch_name: 'master'
+      )
     end
 
     context 'with group_level_analytics_dashboard license' do
@@ -44,23 +60,97 @@ RSpec.describe 'Analytics Dashboard', :js, feature_category: :value_stream_manag
           expect(page).to have_content _('Dashboards are created by editing the groups dashboard files')
         end
 
+        context 'when a custom dashboard exists' do
+          let_it_be(:pointer_project) { create(:project, :repository, group: group) }
+
+          before_all do
+            create(:analytics_dashboards_pointer, namespace: group, target_project: pointer_project)
+            create_custom_vsd(user, pointer_project, custom_vsd_fixture_path)
+          end
+
+          it 'renders custom dashboard link' do
+            dashboard_items = page.all(dashboard_list_item_testid)
+            first_dashboard = dashboard_items[0]
+            second_dashboard = dashboard_items[1]
+
+            expect(dashboard_items.length).to eq(2)
+            expect(first_dashboard).to have_content _('Value Streams Dashboard')
+            expect(first_dashboard).to have_selector(dashboard_by_gitlab_testid)
+            expect(second_dashboard).to have_content _('Custom VSD')
+            expect(second_dashboard).to have_content _('VSD from fixture')
+            expect(second_dashboard).not_to have_selector(dashboard_by_gitlab_testid)
+          end
+        end
+
         it_behaves_like 'has value streams dashboard link'
       end
 
       context 'for Value streams dashboard' do
-        before do
-          visit_group_value_streams_dashboard(group)
+        context 'with builtin configuration' do
+          before do
+            visit_group_value_streams_dashboard(group)
+          end
+
+          it_behaves_like 'VSD renders as an analytics dashboard'
+          it_behaves_like 'renders link to the feedback survey'
+          it_behaves_like 'renders usage overview metrics'
+          it_behaves_like 'renders dora performers score'
+
+          it_behaves_like 'renders metrics comparison table'
+
+          it 'renders a title for the metrics comparison table' do
+            expect(metric_table).to have_content format(_("Metrics comparison for %{name} group"), name: group.name)
+          end
         end
 
-        it_behaves_like 'VSD renders as an analytics dashboard'
-        it_behaves_like 'renders link to the feedback survey'
-        it_behaves_like 'renders usage overview metrics'
-        it_behaves_like 'renders dora performers score'
+        context 'with custom configuration' do
+          context 'with valid attributes' do
+            let_it_be(:pointer_project) { create(:project, :repository, group: group) }
 
-        it_behaves_like 'renders metrics comparison table'
+            before_all do
+              create(:analytics_dashboards_pointer, namespace: group, target_project: pointer_project)
+              create_custom_vsd(user, pointer_project, custom_vsd_fixture_path)
+            end
 
-        it 'renders a title for the metrics comparison table' do
-          expect(metric_table).to have_content format(_("Metrics comparison for %{name} group"), name: group.name)
+            before do
+              visit_group_value_streams_dashboard(group, 'Custom VSD')
+            end
+
+            it 'renders dashboard correctly' do
+              within find_by_testid('dashboard-description') do |panel|
+                expect(panel).to have_content _('VSD from fixture')
+              end
+              within find_by_testid('gridstack-grid') do |panel|
+                expect(panel).to have_content _('Custom Panel 1')
+              end
+            end
+
+            # Configuration fixture has usage overview panel
+            it_behaves_like 'renders usage overview metrics', false
+          end
+
+          context 'with invalid attributes' do
+            let_it_be(:pointer_project) { create(:project, :repository, group: group) }
+            let_it_be(:invalid_custom_vsd_fixture_path) do
+              'ee/spec/fixtures/analytics/invalid_value_stream_dashboard_configuration.yaml'
+            end
+
+            before_all do
+              create(:analytics_dashboards_pointer, namespace: group, target_project: pointer_project)
+              create_custom_vsd(user, pointer_project, invalid_custom_vsd_fixture_path)
+            end
+
+            before do
+              visit_group_value_streams_dashboard(group, 'Invalid VSD')
+            end
+
+            it 'renders error' do
+              find_by_testid('panel-not-exists').hover
+
+              expect(page).to have_content(_('Something is wrong with your panel visualization configuration.'))
+              expect(page).to have_link(text: 'troubleshooting documentation')
+            end
+          end
         end
       end
     end
@@ -93,6 +183,7 @@ RSpec.describe 'Analytics Dashboard', :js, feature_category: :value_stream_manag
 
       it 'renders a title for the metrics comparison table' do
         comparison_title = find_by_testid('comparison-chart-title')
+
         expect(comparison_title).to have_content format(_('Metrics comparison for %{name} group'), name: group.name)
       end
     end
