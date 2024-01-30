a785a2784320a7340d36a3daad18bf29a9d6bbfe
Daniele Rossetti <drossetti@gitlab.com>
2024-01-30 23:05:27 +0000

    Scaffold Logs UI feature (frontend)

---
+++ ee/spec/frontend/logs/list_index_spec.js
@@ -0,0 +1,32 @@
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+import ListIndex from 'ee/logs/list_index.vue';
+import ProvisionedObservabilityContainer from '~/observability/components/provisioned_observability_container.vue';
+
+describe('ListIndex', () => {
+  const props = {
+    apiConfig: {
+      oauthUrl: 'https://example.com/oauth',
+      tracingUrl: 'https://example.com/tracing',
+      provisioningUrl: 'https://example.com/provisioning',
+      servicesUrl: 'https://example.com/services',
+      operationsUrl: 'https://example.com/operations',
+      metricsUrl: 'https://example.com/metricsUrl',
+    },
+  };
+
+  let wrapper;
+
+  const mountComponent = () => {
+    wrapper = shallowMountExtended(ListIndex, {
+      propsData: props,
+    });
+  };
+
+  it('renders provisioned-observability-container component', () => {
+    mountComponent();
+
+    const observabilityContainer = wrapper.findComponent(ProvisionedObservabilityContainer);
+    expect(observabilityContainer.exists()).toBe(true);
+    expect(observabilityContainer.props('apiConfig')).toStrictEqual(props.apiConfig);
+  });
+});
