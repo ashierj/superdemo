a69bbb81e233e2b8dddbccb9b93a13e9bce4db7a
Rémy Coutable <remy@rymai.me>
2024-01-31 09:24:32 +0100

    Add tests to detect useless and missing CI patterns
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- spec/dot_gitlab_ci/rules_spec.rb
+++ spec/dot_gitlab_ci/rules_spec.rb
@@ -58,4 +58,85 @@ RSpec.describe '.gitlab/ci/rules.gitlab-ci.yml', feature_category: :tooling do
       end
     end
   end
+
+  describe 'patterns' do
+    foss_context = !Gitlab.ee?
+    no_coverage_needed = (
+      [
+        ".editorconfig",
+        ".foreman",
+        ".git-blame-ignore-revs",
+        ".gitlab/CODEOWNERS",
+        ".gitleaksignore",
+        ".license_encryption_key.pub",
+        ".mailmap",
+        ".prettierignore",
+        ".projections.json.example",
+        ".rubocop_revert_ignores.txt",
+        ".ruby-version",
+        ".test_license_encryption_key.pub",
+        ".tool-versions",
+        ".vale.ini",
+        ".vscode/extensions.json",
+        "Gemfile.checksum",
+        "Guardfile",
+        "INSTALLATION_TYPE",
+        "LICENSE",
+        "Pipfile.lock",
+        "ee/LICENSE"
+      ] +
+      Dir.glob('.github/*', File::FNM_DOTMATCH) +
+      Dir.glob('.gitlab/{issue,merge_request}_templates/**/*', File::FNM_DOTMATCH) +
+      Dir.glob('.gitlab/*.toml', File::FNM_DOTMATCH) +
+      Dir.glob('.lefthook/**/*', File::FNM_DOTMATCH) +
+      Dir.glob('changelogs/*', File::FNM_DOTMATCH) +
+      Dir.glob('file_hooks/**/*', File::FNM_DOTMATCH) +
+      Dir.glob('patches/*', File::FNM_DOTMATCH) +
+      Dir.glob('tmp/**/*', File::FNM_DOTMATCH) +
+      Dir.glob('{,**/}.gitkeep', File::FNM_DOTMATCH) +
+      Dir.glob('{,**/}.gitignore', File::FNM_DOTMATCH) +
+      Dir.glob('*.md', File::FNM_DOTMATCH)
+    ).freeze
+    expected_missing_coverage = (
+      Dir.glob("keeps/**/*") +
+      Dir.glob("metrics_server/*") +
+      Dir.glob("sidekiq_cluster/*") +
+      ["\"workhorse/testdata/file-\\303\\244.pdf\""]
+    ).freeze
+    all_files = `git ls-files`.split("\n") - no_coverage_needed
+    all_files -= Dir.glob('ee/**/*') if foss_context
+
+    all_patterns_files = Set.new
+
+    config.each do |name, patterns|
+      next unless name.start_with?('.')
+      next unless name.end_with?('patterns')
+      # Ignore EE-only patterns list when in FOSS context
+      next if foss_context && patterns.all? { |pattern| pattern =~ %r|{?ee/| }
+
+      describe "patterns list `#{name}`" do
+        patterns.each do |pattern|
+          pattern_files = Dir.glob(pattern, File::FNM_DOTMATCH)
+          all_patterns_files.merge(pattern_files)
+
+          it "detects `#{pattern}` as a matching pattern" do
+            matching_files = (all_files & pattern_files)
+
+            expect(matching_files).not_to be_empty
+          end
+        end
+      end
+    end
+
+    describe 'missed coverage', :aggregate_failures do
+      it 'does not miss coverage' do
+        missed_files = (all_files - all_patterns_files.to_a)
+        missed_files_without_expected_missing_coverage = (missed_files - expected_missing_coverage)
+
+        expect(missed_files).not_to be_empty
+        expect(missed_files_without_expected_missing_coverage).to be_empty
+        p(missed_files_without_expected_missing_coverage) unless missed_files_without_expected_missing_coverage.empty?
+      end
+    end
+  end
 end
