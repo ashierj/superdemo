9b91529d6a563c3f7ad3d6ebde84bfda1d67bd43
Miranda Fluharty <mfluharty@gitlab.com>
2024-02-01 13:39:34 +0000

    Make variable value listbox searchable
    So that it can be filled using only the keyboard
    
    Changelog: fixed

---
+++ app/assets/javascripts/ci/pipeline_new/components/variable_values_listbox.vue
@@ -0,0 +1,64 @@
+<script>
+import { GlCollapsibleListbox } from '@gitlab/ui';
+import { n__ } from '~/locale';
+
+export default {
+  name: 'VariableValuesListbox',
+  components: {
+    GlCollapsibleListbox,
+  },
+  props: {
+    selected: {
+      type: String,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      searchTerm: '',
+    };
+  },
+  computed: {
+    searchSummary() {
+      return n__(
+        'CiVariables|%d value found',
+        'CiVariables|%d values found',
+        this.filteredItems.length,
+      );
+    },
+    filteredItems() {
+      return this.items.filter((option) => option.text.toLowerCase().includes(this.searchTerm));
+    },
+  },
+  methods: {
+    onSearch(searchTerm) {
+      this.searchTerm = searchTerm.trim().toLowerCase();
+    },
+  },
+};
+</script>
+<template>
+  <gl-collapsible-listbox
+    :items="filteredItems"
+    :toggle-text="selected"
+    :selected="selected"
+    :search-placeholder="s__('CiVariables|Search values')"
+    :no-results-text="s__('CiVariables|No matching values')"
+    searchable
+    block
+    fluid-width
+    @search="onSearch"
+    @select="$emit('select', $event)"
+  >
+    <template #search-summary-sr-only>
+      {{ searchSummary }}
+    </template>
+    <template #list-item="{ item: { text } }">
+      <span data-testid="ci-variable-value-dropdown-item">{{ text }}</span>
+    </template>
+  </gl-collapsible-listbox>
+</template>
---
+++ spec/frontend/ci/pipeline_new/components/variable_values_listbox_spec.js
@@ -0,0 +1,61 @@
+import { GlCollapsibleListbox } from '@gitlab/ui';
+import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+import waitForPromises from 'helpers/wait_for_promises';
+import VariableValuesListbox from '~/ci/pipeline_new/components/variable_values_listbox.vue';
+import { mockYamlVariables } from '../mock_data';
+
+const { value, valueOptions } = mockYamlVariables[2];
+
+describe('Variable values listbox', () => {
+  let wrapper;
+
+  const findListbox = () => wrapper.findComponent(GlCollapsibleListbox);
+  const findListboxItems = () => wrapper.findAllByTestId('ci-variable-value-dropdown-item');
+  const search = (searchString) => findListbox().vm.$emit('search', searchString);
+
+  const createComponent = () => {
+    wrapper = shallowMountExtended(VariableValuesListbox, {
+      propsData: {
+        selected: value,
+        items: valueOptions.map((option) => ({
+          text: option,
+          value: option,
+        })),
+      },
+      stubs: {
+        GlCollapsibleListbox,
+      },
+    });
+  };
+
+  beforeEach(() => {
+    createComponent();
+  });
+
+  it('multiple predefined values are rendered as a dropdown', () => {
+    expect(findListboxItems().at(0).text()).toBe(valueOptions[0]);
+    expect(findListboxItems().at(1).text()).toBe(valueOptions[1]);
+    expect(findListboxItems().at(2).text()).toBe(valueOptions[2]);
+  });
+
+  it('variable with multiple predefined values sets value as the default', () => {
+    expect(findListbox().props('selected')).toBe(value);
+  });
+
+  it('filters options based on search', async () => {
+    const searchString = 'prod';
+
+    search(searchString);
+
+    await waitForPromises();
+
+    expect(findListboxItems().length).toBe(1);
+    expect(findListboxItems().at(0).text()).toContain(searchString);
+
+    search('');
+
+    await waitForPromises();
+
+    expect(findListboxItems().length).toBe(3);
+  });
+});
