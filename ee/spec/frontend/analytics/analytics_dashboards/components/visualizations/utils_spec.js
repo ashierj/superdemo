2b7b2f8d01dc6971c8db2458a4316de6d94ff428
Elwyn Benson <ebenson@gitlab.com>
2024-02-02 09:41:09 +0000

    Add delimiters to large numbers in analytics chart tooltips
    This ensures chart tooltips remain easy to read when numerical values
    get large.
    
    Changelog: changed
    EE: true

---
+++ ee/spec/frontend/analytics/analytics_dashboards/components/visualizations/utils_spec.js
@@ -0,0 +1,25 @@
+import { formatVisualizationValue } from 'ee/analytics/analytics_dashboards/components/visualizations/utils';
+
+describe('formatVisualizationValue', () => {
+  describe('when the value is not numeric', () => {
+    it.each(['abc', true, null, undefined])('returns the value without modification', (value) => {
+      expect(formatVisualizationValue(value)).toBe(value);
+    });
+  });
+
+  describe('when the value is numeric', () => {
+    it.each([
+      [123, '123'],
+      [1234, '1,234'],
+      [-123, '-123'],
+      [123.12, '123'],
+      [-1234.12, '-1,234'],
+      ['1234567890', '1,234,567,890'],
+      ['1234567890.123456', '1,234,567,890'],
+      ['-1234567890', '-1,234,567,890'],
+      ['-1234567890.123456', '-1,234,567,890'],
+    ])('returns the formatted value', (value, expected) => {
+      expect(formatVisualizationValue(value)).toBe(expected);
+    });
+  });
+});
