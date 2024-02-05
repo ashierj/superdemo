bcdedf57aa6ab804207e5c4b858c36ebd62c6ee6
Elwyn Benson <ebenson@gitlab.com>
2024-02-05 13:55:12 +1300

    Fix bug where date strings where incorrectly formatted
    Ensure date strings within product analytics data tables and
    chart tooltips display as their original unformatted value instead
    of incorrectly being treated as a delimited number.
    
    Changelog: fixed
    EE: true

--- ee/app/assets/javascripts/analytics/analytics_dashboards/components/visualizations/utils.js
+++ ee/app/assets/javascripts/analytics/analytics_dashboards/components/visualizations/utils.js
@@ -1,7 +1,12 @@
 import { isNumeric } from '~/lib/utils/number_utils';
 import { formatNumber } from '~/locale';
+import { isValidDateString } from '~/lib/utils/datetime_range';
 
 export function formatVisualizationValue(value) {
+  if (isValidDateString(value)) {
+    return value;
+  }
+
   if (isNumeric(value)) {
     return formatNumber(parseInt(value, 10));
   }
