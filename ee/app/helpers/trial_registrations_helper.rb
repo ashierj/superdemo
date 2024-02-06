c1935cc5b8e6b178432833deed666dfc922f3af3
Serhii Yarynovskyi <syarynovskyi@gitlab.com>
2024-02-06 23:10:12 +0000

    Add logos to trial registration page
    Add logos, tracking and styling to trial page
    with free_trial_registration_redesign experiment

--- ee/app/helpers/trial_registrations_helper.rb
+++ ee/app/helpers/trial_registrations_helper.rb
@@ -1,6 +1,39 @@
 # frozen_string_literal: true
 
 module TrialRegistrationsHelper
+  TRUSTED_BY_LOGOS = [
+    {
+      path: 'marketing/t-mobile.svg',
+      alt: 'T-Mobile',
+      title: 'T-Mobile'
+    },
+    {
+      path: 'marketing/goldman-sachs.svg',
+      alt: 'Goldman Sachs',
+      title: 'Goldman Sachs'
+    },
+    {
+      path: 'marketing/cncf.svg',
+      alt: 'Cloud Native Computing Foundation',
+      title: 'Cloud Native Computing Foundation'
+    },
+    {
+      path: 'illustrations/third-party-logos/siemens.svg',
+      alt: 'Siemens',
+      title: 'Siemens'
+    },
+    {
+      path: 'marketing/nvidia.svg',
+      alt: 'NVIDIA',
+      title: 'NVIDIA'
+    },
+    {
+      path: 'marketing/ubs.svg',
+      alt: 'UBS',
+      title: 'UBS'
+    }
+  ].freeze
+
   def social_signin_enabled?
     ::Gitlab.com? &&
       omniauth_enabled? &&
