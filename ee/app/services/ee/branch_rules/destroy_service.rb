8511142540a865e328a75663e6f56e8113fa039d
Joe Woodward <j@joewoodward.me>
2024-02-07 12:15:32 +0000

    Implement EE extension for BranchRules::DestroyService
    In EE we have 2 additional branch rule classes,
    `Projects::AllBranchesRule` and `Projects::AllProtectedBranchesRule`.
    This change adds an EE extension to handle destroying those rules.
    
    We use the existing service classes to destroy the records and return a
    ServiceResponse.
    
    This change also includes a BaseService extension to delegate
    `approval_project_rules` and `external_status_checks` to the
    `branch_rule`.
    
    We have changed the logic in CE to check if the
    `branch_rule.is_a?(Projects::BranchRule)` as `is_a?` includes ancestors.
    Now using `instance_of?` to be more accurate. Without this change
    `Projects::AllBranchesRule` and `Projects::AllProtectedBranchesRule`
    would also return true as they are decendents of `Projects::BranchRule`.
    
    Includes new trait for `approval_project_rule` factory
    `:for_all_protected_branches`.

---
+++ ee/app/services/ee/branch_rules/destroy_service.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module EE
+  module BranchRules
+    module DestroyService
+      extend ::Gitlab::Utils::Override
+
+      # rubocop:disable Cop/AvoidReturnFromBlocks -- The overriden `execute`
+      # method yields and then raises an error. We use `return` here to exit
+      # the super method. If we used break it would break out of the block and
+      # continue execution of the super method causing the error to be raised.
+      override :execute
+      def execute
+        super do
+          case branch_rule
+          when ::Projects::AllBranchesRule
+            return destroy_all_branches_rule
+          when ::Projects::AllProtectedBranchesRule
+            return destroy_all_protected_branches_rule
+          end
+        end
+      end
+      # rubocop:enable Cop/AvoidReturnFromBlocks
+
+      private
+
+      def destroy_all_branches_rule
+        response = destroy_approval_project_rules
+
+        return response if response.error?
+
+        destroy_external_status_checks
+      end
+
+      def destroy_all_protected_branches_rule
+        destroy_approval_project_rules
+      end
+
+      def destroy_approval_project_rules
+        errors = approval_project_rules.find_each.each_with_object([]) do |rule, error_accumulator|
+          response = ::ApprovalRules::ProjectRuleDestroyService.new(rule, current_user).execute
+
+          error_accumulator << response if response[:status] == :error
+        end
+        return ::ServiceResponse.success if errors.blank?
+
+        ::ServiceResponse.error(message: "Failed to delete approval #{'rule'.pluralize(errors.count)}.")
+      end
+
+      def destroy_external_status_checks
+        errors = external_status_checks.find_each.each_with_object([]) do |check, error_accumulator|
+          response = ::ExternalStatusChecks::DestroyService.new(
+            container: project, current_user: current_user
+          ).execute(check)
+
+          error_accumulator << response if response[:status] == :error
+        end
+
+        return ::ServiceResponse.success if errors.blank?
+
+        ::ServiceResponse.error(message: "Failed to delete external status #{'check'.pluralize(errors.count)}.")
+      end
+    end
+  end
+end
