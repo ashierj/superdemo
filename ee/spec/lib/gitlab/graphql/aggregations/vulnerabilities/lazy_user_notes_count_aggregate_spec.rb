c5a27b98a1bf49caa70bbccc31498647b8e0d016
Michael Becker <11881043-wandering_person@users.noreply.gitlab.com>
2024-02-09 14:00:08 +0700

    Fix flaky test `lazy_user_notes_count_aggregate_spec.rb`
    This commit modifies the `lazy_user_notes_count_aggregate_spec.rb`
    spec to de-duplicate the expected array of `ids`.
    
    This is because, as currently written, if `FactoryBot` assigns an id
    of 20 (or 10 or 30) to the `vulnerability`, the spec will fail, making
    this a flaky spec.
    
    You can see my [comment][0] on the issue for a more detailed breakdown
    of the flakiness
    
    [0]:https://gitlab.com/gitlab-org/gitlab/-/issues/441189#note_1765583669
    
    resolves: https://gitlab.com/gitlab-org/gitlab/-/issues/441189

--- ee/spec/lib/gitlab/graphql/aggregations/vulnerabilities/lazy_user_notes_count_aggregate_spec.rb
+++ ee/spec/lib/gitlab/graphql/aggregations/vulnerabilities/lazy_user_notes_count_aggregate_spec.rb
@@ -17,11 +17,24 @@ RSpec.describe Gitlab::Graphql::Aggregations::Vulnerabilities::LazyUserNotesCoun
       expect(subject.vulnerability).to match vulnerability
     end
 
-    it 'uses lazy_user_notes_count_aggregate to collect aggregates', quarantine: 'https://gitlab.com/gitlab-org/gitlab/-/issues/441189' do
-      subject = described_class.new({ lazy_user_notes_count_aggregate: { pending_vulnerability_ids: [10, 20, 30].to_set, loaded_objects: {} } }, vulnerability)
+    context 'when there are existing pending_vulnerability_ids' do
+      subject(:result) do
+        described_class.new(
+          { lazy_user_notes_count_aggregate: {
+            pending_vulnerability_ids: pending_ids_set,
+            loaded_objects: {}
+          } },
+          vulnerability
+        )
+      end
 
-      expect(subject.lazy_state[:pending_vulnerability_ids]).to match_array [10, 20, 30, vulnerability.id]
-      expect(subject.vulnerability).to match vulnerability
+      let(:pending_ids_set) { [10, 20, 30].to_set }
+      let(:expected_ids) { pending_ids_set.dup.add(vulnerability.id) }
+
+      it 'uses lazy_user_notes_count_aggregate to collect aggregates' do
+        expect(result.lazy_state[:pending_vulnerability_ids]).to match_array expected_ids
+        expect(result.vulnerability).to match vulnerability
+      end
     end
   end
 
