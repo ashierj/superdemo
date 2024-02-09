3f487a7e3b2a90c10505d4f47627e7dd192e0d5a
Gosia Ksionek <mksionek@gitlab.com>
2024-02-09 08:10:46 +0000

    Fix error in CloudConnector::Access when we have data, but the date is nil

--- ee/spec/models/cloud_connector/access_spec.rb
+++ ee/spec/models/cloud_connector/access_spec.rb
@@ -19,9 +19,9 @@ RSpec.describe CloudConnector::Access, models: true, feature_category: :cloud_co
     end
 
     context 'with data' do
-      let_it_be(:service_start_time) { DateTime.new(2024, 1, 29, 12, 0, 0) }
-      let_it_be(:data) { { available_services: [{ name: "duo_chat", serviceStartTime: service_start_time.to_s }] } }
-      let_it_be(:cloud_connector_access) { create(:cloud_connector_access, data: data) }
+      let(:service_start_time) { DateTime.new(2024, 1, 29, 12, 0, 0).to_s }
+      let(:data) { { available_services: [{ name: "duo_chat", serviceStartTime: service_start_time }] } }
+      let!(:cloud_connector_access) { create(:cloud_connector_access, data: data) }
 
       context 'with service defined' do
         it 'returns the service start time' do
@@ -32,6 +32,14 @@ RSpec.describe CloudConnector::Access, models: true, feature_category: :cloud_co
       context 'without service defined' do
         it 'returns the service start time' do
           expect(described_class.service_start_date_for('gitlab')).to be_nil
+        end
+      end
+
+      context 'without service start date' do
+        let(:service_start_time) { nil }
+
+        it 'returns nil' do
+          expect(described_class.service_start_date_for('duo_chat')).to be_nil
         end
       end
     end
