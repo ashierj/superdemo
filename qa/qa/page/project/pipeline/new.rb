268508c3e2f77102721e0d5e32bc408d19070662
Miranda Fluharty <mfluharty@gitlab.com>
2024-02-12 15:53:16 +0000

    Look for testid in form component again
    Now that we removed the testids from the listbox component

--- qa/qa/page/project/pipeline/new.rb
+++ qa/qa/page/project/pipeline/new.rb
@@ -10,9 +10,6 @@ module QA
             element 'ci-variable-row-container'
             element 'pipeline-form-ci-variable-key-field'
             element 'pipeline-form-ci-variable-value-field'
-          end
-
-          view 'app/assets/javascripts/ci/pipeline_new/components/variable_values_listbox.vue' do
             element 'pipeline-form-ci-variable-value-dropdown'
           end
 
