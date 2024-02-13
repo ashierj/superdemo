fe321c6b1c7c772f3f9079b6eedc51e6626c1afb
Jacques Erasmus <jerasmus@gitlab.com>
2024-02-13 11:40:43 +0000

    Use Duo Chat for explain code feature
    Use the Duo Chat component instead for explain code feature
    
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/ai/components/ai_genie.vue
+++ ee/app/assets/javascripts/ai/components/ai_genie.vue
@@ -1,20 +1,19 @@
 <script>
 import { debounce } from 'lodash';
 import { GlButton, GlTooltipDirective } from '@gitlab/ui';
+import { createAlert } from '~/alert';
+import { s__ } from '~/locale';
 import SafeHtml from '~/vue_shared/directives/safe_html';
 import LineHighlighter from '~/blob/line_highlighter';
-import { generateExplainCodePrompt, generateChatPrompt } from 'ee/ai/utils';
-import AiGenieChat from 'ee/ai/components/ai_genie_chat.vue';
-import CodeBlockHighlighted from '~/vue_shared/components/code_block_highlighted.vue';
+import { helpCenterState } from '~/super_sidebar/constants';
 import glFeatureFlagsMixin from '~/vue_shared/mixins/gl_feature_flags_mixin';
-import aiResponseSubscription from 'ee/graphql_shared/subscriptions/ai_completion_response.subscription.graphql';
+import chatMutation from 'ee/ai/graphql/chat.mutation.graphql';
 import {
   i18n,
   AI_GENIE_DEBOUNCE,
-  GENIE_CHAT_MODEL_ROLES,
   EXPLAIN_CODE_TRACKING_EVENT_NAME,
+  GENIE_CHAT_EXPLAIN_MESSAGE,
 } from '../constants';
-import explainCodeMutation from '../graphql/explain_code.mutation.graphql';
 
 const linesWithDigitsOnly = /^\d+$\n/gm;
 
@@ -24,8 +23,6 @@ export default {
   trackingEventName: EXPLAIN_CODE_TRACKING_EVENT_NAME,
   components: {
     GlButton,
-    AiGenieChat,
-    CodeBlockHighlighted,
   },
   directives: {
     SafeHtml,
@@ -37,7 +34,7 @@ export default {
       trackingEventName: EXPLAIN_CODE_TRACKING_EVENT_NAME,
     };
   },
-  inject: ['resourceId', 'userId'],
+  inject: ['resourceId'],
   props: {
     containerSelector: {
       type: String,
@@ -49,65 +46,19 @@ export default {
       default: '',
     },
   },
-  apollo: {
-    // https://apollo.vuejs.org/guide/apollo/subscriptions.html#simple-subscription
-    $subscribe: {
-      aiCompletionResponse: {
-        query: aiResponseSubscription,
-        variables() {
-          return {
-            resourceId: this.resourceId,
-            userId: this.userId,
-          };
-        },
-        result({ data }) {
-          // When we first subscribe, we will receive a null aiCompletionResponse. We do nothing in this case.
-          const { errors = [], content } = data.aiCompletionResponse || {};
-
-          if (errors.length) {
-            this.isLoading = false;
-            this.codeExplanationError = this.$options.i18n.REQUEST_ERROR;
-          } else if (content) {
-            this.isLoading = false;
-            this.messages.push({
-              role: GENIE_CHAT_MODEL_ROLES.assistant,
-              content,
-            });
-          }
-        },
-        error() {
-          this.codeExplanationError = this.$options.i18n.REQUEST_ERROR;
-          this.isLoading = false;
-        },
-      },
-    },
-  },
   data() {
     return {
-      isLoading: false,
-      codeExplanationError: '',
       selectedText: '',
-      snippetLanguage: 'plaintext',
       shouldShowButton: false,
       container: null,
       top: null,
-      messages: [],
       lineHighlighter: null,
     };
   },
   computed: {
-    shouldShowChat() {
-      return this.isLoading || Boolean(this.messages.length) || Boolean(this.codeExplanationError);
-    },
     rootStyle() {
       if (!this.top) return null;
       return { top: `${this.top}px` };
-    },
-    filteredMessages() {
-      return this.messages?.slice(2) || []; // drop the `system` and the first `user` prompts
-    },
-    isChatAvailable() {
-      return this.glFeatures.explainCodeChat && this.messages.length > 0;
     },
   },
   created() {
@@ -126,7 +77,6 @@ export default {
       if (!this.container) {
         throw new Error(this.$options.i18n.GENIE_NO_CONTAINER_ERROR);
       }
-      this.snippetLanguage = this.container.querySelector('[lang]')?.lang || this.snippetLanguage;
       const selection = window.getSelection();
       if (this.isWithinContainer(selection)) {
         this.setPosition(selection);
@@ -153,14 +103,31 @@ export default {
       this.top = Math.min(startSelectionTop, finishSelectionTop) - containerTop + containerOffset;
     },
     requestCodeExplanation() {
-      this.messages = [];
-      this.codeExplanationError = '';
       this.selectedText = window.getSelection().toString().replace(linesWithDigitsOnly, '');
 
       this.setHighlightedLines();
 
-      const prompt = generateExplainCodePrompt(this.selectedText, this.filePath);
-      this.chat(prompt);
+      helpCenterState.showTanukiBotChatDrawer = true;
+
+      this.$apollo
+        .mutate({
+          mutation: chatMutation,
+          variables: {
+            question: GENIE_CHAT_EXPLAIN_MESSAGE,
+            resourceId: this.resourceId,
+            currentFileContext: {
+              fileName: this.filePath,
+              selectedText: this.selectedText,
+            },
+          },
+        })
+        .catch((error) => {
+          createAlert({
+            message: s__('AI|An error occurred while explaining the code.'),
+            captureError: true,
+            error,
+          });
+        });
     },
     setHighlightedLines() {
       const getSelection = window.getSelection();
@@ -181,29 +148,6 @@ export default {
       window.getSelection()?.removeAllRanges();
       this.lineHighlighter.clearHighlight();
     },
-    chat(prompt) {
-      this.isLoading = true;
-      const handleError = (err) => {
-        this.codeExplanationError = err?.message || this.$options.i18n.REQUEST_ERROR;
-        this.isLoading = false;
-      };
-      try {
-        this.messages = generateChatPrompt(prompt, this.messages);
-        this.$apollo
-          .mutate({
-            mutation: explainCodeMutation,
-            variables: {
-              messages: this.messages,
-              resourceId: this.resourceId,
-            },
-          })
-          .catch((err) => {
-            handleError(err);
-          });
-      } catch (err) {
-        handleError(err);
-      }
-    },
   },
 };
 </script>
@@ -221,26 +165,5 @@ export default {
       class="gl-p-0! gl-display-block gl-bg-white! explain-the-code gl-rounded-full!"
       @click="requestCodeExplanation"
     />
-    <ai-genie-chat
-      v-if="shouldShowChat"
-      :is-chat-available="isChatAvailable"
-      :is-loading="isLoading"
-      :messages="filteredMessages"
-      :error="codeExplanationError"
-      @send-chat-prompt="chat"
-      @chat-hidden="clearHighlightedLines"
-    >
-      <template #title>
-        {{ $options.i18n.GENIE_CHAT_TITLE }}
-      </template>
-      <template #hero>
-        <code-block-highlighted
-          :language="snippetLanguage"
-          :code="selectedText"
-          max-height="20rem"
-          class="gl-border-t gl-border-b gl-rounded-0! gl-mb-0 gl-overflow-y-auto"
-        />
-      </template>
-    </ai-genie-chat>
   </div>
 </template>
