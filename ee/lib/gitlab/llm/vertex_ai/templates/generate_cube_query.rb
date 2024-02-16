465af3ad078d6f60aff3db1070f0e7aecf7fbd90
Elwyn Benson <ebenson@gitlab.com>
2024-02-16 15:21:23 +0000

    Add extra descriptions to dimensions
    LLM was confused about different URL dimensions, and was not reliably
    understanding that "agentName" means browser

--- ee/lib/gitlab/llm/vertex_ai/templates/generate_cube_query.rb
+++ ee/lib/gitlab/llm/vertex_ai/templates/generate_cube_query.rb
@@ -25,12 +25,23 @@ order: An object, where the keys are measures or dimensions to order by and thei
 
 "TrackedEvents.pageViewsCount" which counts the number of page views
 "TrackedEvents.uniqueUsersCount" which counts the number of unique users
+"TrackedEvents.linkClicksCount" which counts the number of clicks on links
+"TrackedEvents.count" which counts the number of events
+"Sessions.count" which counts the number of sessions
+"Sessions.averagePerUser" which counts the average number of sessions per user
+"Sessions.averageDurationMinutes" which counts the average duration of sessions in minutes
+"Sessions.uniqueUsersCount" which counts the number of unique users who had sessions
+"Sessions.usersCount" which counts the number of non-unique users who had sessions
+"ReturningUsers.allSessionsCount" which counts the number of sessions for returning users
+"ReturningUsers.returningUserPercentage" which counts the percentage of returning users.
 
 "dimensions" must be zero or more of the following:
 
 "TrackedEvents.pageUrlhosts"
-"TrackedEvents.pageUrlpath"
+"TrackedEvents.pageUrlpath" which refers to a page path or unique page
 "TrackedEvents.event"
+"TrackedEvents.eventId"
+"TrackedEvents.eventName"
 "TrackedEvents.pageTitle"
 "TrackedEvents.osFamily"
 "TrackedEvents.osName"
@@ -39,12 +50,33 @@ order: An object, where the keys are measures or dimensions to order by and thei
 "TrackedEvents.agentName"
 "TrackedEvents.agentVersion"
 "TrackedEvents.pageReferrer"
-"TrackedEvents.pageUrl"
+"TrackedEvents.pageUrl" which is a full url
+"TrackedEvents.baseUrl" which is the protocol, host and port of the url
 "TrackedEvents.useragent"
 "TrackedEvents.derivedTstamp"
 "TrackedEvents.browserLanguage"
+"TrackedEvents.documentLanguage"
 "TrackedEvents.viewportSize"
+"TrackedEvents.targetUrl"
+"TrackedEvents.elementId"
+"TrackedEvents.customEventName"
+"TrackedEvents.customEventProps"
+"TrackedEvents.customUserProps"
 "TrackedEvents.userId"
+"Sessions.startAt"
+"Sessions.userId"
+"Sessions.sessionID"
+"Sessions.endAt"
+"Sessions.agentName" which is the browser or other agent triggering the event
+"Sessions.osFamily"
+"Sessions.osName"
+"Sessions.osVersion"
+"Sessions.osVersionMajor"
+"Sessions.agentVersion"
+"Sessions.browserLanguage"
+"Sessions.documentLanguage"
+"Sessions.viewportSize"
+"ReturningUsers.first_timestamp"
 
 "timeDimensions" is an array of zero or more JSON objects with the following root keys, all of which are mandatory:
 
@@ -52,6 +84,7 @@ order: An object, where the keys are measures or dimensions to order by and thei
 "dateRange" is an array of two dates between which data is returned. They MUST be in the format YYYY-MM-DD. Alternatively "date range" can be a single string to represent a date range such as "last week" or "this month".
 "granularity" is the granularity of the results. Pick something sensible for this value from "hour", "day", "week" or "month"
 
+Do not include "null" in any part of your response.
 
 The question you need to answer is "#{@user_input}"
 
