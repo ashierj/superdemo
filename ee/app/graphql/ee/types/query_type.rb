99907fa1e469efdb828d6e43aad19b326fcbdaa4
Russell Dickenson <rdickenson@gitlab.com>
2024-02-16 13:46:30 +1000

    Update description of GitLab Duo Vulnerability explanation

--- ee/app/graphql/ee/types/query_type.rb
+++ ee/app/graphql/ee/types/query_type.rb
@@ -39,7 +39,7 @@ module EE
           null: true,
           calls_gitaly: true,
           alpha: { milestone: '16.2' },
-          description: "GitLab Duo Vulnerability summary prompt for a specified vulnerability",
+          description: "GitLab Duo Vulnerability explanation prompt for a specified vulnerability",
           resolver: ::Resolvers::Ai::ExplainVulnerabilityPromptResolver
         field :geo_node, ::Types::Geo::GeoNodeType,
           null: true,
