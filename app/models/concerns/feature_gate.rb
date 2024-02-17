849fc47d8727d5dc717a358e51c69f2ee8acec79
Brian Williams <bwilliams@gitlab.com>
2024-02-17 08:14:20 -0600

    Add `.actor_from_id` to check feature flags with model id
    Internally, flipper checks the model class name and model id in order to
    determine if a feature is enabled for an actor. If we already have an
    actor's ID, there is no need to make a database query for that actor for
    the sole purpose of performing a feature flag check. Here we add the
    `actor_from_id` method to models, which returns a wrapper around the
    class name and model ID that builds a `flipper_id`. This lets us perform
    one query for a feature flag check instead of two. The only downside of
    this approach is that we might perform feature flag checks for actors
    which no longer exist. I think this downside is negligible because:
    
    1. If an administrator explicitly enabled the flag for a given actor,
       then people must be using that object and it's very unlikely for it
       to be deleted.
    2. In the contexts where we have an ID and only want to perform a
       feature flag check, we are probably passing that ID downstream to
       another process (like a sidekiq worker) and that process will perform
       an existence check on the object.

--- app/models/concerns/feature_gate.rb
+++ app/models/concerns/feature_gate.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module FeatureGate
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def actor_from_id(model_id)
+      ::Feature::ActorWrapper.new(self, model_id)
+    end
+  end
+
   def flipper_id
     return if new_record?
 
--- spec/models/concerns/feature_gate_spec.rb
+++ spec/models/concerns/feature_gate_spec.rb
@@ -3,6 +3,24 @@
 require 'spec_helper'
 
 RSpec.describe FeatureGate do
+  describe '.actor_from_id' do
+    using RSpec::Parameterized::TableSyntax
+
+    subject(:actor_from_id) { model_class.actor_from_id(model_id) }
+
+    where(:model_class, :model_id, :expected) do
+      Project | 1 | 'Project:1'
+      Group   | 2 | 'Group:2'
+      User    | 3 | 'User:3'
+    end
+
+    with_them do
+      it 'returns an object that has the correct flipper_id' do
+        expect(actor_from_id).to have_attributes(flipper_id: expected)
+      end
+    end
+  end
+
   describe 'User' do
     describe '#flipper_id' do
       context 'when user is not persisted' do
