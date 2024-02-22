08b3f1a836293ba1c6b049b0caeae5b7c61103c5
Marc Saleiko <msaleiko@gitlab.com>
2024-02-22 16:34:18 +0000

    Adds Ai agents debug chat interface

--- ee/app/assets/javascripts/ml/ai_agents/views/show_agent.vue
+++ ee/app/assets/javascripts/ml/ai_agents/views/show_agent.vue
@@ -1,23 +1,95 @@
 <script>
-import { GlExperimentBadge } from '@gitlab/ui';
+import { GlExperimentBadge, GlDuoChat } from '@gitlab/ui';
+import aiResponseSubscription from 'ee/graphql_shared/subscriptions/ai_completion_response.subscription.graphql';
 import TitleArea from '~/vue_shared/components/registry/title_area.vue';
+import { renderMarkdown } from '~/notes/utils';
+import { renderGFM } from '~/behaviors/markdown/render_gfm';
 import { sprintf, s__ } from '~/locale';
+import { GENIE_CHAT_MODEL_ROLES } from 'ee/ai/constants';
+import chatMutation from 'ee/ai/graphql/chat.mutation.graphql';
 
 export default {
   name: 'ShowAiAgent',
+  // Needed to override the default predefined prompts
+  predefinedPrompts: [],
   components: {
     TitleArea,
     GlExperimentBadge,
+    GlDuoChat,
   },
   provide() {
     return {
       projectPath: this.projectPath,
+      userId: this.userId,
+      renderMarkdown,
+      renderGFM,
     };
   },
-  inject: ['projectPath'],
+  inject: ['projectPath', 'userId'],
+  apollo: {
+    // https://apollo.vuejs.org/guide-option/subscriptions.html#simple-subscription
+    $subscribe: {
+      aiCompletionResponse: {
+        query: aiResponseSubscription,
+        variables() {
+          return {
+            userId: this.userId,
+            agentVersionId: `gid://gitlab/Ai::AgentVersion/${this.$route.params.agentId}`,
+            aiAction: 'CHAT',
+          };
+        },
+        result({ data }) {
+          const response = data?.aiCompletionResponse;
+
+          if (!response) {
+            return;
+          }
+
+          this.messages.push(response);
+
+          if (response.role.toLowerCase() === GENIE_CHAT_MODEL_ROLES.assistant) {
+            this.isLoading = false;
+          }
+        },
+        error(err) {
+          this.error = err.toString();
+        },
+      },
+    },
+  },
+  data() {
+    return {
+      error: null,
+      messages: [],
+      isLoading: false,
+    };
+  },
   computed: {
     title() {
       return sprintf(s__('AIAgent|AI Agent: %{agentId}'), { agentId: this.$route.params.agentId });
+    },
+  },
+  methods: {
+    onSendChatPrompt(question = '') {
+      this.isLoading = true;
+
+      this.$apollo
+        .mutate({
+          mutation: chatMutation,
+          variables: {
+            question,
+            resourceId: this.userId,
+            agentVersionId: `gid://gitlab/Ai::AgentVersion/${this.$route.params.agentId}`,
+          },
+        })
+        .then(() => {
+          // we add the user message in the aiCompletionResponse subscription
+          this.isLoading = true;
+        })
+        .catch((err) => {
+          this.error = err.toString();
+          this.isLoading = false;
+        });
     },
   },
 };
@@ -33,5 +105,21 @@ export default {
         </div>
       </template>
     </title-area>
+
+    <gl-duo-chat
+      :messages="messages"
+      :error="error"
+      :is-loading="isLoading"
+      :predefined-prompts="$options.predefinedPrompts"
+      :tool-name="s__('AIAgent|Agent')"
+      class="ai-agent-chat gl-w-full! gl-static gl-border-r gl-border-transparent"
+      :empty-state-title="s__('AIAgent|Try out your agent')"
+      :empty-state-description="
+        s__('AIAgent|Your agent\'s system prompt will be applied to the chat input.')
+      "
+      :chat-prompt-placeholder="s__('AIAgent|Ask your agent')"
+      :show-header="false"
+      @send-chat-prompt="onSendChatPrompt"
+    />
   </div>
 </template>
--- ee/spec/frontend/ml/ai_agents/views/show_agent_spec.js
+++ ee/spec/frontend/ml/ai_agents/views/show_agent_spec.js
@@ -1,34 +1,126 @@
-import { GlExperimentBadge } from '@gitlab/ui';
+import { GlDuoChat, GlExperimentBadge } from '@gitlab/ui';
+import Vue, { nextTick } from 'vue';
+import VueApollo from 'vue-apollo';
 import { shallowMountExtended } from 'helpers/vue_test_utils_helper';
+import createMockApollo from 'helpers/mock_apollo_helper';
+import { getMarkdown } from '~/rest_api';
 import ShowAgent from 'ee/ml/ai_agents/views/show_agent.vue';
 import TitleArea from '~/vue_shared/components/registry/title_area.vue';
+import aiResponseSubscription from 'ee/graphql_shared/subscriptions/ai_completion_response.subscription.graphql';
+import waitForPromises from 'helpers/wait_for_promises';
+import chatMutation from 'ee/ai/graphql/chat.mutation.graphql';
+import {
+  MOCK_USER_MESSAGE,
+  MOCK_USER_ID,
+  MOCK_TANUKI_SUCCESS_RES,
+  MOCK_TANUKI_BOT_MUTATATION_RES,
+} from '../../../ai/tanuki_bot/mock_data';
 
-let wrapper;
+Vue.use(VueApollo);
 
-const createWrapper = () => {
-  wrapper = shallowMountExtended(ShowAgent, {
-    provide: { projectPath: 'path/to/project' },
-    mocks: {
-      $route: {
-        params: {
-          agentId: 2,
-        },
-      },
-    },
-  });
-};
-
-const findTitleArea = () => wrapper.findComponent(TitleArea);
-const findBadge = () => wrapper.findComponent(GlExperimentBadge);
+jest.mock('~/rest_api');
 
 describe('ee/ml/ai_agents/views/create_agent', () => {
-  beforeEach(() => createWrapper());
+  let wrapper;
 
-  it('shows the title', () => {
-    expect(findTitleArea().text()).toContain('AI Agent: 2');
+  const subscriptionHandlerMock = jest.fn().mockResolvedValue(MOCK_TANUKI_SUCCESS_RES);
+  const chatMutationHandlerMock = jest.fn().mockResolvedValue(MOCK_TANUKI_BOT_MUTATATION_RES);
+  const agentId = 2;
+
+  const findTitleArea = () => wrapper.findComponent(TitleArea);
+  const findBadge = () => wrapper.findComponent(GlExperimentBadge);
+  const findGlDuoChat = () => wrapper.findComponent(GlDuoChat);
+
+  const createWrapper = () => {
+    const apolloProvider = createMockApollo([
+      [aiResponseSubscription, subscriptionHandlerMock],
+      [chatMutation, chatMutationHandlerMock],
+    ]);
+
+    wrapper = shallowMountExtended(ShowAgent, {
+      apolloProvider,
+      provide: { projectPath: 'path/to/project', userId: MOCK_USER_ID },
+      mocks: {
+        $route: {
+          params: {
+            agentId,
+          },
+        },
+      },
+    });
+  };
+
+  beforeEach(() => {
+    getMarkdown.mockImplementation(({ text }) => Promise.resolve({ data: { html: text } }));
   });
 
-  it('displays the experiment badge', () => {
-    expect(findBadge().exists()).toBe(true);
+  describe('rendering', () => {
+    beforeEach(() => {
+      createWrapper();
+    });
+
+    it('shows the title', () => {
+      expect(findTitleArea().text()).toContain('AI Agent: 2');
+    });
+
+    it('displays the experiment badge', () => {
+      expect(findBadge().exists()).toBe(true);
+    });
+
+    it('renders the DuoChat component', () => {
+      expect(findGlDuoChat().exists()).toBe(true);
+    });
+  });
+
+  describe('@send-chat-prompt', () => {
+    beforeEach(() => {
+      createWrapper();
+    });
+
+    it('does set loading to `true` for a user message', async () => {
+      findGlDuoChat().vm.$emit('send-chat-prompt', MOCK_USER_MESSAGE.content);
+      await nextTick();
+      expect(findGlDuoChat().props('isLoading')).toBe(true);
+    });
+
+    it('calls correct GraphQL mutation', async () => {
+      findGlDuoChat().vm.$emit('send-chat-prompt', MOCK_USER_MESSAGE.content);
+      await nextTick();
+      expect(chatMutationHandlerMock).toHaveBeenCalledWith({
+        resourceId: MOCK_USER_ID,
+        agentVersionId: `gid://gitlab/Ai::AgentVersion/${agentId}`,
+        question: MOCK_USER_MESSAGE.content,
+      });
+    });
+  });
+
+  describe('Error conditions', () => {
+    const errorText = 'Fancy foo';
+
+    describe('when subscription fails', () => {
+      beforeEach(async () => {
+        subscriptionHandlerMock.mockRejectedValue(new Error(errorText));
+        createWrapper();
+        await waitForPromises();
+      });
+
+      it('throws error and displays error message', () => {
+        expect(findGlDuoChat().props('error')).toBe(`Error: ${errorText}`);
+      });
+    });
+
+    describe('when mutation fails', () => {
+      beforeEach(async () => {
+        chatMutationHandlerMock.mockRejectedValue(new Error(errorText));
+        createWrapper();
+        await waitForPromises();
+        findGlDuoChat().vm.$emit('send-chat-prompt', MOCK_USER_MESSAGE.content);
+        await waitForPromises();
+      });
+
+      it('throws error and displays error message', () => {
+        expect(findGlDuoChat().props('error')).toBe(`Error: ${errorText}`);
+      });
+    });
   });
 });
