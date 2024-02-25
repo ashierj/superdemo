1193fec86a5b0442ab5f949e56ebad2cc3ae0b38
Gabriel Mazetto <gabriel@gitlab.com>
2024-02-25 23:30:10 +0100

    Introduce Output class to centralise output formats

--- gems/gitlab-backup-cli/lib/gitlab/backup/cli.rb
+++ gems/gitlab-backup-cli/lib/gitlab/backup/cli.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+require 'active_support/all'
+require 'rainbow/refinement'
+
 module Gitlab
   module Backup
     # GitLab Backup CLI
     module Cli
       autoload :VERSION, 'gitlab/backup/cli/version'
+      autoload :Output, 'gitlab/backup/cli/output'
       autoload :Runner, 'gitlab/backup/cli/runner'
       autoload :Utils, 'gitlab/backup/cli/utils'
       autoload :Dependencies, 'gitlab/backup/cli/dependencies'
