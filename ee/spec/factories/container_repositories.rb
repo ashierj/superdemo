7dce926636d80e26c4e99ac74c09c3795063151a
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2024-02-26 20:09:05 -0300

    Fix container repositories checksum mismatch errors
    This commit adds back the method to schedules a verification
    job after a model record is created/updated.
    
    Changelog: fixed
    EE: true

--- ee/spec/factories/container_repositories.rb
+++ ee/spec/factories/container_repositories.rb
@@ -5,11 +5,29 @@ FactoryBot.modify do
     trait :verification_succeeded do
       verification_checksum { 'abc' }
       verification_state { ContainerRepository.verification_state_value(:verification_succeeded) }
+
+      #
+      # Geo::VerifiableReplicator#after_verifiable_update tries to verify
+      # the replicable async and marks it as verification started when the
+      # model record is created/updated.
+      #
+      after(:create) do |instance, _|
+        instance.verification_succeeded!
+      end
     end
 
     trait :verification_failed do
       verification_failure { 'Could not calculate the checksum' }
       verification_state { ContainerRepository.verification_state_value(:verification_failed) }
+
+      #
+      # Geo::VerifiableReplicator#after_verifiable_update tries to verify
+      # the replicable async and marks it as verification started when the
+      # model record is created/updated.
+      #
+      after(:create) do |instance, _|
+        instance.verification_failed!
+      end
     end
   end
 end
