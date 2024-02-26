fdba060146a5952726d02d036eca8581ba6b022e
Andrew Evans <aevans@gitlab.com>
2024-02-26 15:10:22 +0000

    Additional cleanup for external usernames on oAuth and LDAP sign up
    Currently if a user joins a GitLab instance via oAuth, LDAP, or SCIM and
    their username begins with multiple `--` characters, these characters
    are removed to ensure a valid username can be created for the new user.
    Illegal characters such as `*&^()` are removed, and illegal extensions
    such as `.git` and `.atom` are also removed.
    
    This change extends the behavior to include all leading legal
    characters: `-` , `_` and `.` . It also trims from the end of the
    potential username, and removes consecutive legal characters so the
    resulting username will pass the check for
    `Gitlab::Regex.oci_repository_path_regex` .
    
    Changelog: changed

--- ee/spec/lib/ee/gitlab/scim/group/provisioning_service_spec.rb
+++ ee/spec/lib/ee/gitlab/scim/group/provisioning_service_spec.rb
@@ -143,6 +143,36 @@ RSpec.describe ::EE::Gitlab::Scim::Group::ProvisioningService, :saas,
           expect(user).to be_confirmed
         end
       end
+
+      context 'when username contains special characters' do
+        subject(:result) { service.execute }
+
+        let_it_be(:service_params) do
+          {
+            email: 'work@example.com',
+            name: 'Test Name',
+            extern_uid: 'test_uid',
+            username: ' --ricky.^#!__the._raccoon--'
+          }
+        end
+
+        it 'sanitizes more special characters from the username' do
+          expect { result }.to change { User.count }.by(1)
+          expect(user.username).to eq('ricky.the.raccoon')
+        end
+
+        context 'and extra_slug_sanitization FF is disabled' do
+          before do
+            stub_feature_flags(extra_slug_path_sanitization: false)
+          end
+
+          it 'cannot create a user due to invalid username' do
+            expect { result }.not_to change { User.count }
+            expect(result.status).to eq(:error)
+            expect(result.message).to include(Gitlab::Regex.oci_repository_path_regex_message)
+          end
+        end
+      end
     end
 
     context 'when a provisioning error occurs' do
