d8e65143b0e464cfc0aa1a1d7c51250f331e48bc
Andrei Zubov <azubov@gitlab.com>
2024-02-27 21:15:00 +0000

    Add option to keep shared groups when filtered by parent
    Now it is possible to include invited groups when searching for groups
    by parent. This will allow users to select invited groups when
    protecting an environment
    
    Changelog: changed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/141462
    EE: true

--- ee/spec/finders/autocomplete/group_subgroups_finder_spec.rb
+++ ee/spec/finders/autocomplete/group_subgroups_finder_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe Autocomplete::GroupSubgroupsFinder do
+RSpec.describe Autocomplete::GroupSubgroupsFinder, feature_category: :groups_and_projects do
   describe '#execute' do
     let_it_be(:group) { create(:group, :private) }
     let_it_be(:subgroup_1) { create(:group, :private, parent: group) }
@@ -10,15 +10,38 @@ RSpec.describe Autocomplete::GroupSubgroupsFinder do
     let_it_be(:grandchild_1) { create(:group, :private, parent: subgroup_1) }
     let_it_be(:member_in_group) { create(:user).tap { |u| group.add_reporter(u) } }
     let_it_be(:member_in_subgroup) { create(:user).tap { |u| subgroup_1.add_reporter(u) } }
+    let_it_be(:invited_to_group) { create(:group, :public) }
+    let_it_be(:invited_to_subgroup) { create(:group, :public) }
 
     let(:params) { { group_id: group.id } }
     let(:current_user) { member_in_group }
+
+    before do
+      group.shared_with_groups << invited_to_group
+      grandchild_1.shared_with_groups << invited_to_subgroup
+    end
 
     subject { described_class.new(current_user, params).execute }
 
     it 'returns subgroups', :aggregate_failures do
       expect(subject.count).to eq(2)
       expect(subject).to contain_exactly(subgroup_1, subgroup_2)
+    end
+
+    context 'when include_parent_shared_groups parameter is true' do
+      before do
+        params[:include_parent_shared_groups] = true
+        params[:include_parent_descendants] = true
+      end
+
+      it 'returns subgroups and shared groups' do
+        expect(subject.count).to eq(4)
+        expect(subject).to contain_exactly(
+          subgroup_1,
+          subgroup_2,
+          grandchild_1,
+          invited_to_group)
+      end
     end
 
     context 'when include_parent_descendants parameter is true' do
