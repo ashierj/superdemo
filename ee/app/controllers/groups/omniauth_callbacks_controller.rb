6044f3ded07306227f560a54f9f025e7c7a344d1
Aboobacker MK <akarakath@gitlab.com>
2024-02-28 05:51:17 +0000

    Revert "Merge branch 'filter_saml_response_custom' into 'master'"
    This reverts merge request !144732

--- ee/app/controllers/groups/omniauth_callbacks_controller.rb
+++ ee/app/controllers/groups/omniauth_callbacks_controller.rb
@@ -6,8 +6,6 @@ class Groups::OmniauthCallbacksController < OmniauthCallbacksController
 
   skip_before_action :verify_authenticity_token, only: [:failure, :group_saml]
 
-  before_action :log_saml_response, only: [:group_saml]
-
   feature_category :system_access
   urgency :low
 
@@ -191,9 +189,5 @@ class Groups::OmniauthCallbacksController < OmniauthCallbacksController
     }
 
     ::Gitlab::Audit::Auditor.audit(audit_context)
-  end
-
-  def log_saml_response
-    Gitlab::AuthLogger.info(payload_type: 'saml_response', saml_response: ParameterFilters::SamlResponse.filter(params['SAMLResponse'].dup))
   end
 end
