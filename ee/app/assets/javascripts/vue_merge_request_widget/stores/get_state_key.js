4e8e7a6cc638a06caa5d8e9e2497a201c1cba6b7
Marc Shaw <mshaw@gitlab.com>
2024-03-01 09:46:28 +0900

    Finish removing the policy mergeability check
    MR: gitlab.com/gitlab-org/gitlab/-/merge_requests/146278

--- ee/app/assets/javascripts/vue_merge_request_widget/stores/get_state_key.js
+++ ee/app/assets/javascripts/vue_merge_request_widget/stores/get_state_key.js
@@ -1,15 +1,11 @@
 import CEGetStateKey from '~/vue_merge_request_widget/stores/get_state_key';
-import { DETAILED_MERGE_STATUS, MWCP_MERGE_STRATEGY } from '~/vue_merge_request_widget/constants';
+import { MWCP_MERGE_STRATEGY } from '~/vue_merge_request_widget/constants';
 import { stateKey as CEStateKey } from '~/vue_merge_request_widget/stores/state_maps';
 import { stateKey } from './state_maps';
 
 export default function getStateKey() {
   if (this.isGeoSecondaryNode) {
     return 'geoSecondaryNode';
-  }
-
-  if (this.detailedMergeStatus === DETAILED_MERGE_STATUS.POLICIES_DENIED) {
-    return stateKey.policyViolation;
   }
 
   if (this.jiraAssociation?.enforced && this.jiraAssociation?.issue_keys.length === 0) {
