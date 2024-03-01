212283a3a27ee58c0ff2ffc6a47e2325403044c8
Jarka Košanová <jarka@gitlab.com>
2024-03-01 13:47:33 +0100

    Remove assign_compliance_project_service feature flag
    Changelog: other
    EE: true

--- ee/spec/features/projects/settings/ee/assign_compliance_framework_spec.rb
+++ ee/spec/features/projects/settings/ee/assign_compliance_framework_spec.rb
@@ -8,47 +8,29 @@ RSpec.describe 'Assign compliance framework', feature_category: :compliance_mana
   let_it_be(:user) { create(:user) }
   let_it_be(:framework) { create(:compliance_framework, namespace: project.group) }
 
-  shared_examples 'assigning a compliance framework' do
-    before_all do
-      group.add_owner(user)
-    end
-
-    before do
-      stub_licensed_features(compliance_framework: true, custom_compliance_frameworks: true)
-
-      sign_in(user)
-    end
-
-    it 'assigns a compliance framework' do
-      visit edit_project_path(project)
-
-      page.within('.compliance-framework') do
-        framework_select = find('select')
-        framework_select.select(framework.name)
-
-        click_button 'Save changes'
-        wait_for_requests
-
-        expect(page).to have_content(framework.name)
-      end
-
-      expect(page).to have_content("'#{project.name}' was successfully updated.")
-    end
+  before_all do
+    group.add_owner(user)
   end
 
-  context 'with assign_compliance_project_service feature enabled' do
-    before do
-      stub_feature_flags(assign_compliance_project_service: true)
-    end
+  before do
+    stub_licensed_features(compliance_framework: true, custom_compliance_frameworks: true)
 
-    it_behaves_like 'assigning a compliance framework'
+    sign_in(user)
   end
 
-  context 'with assign_compliance_project_service feature disabled' do
-    before do
-      stub_feature_flags(assign_compliance_project_service: false)
+  it 'assigns a compliance framework' do
+    visit edit_project_path(project)
+
+    page.within('.compliance-framework') do
+      framework_select = find('select')
+      framework_select.select(framework.name)
+
+      click_button 'Save changes'
+      wait_for_requests
+
+      expect(page).to have_content(framework.name)
     end
 
-    it_behaves_like 'assigning a compliance framework'
+    expect(page).to have_content("'#{project.name}' was successfully updated.")
   end
 end
