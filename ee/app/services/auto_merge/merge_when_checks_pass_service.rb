019d917d4541e5963d22e2045cdc182fa749c63b
Marc Shaw <mshaw@gitlab.com>
2024-03-01 12:37:40 +0900

    Auto Merge Refactor: Remove MWCP inheriting from MWPS
    MR: gitlab.com/gitlab-org/gitlab/-/merge_requests/146286

--- ee/app/services/auto_merge/merge_when_checks_pass_service.rb
+++ ee/app/services/auto_merge/merge_when_checks_pass_service.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 module AutoMerge
-  class MergeWhenChecksPassService < AutoMerge::MergeWhenPipelineSucceedsService
+  class MergeWhenChecksPassService < AutoMerge::BaseService
     extend Gitlab::Utils::Override
+
+    def execute(merge_request)
+      super do
+        add_system_note(merge_request)
+      end
+    end
 
     override :process
     def process(merge_request)
@@ -39,6 +45,12 @@ module AutoMerge
       end
     end
 
+    def available_for?(merge_request)
+      super do
+        check_availability(merge_request)
+      end
+    end
+
     private
 
     def add_system_note(merge_request)
@@ -52,13 +64,12 @@ module AutoMerge
       )
     end
 
-    override :check_availability
     def check_availability(merge_request)
       return false if Feature.disabled?(:merge_when_checks_pass, merge_request.project)
       return false unless merge_request.approval_feature_available?
       return false if merge_request.project.merge_trains_enabled?
 
-      super ||
+      merge_request.actual_head_pipeline&.active? ||
         !merge_request.approved? ||
         (Feature.enabled?(:additional_merge_when_checks_ready, merge_request.project) &&
          (merge_request.draft? ||
@@ -66,6 +77,13 @@ module AutoMerge
           merge_request.merge_blocked_by_other_mrs? ||
           !merge_request.mergeable_discussions_state?)
         )
+    end
+
+    def notify(merge_request)
+      return unless merge_request.saved_change_to_auto_merge_enabled?
+
+      notification_service.async.merge_when_pipeline_succeeds(merge_request,
+        current_user)
     end
   end
 end
