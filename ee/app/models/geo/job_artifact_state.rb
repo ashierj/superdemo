3699e9824f98e9b462553413595848dadf8a4286
Panos Kanellidis <pkanellidis@gitlab.com>
2024-03-08 17:23:55 +0000

    Update job_artifacts queries to use partition_id filter
    Changelog: other

--- ee/app/models/geo/job_artifact_state.rb
+++ ee/app/models/geo/job_artifact_state.rb
@@ -8,6 +8,7 @@ module Geo
     self.primary_key = :job_artifact_id
 
     belongs_to :job_artifact, inverse_of: :job_artifact_state, class_name: 'Ci::JobArtifact'
+
     partitionable scope: :job_artifact
   end
 end
