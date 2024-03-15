75573d6a80a1e21d958ed5cb2faf708f231abc54
Joe Woodward <j@joewoodward.me>
2024-03-15 12:13:10 +0000

    Add mutation to edit approval rules
    Requires:
    - id which is a GlobalID
    - name
    - approvals_required
    - user_ids
    - group_ids
    
    Returns:
    - approval_rule which is an instance of ApprovalProjectRule
    
    Closes https://gitlab.com/gitlab-org/gitlab/-/issues/441289

--- ee/app/graphql/ee/types/mutation_type.rb
+++ ee/app/graphql/ee/types/mutation_type.rb
@@ -158,6 +158,7 @@ module EE
         mount_mutation ::Mutations::Groups::SavedReplies::Update, alpha: { milestone: '16.10' }
         mount_mutation ::Mutations::Groups::SavedReplies::Destroy, alpha: { milestone: '16.10' }
         mount_mutation ::Mutations::BranchRules::ApprovalProjectRules::Create, alpha: { milestone: '16.10' }
+        mount_mutation ::Mutations::ApprovalProjectRules::Update, alpha: { milestone: '16.10' }
 
         prepend(Types::DeprecatedMutations)
       end
