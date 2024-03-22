658242b2ff59c12345d2b46a13c47ade8f699291
Zack Cuddy <zcuddy@gitlab.com>
2024-03-22 10:48:34 +0000

    Organization Admin - Add badges to group list
    This change adds badges to the group
    list in the organization admin for
    groups that are pending deletion.

---
+++ ee/app/assets/javascripts/organizations/mock_groups.js
@@ -0,0 +1,12 @@
+// This is temporary mock data that will be removed when completing the following:
+// https://gitlab.com/gitlab-org/gitlab/-/issues/420777
+// https://gitlab.com/gitlab-org/gitlab/-/issues/421441
+
+import { organizationGroups as organizationGroupsCE } from '~/organizations/mock_groups';
+
+export const organizationGroups = organizationGroupsCE.map((group, index, array) => {
+  return {
+    ...group,
+    markedForDeletionOn: index === array.length - 1 ? '2024-01-01' : null,
+  };
+});
