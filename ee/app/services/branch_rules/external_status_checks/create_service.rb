6621fc8370e91d1df4a60dca6c23579983c37e2a
Patrick Cyiza <jpcyiza@gitlab.com>
2024-03-28 14:54:10 +0000

    Make GraphQL mutation ExternalStatusCheckCreate

---
+++ ee/app/services/branch_rules/external_status_checks/create_service.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module BranchRules
+  module ExternalStatusChecks
+    class CreateService < BaseService
+      PERMITTED_PARAMS = %i[name external_url].freeze
+
+      def execute(skip_authorization: false)
+        super
+      rescue Gitlab::Access::AccessDeniedError
+        ServiceResponse.error(
+          message: 'Failed to create rule',
+          payload: { errors: ['Not allowed'] },
+          reason: :access_denied
+        )
+      end
+
+      private
+
+      def authorized?
+        can?(current_user, :update_branch_rule, branch_rule)
+      end
+
+      def execute_on_branch_rule
+        ::ExternalStatusChecks::CreateService.new(
+          container: project,
+          current_user: current_user,
+          params: params.merge(protected_branch_ids: [branch_rule.id])
+        ).execute(skip_authorization: true)
+      end
+
+      def execute_on_all_branches_rule
+        ServiceResponse.error(message: 'All branch rules cannot configure external status checks')
+      end
+
+      def execute_on_all_protected_branches_rule
+        ServiceResponse.error(message: 'All protected branch rules cannot configure external status checks')
+      end
+    end
+  end
+end
---
+++ ee/spec/services/branch_rules/external_status_checks/create_service_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe BranchRules::ExternalStatusChecks::CreateService, feature_category: :source_code_management do
+  let_it_be(:project) { create(:project, :repository) }
+  let_it_be(:user) { create(:user) }
+  let_it_be(:protected_branch) { create(:protected_branch, project: project) }
+
+  let(:branch_rule) { Projects::BranchRule.new(project, protected_branch) }
+  let(:action_allowed) { true }
+  let(:create_service) { ExternalStatusChecks::CreateService }
+  let(:create_service_instance) { instance_double(update_service) }
+  let(:status_check_name) { 'Test' }
+  let(:params) { { name: status_check_name, external_url: 'https://external_url.text/hello.json' } }
+
+  subject(:execute) { described_class.new(branch_rule, user, params).execute }
+
+  before do
+    allow(Ability)
+      .to receive(:allowed?).with(user, :update_branch_rule, branch_rule)
+                            .and_return(action_allowed)
+
+    stub_licensed_features(audit_events: true)
+  end
+
+  it_behaves_like 'create external status services'
+
+  context 'when the given branch rule is not and instance of Projects::BranchRule' do
+    let(:branch_rule) { create(:protected_branch) }
+
+    it 'is unsuccessful' do
+      expect(execute.error?).to be true
+    end
+
+    it 'does not create a new rule' do
+      expect { execute }.not_to change { MergeRequests::ExternalStatusCheck.count }
+    end
+
+    it 'responds with the expected errors' do
+      expect(execute.message).to eq('Unknown branch rule type.')
+    end
+  end
+
+  context 'with ::Projects::AllBranchesRule' do
+    let(:branch_rule) { ::Projects::AllBranchesRule.new(project) }
+
+    it 'responds with the expected errors' do
+      expect(execute.error?).to be true
+      expect { execute }.not_to change { MergeRequests::ExternalStatusCheck.count }
+      expect(execute.message).to eq('All branch rules cannot configure external status checks')
+    end
+  end
+
+  context 'with ::Projects::AllProtectedBranchesRule' do
+    let(:branch_rule) { ::Projects::AllProtectedBranchesRule.new(project) }
+
+    it 'responds with the expected errors' do
+      expect(execute.error?).to be true
+      expect { execute }.not_to change { MergeRequests::ExternalStatusCheck.count }
+      expect(execute.message).to eq('All protected branch rules cannot configure external status checks')
+    end
+  end
+end
