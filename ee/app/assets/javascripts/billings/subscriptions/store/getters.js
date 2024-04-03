f956f13f0d21217f324f3c97755c7fffa17904c8
Laura Callahan <lmeckley@gitlab.com>
2024-04-03 14:31:29 +0000

    Alter billings table for ramon

--- ee/app/assets/javascripts/billings/subscriptions/store/getters.js
+++ ee/app/assets/javascripts/billings/subscriptions/store/getters.js
@@ -1,4 +1,9 @@
-import { TABLE_TYPE_DEFAULT, TABLE_TYPE_FREE, TABLE_TYPE_TRIAL } from 'ee/billings/constants';
+import {
+  TABLE_TYPE_DEFAULT,
+  TABLE_TYPE_FREE,
+  TABLE_TYPE_RAMON,
+  TABLE_TYPE_TRIAL,
+} from 'ee/billings/constants';
 
 export const isFreePlan = (state) => ['free', null].includes(state.plan.code);
 export const tableKey = (state) => {
@@ -7,6 +12,8 @@ export const tableKey = (state) => {
     key = TABLE_TYPE_FREE;
   } else if (state.plan.trial) {
     key = TABLE_TYPE_TRIAL;
+  } else if (state.plan.maximumSeatLimit) {
+    key = TABLE_TYPE_RAMON;
   }
 
   return key;
