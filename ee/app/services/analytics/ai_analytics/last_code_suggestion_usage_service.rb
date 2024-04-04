159a626524ff6193bc8577902f141e59561f48d7
Pavel Shutsin <pshutsin@gitlab.com>
2024-04-04 15:55:58 +0000

    Add DB structure for code suggestions
    This change adds DB tables for storing code suggestion usage data in
    ClickHouse.

---
+++ ee/app/services/analytics/ai_analytics/last_code_suggestion_usage_service.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Analytics
+  module AiAnalytics
+    class LastCodeSuggestionUsageService
+      def initialize(current_user, user_ids:, from:, to:)
+        @current_user = current_user
+        @user_ids = user_ids
+        @from = from
+        @to = to
+      end
+
+      # Return payload is a hash of {user_id => last_usage_date}
+      def execute
+        return feature_unavailable_error unless Gitlab::ClickHouse.globally_enabled_for_analytics?
+
+        ServiceResponse.success(payload: last_usages)
+      end
+
+      private
+
+      QUERY = <<~SQL
+        SELECT
+          max(timestamp) AS last_used_at, user_id
+        FROM code_suggestion_daily_usages
+          WHERE user_id IN ({user_ids:Array(UInt64)})
+          AND timestamp >= {from:Date}
+          AND timestamp <= {to:Date}
+        GROUP BY user_id
+      SQL
+
+      MAX_USER_IDS_SIZE = 10_000
+
+      attr_reader :current_user, :user_ids, :from, :to
+
+      def feature_unavailable_error
+        ServiceResponse.error(
+          message: s_('AiAnalytics|the ClickHouse data store is not available')
+        )
+      end
+
+      def last_usages
+        data = []
+
+        user_ids.each_slice(MAX_USER_IDS_SIZE).map do |user_ids_slice|
+          query = ClickHouse::Client::Query.new(
+            raw_query: QUERY,
+            placeholders: {
+              user_ids: user_ids_slice.to_json,
+              from: from.strftime('%Y-%m-%d'),
+              to: to.strftime('%Y-%m-%d')
+            })
+
+          data += ClickHouse::Client.select(query, :main)
+        end
+
+        data.to_h do |row|
+          [row['user_id'], DateTime.parse(row['last_used_at'])]
+        end
+      end
+    end
+  end
+end
