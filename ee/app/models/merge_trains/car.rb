09fcf24f778807dc4097b17b9088499a9a32234b
Tianwen Chen <tchen@gitlab.com>
2024-04-09 11:37:40 +0000

    Initialize bigint conversion for merge_trains
    This is the first step to convert pipeline_id related to bigint for:
    - merge_trains.pipeline_id
    
    See https://docs.gitlab.com/ee/development/database/avoiding_downtime_in_migrations.html#initialize-the-conversion-and-start-migrating-existing-data-release-n
    
    Changelog: added

--- ee/app/models/merge_trains/car.rb
+++ ee/app/models/merge_trains/car.rb
@@ -5,6 +5,9 @@ module MergeTrains
   class Car < ApplicationRecord
     include Gitlab::Utils::StrongMemoize
     include AfterCommitQueue
+    include IgnorableColumns
+
+    ignore_columns :pipeline_id_convert_to_bigint, remove_with: '17.1', remove_after: '2024-06-14'
 
     # For legacy reasons, each row is a merge train in the database
     self.table_name = 'merge_trains'
