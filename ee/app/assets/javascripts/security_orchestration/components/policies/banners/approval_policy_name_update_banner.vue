21782f49f506e21424294ef9ae73ad0bb3d2235a
Alan (Maciej) Paruszewski <mparuszewski@gitlab.com>
2024-04-10 16:09:27 +0000

    Add info about release notes for policy rename
    - Add link to banner
    - link to release post
    
    Changelog: changed
    EE: true

--- ee/app/assets/javascripts/security_orchestration/components/policies/banners/approval_policy_name_update_banner.vue
+++ ee/app/assets/javascripts/security_orchestration/components/policies/banners/approval_policy_name_update_banner.vue
@@ -1,19 +1,24 @@
 <script>
-import { GlAlert, GlSprintf } from '@gitlab/ui';
+import { GlAlert, GlLink, GlSprintf } from '@gitlab/ui';
 import LocalStorageSync from '~/vue_shared/components/local_storage_sync.vue';
 import { s__ } from '~/locale';
+import { PROMO_URL } from '~/lib/utils/url_utility';
 
 export default {
   BANNER_STORAGE_KEY: 'security_policies_scan_result_name_change',
   i18n: {
     migrationTitle: s__('SecurityOrchestration|Updated policy name'),
-    migrationDescription: s__(
-      'SecurityOrchestration|The %{oldNameStart}Scan result policy%{oldNameEnd} is now called the %{newNameStart}Merge request approval policy%{newNameEnd} to better align with its purpose.',
-    ),
+    migrationDescription: {
+      text: s__(
+        'SecurityOrchestration|The %{oldNameStart}Scan result policy%{oldNameEnd} is now called the %{newNameStart}Merge request approval policy%{newNameEnd} to better align with its purpose. For more details, see %{linkStart}the release notes%{linkEnd}.',
+      ),
+      href: `${PROMO_URL}/releases/2024/03/21/gitlab-16-10-released/#scan-result-policies-are-now-merge-request-approval-policies`,
+    },
   },
   name: 'ApprovalPolicyNameUpdateBanner',
   components: {
     GlAlert,
+    GlLink,
     GlSprintf,
     LocalStorageSync,
   },
@@ -38,12 +43,17 @@ export default {
       :title="$options.i18n.migrationTitle"
       @dismiss="dismissAlert"
     >
-      <gl-sprintf :message="$options.i18n.migrationDescription">
+      <gl-sprintf :message="$options.i18n.migrationDescription.text">
         <template #oldName="{ content }">
           <b>{{ content }}</b>
         </template>
         <template #newName="{ content }">
           <b>{{ content }}</b>
+        </template>
+        <template #link="{ content }">
+          <gl-link :href="$options.i18n.migrationDescription.href" target="_blank">{{
+            content
+          }}</gl-link>
         </template>
       </gl-sprintf>
     </gl-alert>
