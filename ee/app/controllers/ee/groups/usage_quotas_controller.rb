e4fbfd31e5ebe071a848a6c38c00bc9e355133fc
Max Woolf <mwoolf@gitlab.com>
2024-04-11 10:13:51 +0100

    Enabled product_analytics_billing by default
    Changelog: added
    EE: true

--- ee/app/controllers/ee/groups/usage_quotas_controller.rb
+++ ee/app/controllers/ee/groups/usage_quotas_controller.rb
@@ -16,7 +16,7 @@ module EE
           push_frontend_feature_flag(:limited_access_modal)
           push_frontend_feature_flag(:enable_add_on_users_filtering, group)
           push_frontend_feature_flag(:product_analytics_usage_quota_annual_data, group)
-          push_frontend_feature_flag(:product_analytics_billing, group, type: :wip)
+          push_frontend_feature_flag(:product_analytics_billing, group, type: :development)
           push_frontend_feature_flag(:product_analytics_billing_override, group, type: :wip)
         end
       end
