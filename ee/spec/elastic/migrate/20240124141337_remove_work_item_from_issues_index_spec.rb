7fc9441be3d88cafac24d5b972aa1cf9a176ae77
Rémy Coutable <remy@rymai.me>
2024-04-15 17:27:53 +0200

    Quarantine a flaky test
    The `RemoveWorkItemFromIssuesIndex.migrate migration process when documents are still present in the index removes documents from the index` test has been reported as flaky more then 1000 times.
    
    This MR quarantines the test. This is a discussion starting point to let the responsible group know about the flakiness
    so that they can take action:
    
    - accept the merge request and schedule to improve the test
    - close the merge request in favor of another merge request to delete the test
    
    Related to https://gitlab.com/gitlab-org/gitlab/-/issues/448370.
    
    This change was generated by
    [gitlab-housekeeper](https://gitlab.com/gitlab-org/gitlab/-/tree/master/gems/gitlab-housekeeper)
    using the Keeps::QuarantineFlakyTests keep.
    
    To provide feedback on your experience with `gitlab-housekeeper` please comment in
    <https://gitlab.com/gitlab-org/gitlab/-/issues/442003>.
    
    Changelog: other

--- ee/spec/elastic/migrate/20240124141337_remove_work_item_from_issues_index_spec.rb
+++ ee/spec/elastic/migrate/20240124141337_remove_work_item_from_issues_index_spec.rb
@@ -5,7 +5,8 @@ require_relative 'migration_shared_examples'
 require File.expand_path('ee/elastic/migrate/20240124141337_remove_work_item_from_issues_index.rb')
 
 RSpec.describe RemoveWorkItemFromIssuesIndex, :elastic, :sidekiq_inline, feature_category: :global_search do
-  include_examples 'migration deletes documents based on schema version' do
+  include_examples 'migration deletes documents based on schema version',
+    quarantine: 'https://gitlab.com/gitlab-org/gitlab/-/issues/448370' do
     let(:version) { 20240124141337 }
     let(:objects) { create_list(:work_item, 3) }
     let(:expected_throttle_delay) { 1.minute }
