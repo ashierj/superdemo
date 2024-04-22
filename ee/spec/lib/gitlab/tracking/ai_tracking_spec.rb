05720d70cabeeb6903e8e5e4b89fb9f8408116ab
Pavel Shutsin <pshutsin@gitlab.com>
2024-04-22 10:45:51 +0200

    Add ClickHouse check to AiMetrics data gathering
    We shouldn't bloat Redis if ClickHouse
    is not enabled.

--- ee/spec/lib/gitlab/tracking/ai_tracking_spec.rb
+++ ee/spec/lib/gitlab/tracking/ai_tracking_spec.rb
@@ -3,15 +3,19 @@
 require 'spec_helper'
 
 RSpec.describe Gitlab::Tracking::AiTracking, feature_category: :value_stream_management do
-  describe '.track_event', :freeze_time do
+  describe '.track_event', :freeze_time, :click_house do
     subject(:track_event) { described_class.track_event(event_name, event_context) }
 
     let(:event_context) { { user_id: 1, language: 'ruby' } }
     let(:event_name) { 'code_suggestions_requested' }
 
+    before do
+      stub_application_setting(use_clickhouse_for_analytics: true)
+    end
+
     it 'writes to ClickHouse buffer with event data' do
       expect(::ClickHouse::WriteBuffer)
-        .to receive(:write_event).with(event_context.merge(event: 1, timestamp: Time.zone.now)).once
+        .to receive(:write_event).with(event_context.merge(event: 1, timestamp: Time.current)).once
 
       track_event
     end
@@ -19,6 +23,18 @@ RSpec.describe Gitlab::Tracking::AiTracking, feature_category: :value_stream_man
     context 'when :ai_tracking_data_gathering feature flag is disabled' do
       before do
         stub_feature_flags(ai_tracking_data_gathering: false)
+      end
+
+      it 'does not write to ClickHouse buffer' do
+        expect(::ClickHouse::WriteBuffer).not_to receive(:write_event)
+
+        track_event
+      end
+    end
+
+    context 'when clickhouse is not enabled' do
+      before do
+        stub_application_setting(use_clickhouse_for_analytics: false)
       end
 
       it 'does not write to ClickHouse buffer' do
