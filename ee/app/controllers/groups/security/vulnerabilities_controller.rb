bd24183ddb5dd20ccfa9350d6c92468513163e31
Samantha Ming <sming@gitlab.com>
2024-04-23 20:00:46 +0200

    Remove FF activity_filter_has_remediations
    Remove the 'activity_filter_has_remediations' feature flag, which was used to
    control the development of the solution option in the activity filter.
    
    Changelog: changed
    EE: true

--- ee/app/controllers/groups/security/vulnerabilities_controller.rb
+++ ee/app/controllers/groups/security/vulnerabilities_controller.rb
@@ -13,7 +13,6 @@ module Groups
 
       before_action do
         push_frontend_feature_flag(:activity_filter_has_mr, @project)
-        push_frontend_feature_flag(:activity_filter_has_remediations, @project)
         push_frontend_feature_flag(:group_level_vulnerability_report_grouping, @group)
         push_frontend_feature_flag(:container_scanning_for_registry)
         push_frontend_feature_flag(:vulnerability_owasp_top_10_group, @group)
