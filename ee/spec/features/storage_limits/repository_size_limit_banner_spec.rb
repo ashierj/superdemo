721ebe8d274eb5b470a39f71253c36a19184dd24
Sheldon Led <sheldonled@gitlab.com>
2024-04-24 14:15:33 +0000

    Only show project limits alert on affected project
    Hides over storage limit alerts for projects under limits
    and for the group, except if in the group Usage Quotas.
    
    Changelog: changed
    MR: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/150017
    EE: true

--- ee/spec/features/storage_limits/repository_size_limit_banner_spec.rb
+++ ee/spec/features/storage_limits/repository_size_limit_banner_spec.rb
@@ -5,7 +5,7 @@ require "spec_helper"
 RSpec.describe "Repository size limit banner", :js, :saas, feature_category: :consumables_cost_management do
   include NamespaceStorageHelpers
 
-  let_it_be(:owner) { create(:user) }
+  let_it_be(:owner) { create(:user, namespace: create(:user_namespace)) }
   let_it_be(:free_group) { create(:group, owners: owner) }
   let_it_be_with_refind(:free_group_project) { create(:project, :repository, group: free_group) }
   let_it_be(:paid_group) { create(:group_with_plan, plan: :ultimate_plan, owners: owner) }
@@ -27,7 +27,7 @@ RSpec.describe "Repository size limit banner", :js, :saas, feature_category: :co
     it 'shows the banner when a project repository in a free group has exceed the storage limit' do
       free_group_project.statistics.update!(repository_size: 11.megabytes)
 
-      visit(group_path(free_group))
+      visit(project_path(free_group_project))
 
       expect(page).to have_text("You have reached the free storage limit of 10 MiB on 1 project")
     end
@@ -35,7 +35,7 @@ RSpec.describe "Repository size limit banner", :js, :saas, feature_category: :co
     it 'shows the banner when a project repository in a paid group has exceed the storage limit' do
       paid_group_project.statistics.update!(repository_size: 11.megabytes)
 
-      visit(group_path(paid_group))
+      visit(project_path(paid_group_project))
 
       expect(page).to have_text("You have reached the free storage limit of 10 MiB on 1 project")
     end
@@ -49,7 +49,7 @@ RSpec.describe "Repository size limit banner", :js, :saas, feature_category: :co
     it 'shows the banner when a project repository in a paid group has exceed the storage limit' do
       paid_group_project.statistics.update!(repository_size: 11.megabytes)
 
-      visit(group_path(paid_group))
+      visit(project_path(paid_group_project))
 
       expect(page).to have_text("You have reached the free storage limit of 10 MiB on 1 project")
     end
