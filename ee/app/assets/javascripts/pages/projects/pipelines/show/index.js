24fe55c0236146dde2e8d6c23be8f7a59bf5ca1c
Eduardo Sanz García <esanz-garcia@gitlab.com>
2024-04-26 08:42:43 +0000

    Remove unused functionality
    None of the classes in `init_pipelines.js` are present in the code:
    
    - js-toggle-mr-list
    - js-truncated-mr-list
    - js-full-mr-list
    - js-pipeline-tab-link
    - js-pipeline-container
    
    So all the related functionality can be deleted.

--- ee/app/assets/javascripts/pages/projects/pipelines/show/index.js
+++ ee/app/assets/javascripts/pages/projects/pipelines/show/index.js
@@ -1,10 +1,8 @@
 import initCCValidationRequiredAlert from 'ee/credit_card_validation_required_alert';
-import initPipelines from '~/pages/projects/pipelines/init_pipelines';
 import initPipelineDetails from '~/ci/pipeline_details/pipeline_details_bundle';
 import initCodequalityReport from './codequality_report';
 import initLicenseReport from './license_report';
 
-initPipelines();
 initPipelineDetails();
 initLicenseReport();
 initCodequalityReport();
