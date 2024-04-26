2fd20842d17724f3a974bc89b27d45b4762a5080
Lukas Eipert <leipert@gitlab.com>
2024-04-26 18:28:01 +0200

    Move vite copy plugin to a separate file
    Yes, and that is the last one for now!

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { readFileSync } from 'node:fs';
-import { stat, mkdir, copyFile } from 'node:fs/promises';
 import path from 'node:path';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2';
 import graphql from '@rollup/plugin-graphql';
-import globby from 'globby';
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 import webpackConfig from './config/webpack.config';
 import {
@@ -18,6 +16,7 @@ import {
 /* eslint-disable import/extensions */
 import { viteCSSCompilerPlugin } from './scripts/frontend/lib/compile_css.mjs';
 import { viteTailwindCompilerPlugin } from './scripts/frontend/tailwindcss.mjs';
+import { CopyPlugin } from './config/helpers/vite_plugin_copy.mjs';
 import { AutoStopPlugin } from './config/helpers/vite_plugin_auto_stop.mjs';
 import {
   PageEntrypointsPlugin,
@@ -59,76 +58,6 @@ const JH_ALIAS_FALLBACK = [
   },
 ];
 
-/**
- * This is a simple-reimplementation of the copy-webpack-plugin
- *
- * it also uses the `globby` package under the hood, and _only_ allows for copying
- * 1. absolute paths
- * 2. files and directories.
- */
-function viteCopyPlugin({ patterns }) {
-  return {
-    name: 'viteCopyPlugin',
-    async configureServer() {
-      console.warn('Start copying files...');
-      let count = 0;
-
-      const allTheFiles = patterns.map(async (patternEntry) => {
-        const { from, to, globOptions = {} } = patternEntry;
-
-        // By only supporting absolute paths we simplify
-        // the implementation a lot
-        if (!path.isAbsolute(from)) {
-          throw new Error(`'from' path is not absolute: ${path}`);
-        }
-        if (!path.isAbsolute(to)) {
-          throw new Error(`'to' path is not absolute: ${path}`);
-        }
-
-        let pattern = '';
-        let sourceRoot = '';
-        const fromStat = await stat(from);
-        if (fromStat.isDirectory()) {
-          sourceRoot = from;
-          pattern = path.join(from, '**/*');
-        } else if (fromStat.isFile()) {
-          sourceRoot = path.dirname(from);
-          pattern = from;
-        } else {
-          // No need to support globs, because we do not
-          // use them yet...
-          throw new Error('Our implementation does not support globs.');
-        }
-
-        globOptions.dot = globOptions.dot ?? true;
-
-        const paths = await globby(pattern, globOptions);
-
-        return paths.map((srcPath) => {
-          const targetPath = path.join(to, path.relative(sourceRoot, srcPath));
-          return { srcPath, targetPath };
-        });
-      });
-
-      const srcTargetMap = (await Promise.all(allTheFiles)).flat();
-
-      await Promise.all(
-        srcTargetMap.map(async ({ srcPath, targetPath }) => {
-          try {
-            await mkdir(path.dirname(targetPath), { recursive: true });
-            await copyFile(srcPath, targetPath);
-            count += 1;
-          } catch (e) {
-            console.warn(`Could not copy ${srcPath} => ${targetPath}`);
-          }
-        }),
-      );
-
-      console.warn(`Done copying ${count} files...`);
-    },
-  };
-}
-
 export default defineConfig({
   cacheDir: path.resolve(__dirname, 'tmp/cache/vite'),
   resolve: {
@@ -150,7 +79,7 @@ export default defineConfig({
     PageEntrypointsPlugin(),
     viteCSSCompilerPlugin({ shouldWatch: viteGDKConfig.hmr !== null }),
     viteTailwindCompilerPlugin({ shouldWatch: viteGDKConfig.hmr !== null }),
-    viteCopyPlugin({
+    CopyPlugin({
       patterns: copyFilesPatterns,
     }),
     viteGDKConfig.enabled ? AutoStopPlugin() : null,
